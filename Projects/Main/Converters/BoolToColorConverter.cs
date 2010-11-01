87860c067003d9eb4aa9128a75f259cb64d226e4
Alexander Sevastyanov <asevastyanov@griddynamics.com>
2010-11-01 20:22:56 +0300

    WPF Decoration

---
+++ Projects/Main/Converters/BoolToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Main.Converters
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool IsActive = (bool)value;
+            if (IsActive)
+                return new SolidColorBrush(Colors.Red);
+            return new SolidColorBrush(Colors.Black);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
