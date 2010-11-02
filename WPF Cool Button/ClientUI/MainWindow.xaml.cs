6ea87c17608f84945c8f446794c38a40b5640d0a
Alexander Sevastyanov <xbadcode@yandex.ru>
2010-11-03 00:58:10 +0300

    Cool Button Solution Added

---
+++ WPF Cool Button/ClientUI/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿//        Another Demo from Andy L. & MissedMemo.com
+// Borrow whatever code seems useful - just don't try to hold
+// me responsible for any ill effects. My demos sometimes use
+// licensed images which CANNOT legally be copied and reused.
+
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace ClientUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            // Toggle visibility of right-side panel on or off
+            CommandBindings.Add( new CommandBinding( CustomCommands.ToggleSecondView, ( sender, args ) =>
+                {
+                    panelRight.Visibility = panelRight.IsVisible ? Visibility.Collapsed : Visibility.Visible;
+                }
+            ) );
+        }
+    }
+}
