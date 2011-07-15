c936eafe888dc0fc88111df92fb7afebd4ff6370
xbadcode <xbadcode@yandex.ru>
2011-07-16 02:42:20 +0400

    DataGrid style refactoring

--- Projects/FireAdministrator/Modules/DevicesModule/ViewModels/Devices/DeviceViewModel.cs
+++ Projects/FireAdministrator/Modules/DevicesModule/ViewModels/Devices/DeviceViewModel.cs
@@ -28,17 +28,6 @@ namespace DevicesModule.ViewModels
             ShowPropertiesCommand = new RelayCommand(OnShowProperties, CanShowProperties);
         }
 
-        bool isExpanded;
-        public bool IsExpanded2
-        {
-            get { return isExpanded; }
-            set
-            {
-                isExpanded = value;
-                OnPropertyChanged("IsExpanded2");
-            }
-        }
-
         public void Initialize(Device device, ObservableCollection<DeviceViewModel> sourceDevices)
         {
             Source = sourceDevices;
