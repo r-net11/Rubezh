2744baf9cf9a44491d8612236b110bb75b58a431
pavlovpavel <pablo_new@mail.ru>
2011-08-16 06:39:57 +0400

    Исправлена серилизация на DataContract (не тестил)

--- Projects/FiresecService/FiresecServiceAPI/Models/Plans/Plan.cs
+++ Projects/FiresecService/FiresecServiceAPI/Models/Plans/Plan.cs
@@ -1,28 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Xml.Serialization;
+using System.Runtime.Serialization;
 
 namespace FiresecAPI.Models
 {
-    [Serializable]
+    [DataContract]
     public class Plan
     {
         public Plan Parent { get; set; }
         public List<Plan> Children { get; set; }
 
-        [XmlAttribute]
+        [DataMember]
         public string Name { get; set; }
-        [XmlAttribute]
+        [DataMember]
         public string Caption { get; set; }
-        [XmlAttribute]
+        [DataMember]
         public string Description { get; set; }
-        [XmlAttribute]
+        [DataMember]
         public string BackgroundSource { get; set; }
-        [XmlAttribute]
+        [DataMember]
         public bool ShowBackgroundImage { get; set; }
-        [XmlAttribute]
+        [DataMember]
         public double Width { get; set; }
-        [XmlAttribute]
+        [DataMember]
         public double Height { get; set; }
 
         public List<ElementSubPlan> ElementSubPlans { get; set; }
