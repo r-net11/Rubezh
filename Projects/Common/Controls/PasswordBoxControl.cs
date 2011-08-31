4ec73003548a10429053abcffed7031a2840cb97
MatazovAY <matazovay@gmail.com>
2011-08-31 12:46:43 +0400

    remove PasswordHelper, add PasswordBoxControl (change in Security module)

---
+++ Projects/Common/Controls/PasswordBoxControl.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Controls
+{
+    public class PasswordBoxControl : Decorator
+    {
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(string), typeof(PasswordBoxControl),
+            new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnPasswordPropertyChanged)));
+       
+        public PasswordBoxControl()
+        {
+            savedCallback = HandlePasswordChanged;
+
+            PasswordBox passwordBox = new PasswordBox();
+            passwordBox.PasswordChanged += savedCallback;
+            Child = passwordBox;
+        }
+
+        private bool isPreventCallback;
+        private RoutedEventHandler savedCallback;
+
+
+        public string Password
+        {
+            get { return GetValue(PasswordProperty) as string; }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        private static void OnPasswordPropertyChanged(DependencyObject dp, DependencyPropertyChangedEventArgs e)
+        {
+            PasswordBoxControl passwordBoxControl = dp as PasswordBoxControl;
+            if (passwordBoxControl == null)
+            {
+                return;
+            }
+            PasswordBox passwordBox = passwordBoxControl.Child as PasswordBox;
+
+            if (passwordBoxControl.isPreventCallback)
+            {
+                return;
+            }
+
+            passwordBox.PasswordChanged -= passwordBoxControl.savedCallback;
+            passwordBox.Password = (e.NewValue != null) ? e.NewValue.ToString() : "";
+            passwordBox.PasswordChanged += passwordBoxControl.savedCallback;
+        }
+
+        private void HandlePasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+            if (passwordBox == null)
+            {
+                return;
+            }
+            isPreventCallback = true;
+            Password = passwordBox.Password;
+            isPreventCallback = false;
+        }
+    }
+}
