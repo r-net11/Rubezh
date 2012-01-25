d2af107a5f29d329558b58c2b2e161fce4715d29
Alexander Sevastyanov <xbadcode@yandex.ru>
2012-01-25 17:28:56 +0400

    bugfix

---
+++ Projects/FireAdministrator/Modules/PlansModule/ViewModels/ElementGroupViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Infrastructure.Common;
+using System.Collections.ObjectModel;
+
+namespace PlansModule.ViewModels
+{
+    public class ElementGroupViewModel : BaseViewModel
+    {
+        public ElementGroupViewModel()
+        {
+            _isAllVisible = true;
+            _isAllSelectable = true;
+            Elements = new ObservableCollection<ElementViewModel>();
+        }
+
+        public string Name { get; set; }
+        public ObservableCollection<ElementViewModel> Elements { get; set; }
+
+        ElementViewModel _selectedElement;
+        public ElementViewModel SelectedElement
+        {
+            get { return _selectedElement; }
+            set
+            {
+                _selectedElement = value;
+                OnPropertyChanged("SelectedElement");
+            }
+        }
+
+        bool _isExpanded;
+        public bool IsExpanded
+        {
+            get { return _isExpanded; }
+            set
+            {
+                _isExpanded = value;
+                OnPropertyChanged("IsExpanded");
+            }
+        }
+
+        bool _isAllVisible;
+        public bool IsAllVisible
+        {
+            get { return _isAllVisible; }
+            set
+            {
+                _isAllVisible = value;
+                foreach (var device in Elements)
+                    device.IsVisible = value;
+                OnPropertyChanged("IsAllVisible");
+            }
+        }
+
+        bool _isAllSelectable;
+        public bool IsAllSelectable
+        {
+            get { return _isAllSelectable; }
+            set
+            {
+                _isAllSelectable = value;
+                foreach (var device in Elements)
+                    device.IsSelectable = value;
+                OnPropertyChanged("IsAllSelectable");
+            }
+        }
+
+        public bool HasElements
+        {
+            get { return Elements.Count > 0; }
+        }
+
+        public void UpdateHasElements()
+        {
+            OnPropertyChanged("HasElements");
+        }
+    }
+}
--- Projects/FireAdministrator/Modules/PlansModule/ViewModels/ElementsViewModel.cs
+++ Projects/FireAdministrator/Modules/PlansModule/ViewModels/ElementsViewModel.cs
@@ -17,18 +17,29 @@ namespace PlansModule.ViewModels
         public ElementsViewModel(DesignerCanvas designerCanvas)
         {
             DesignerCanvas = designerCanvas;
-            Devices = new ObservableCollection<ElementViewModel>();
-            Zones = new ObservableCollection<ElementViewModel>();
-            SubPlans = new ObservableCollection<ElementViewModel>();
-            Elements = new ObservableCollection<ElementViewModel>();
+
+            ElementGroupDevices = new ElementGroupViewModel() { Name = "Устройства" };
+            ElementGroupZones = new ElementGroupViewModel() { Name = "Зоны" };
+            ElementGroupSubPlans = new ElementGroupViewModel() { Name = "Подпланы" };
+            ElementGroupElements = new ElementGroupViewModel() { Name = "Элементы" };
+            ElementGroups = new List<ElementGroupViewModel>();
+            ElementGroups.Add(ElementGroupDevices);
+            ElementGroups.Add(ElementGroupZones);
+            ElementGroups.Add(ElementGroupSubPlans);
+            ElementGroups.Add(ElementGroupElements);
 
             ServiceFactory.Events.GetEvent<ElementAddedEvent>().Subscribe(OnElementAdded);
             ServiceFactory.Events.GetEvent<ElementRemovedEvent>().Subscribe(OnElementRemoved);
             ServiceFactory.Events.GetEvent<ElementChangedEvent>().Subscribe(OnElementChanged);
-            //ServiceFactory.Events.GetEvent<PlanChangedEvent>().Subscribe(OnPlansChanged);
             ServiceFactory.Events.GetEvent<ElementSelectedEvent>().Subscribe(OnElementSelected);
             Update();
         }
+
+        public List<ElementGroupViewModel> ElementGroups { get; private set; }
+        ElementGroupViewModel ElementGroupDevices;
+        ElementGroupViewModel ElementGroupZones;
+        ElementGroupViewModel ElementGroupSubPlans;
+        ElementGroupViewModel ElementGroupElements;
 
         void OnPlansChanged(Guid planUID)
         {
@@ -37,24 +48,15 @@ namespace PlansModule.ViewModels
 
         public void Update()
         {
-            Devices.Clear();
-            Zones.Clear();
-            SubPlans.Clear();
-            Elements.Clear();
+            foreach (var elementGroup in ElementGroups)
+            {
+                elementGroup.Elements.Clear();
+            }
 
             foreach (var designerItem in DesignerCanvas.Items)
             {
                 AddDesignerItem(designerItem);
             }
-
-            IsDevicesVisible = true;
-            IsZonesVisible = true;
-            IsSubPlansVisible = true;
-            IsElementsVisible = true;
-            IsDevicesSelectable = true;
-            IsZonesSelectable = true;
-            IsSubPlansSelectable = true;
-            IsElementsSelectable = true;
         }
 
         void AddDesignerItem(DesignerItem designerItem)
@@ -66,7 +68,7 @@ namespace PlansModule.ViewModels
             {
                 ElementDevice elementDevice = elementBase as ElementDevice;
                 name = elementDevice.Device.DottedAddress + " " + elementDevice.Device.Driver.ShortName;
-                Devices.Add(new ElementViewModel(designerItem, name));
+                ElementGroupDevices.Elements.Add(new ElementViewModel(designerItem, name));
             }
             if (elementBase is IElementZone)
             {
@@ -79,7 +81,7 @@ namespace PlansModule.ViewModels
                 {
                     name = "Несвязанная зона";
                 }
-                Zones.Add(new ElementViewModel(designerItem, name));
+                ElementGroupZones.Elements.Add(new ElementViewModel(designerItem, name));
             }
             if (elementBase is ElementSubPlan)
             {
@@ -92,160 +94,31 @@ namespace PlansModule.ViewModels
                 {
                     name = "Несвязанный подплан";
                 }
-                SubPlans.Add(new ElementViewModel(designerItem, name));
+                ElementGroupSubPlans.Elements.Add(new ElementViewModel(designerItem, name));
             }
             if (elementBase is ElementEllipse)
             {
-                Elements.Add(new ElementViewModel(designerItem, "Эллипс"));
+                ElementGroupElements.Elements.Add(new ElementViewModel(designerItem, "Эллипс"));
             }
             if (elementBase is ElementPolygon)
             {
-                Elements.Add(new ElementViewModel(designerItem, "Многоугольник"));
+                ElementGroupElements.Elements.Add(new ElementViewModel(designerItem, "Многоугольник"));
             }
             if (elementBase is ElementRectangle)
             {
-                Elements.Add(new ElementViewModel(designerItem, "Прямоугольник"));
+                ElementGroupElements.Elements.Add(new ElementViewModel(designerItem, "Прямоугольник"));
             }
             if (elementBase is ElementTextBlock)
             {
-                Elements.Add(new ElementViewModel(designerItem, "Надпись"));
+                ElementGroupElements.Elements.Add(new ElementViewModel(designerItem, "Надпись"));
             }
-        }
-
-        public ObservableCollection<ElementViewModel> Devices { get; private set; }
-        public ObservableCollection<ElementViewModel> Zones { get; private set; }
-        public ObservableCollection<ElementViewModel> SubPlans { get; private set; }
-        public ObservableCollection<ElementViewModel> Elements { get; private set; }
-
-        public bool HasDevices
-        {
-            get { return Devices.Count > 0; }
-        }
-
-        public bool HasZones
-        {
-            get { return Zones.Count > 0; }
-        }
-
-        public bool HasSubPlans
-        {
-            get { return SubPlans.Count > 0; }
-        }
-
-        public bool HasElements
-        {
-            get { return Elements.Count > 0; }
         }
 
         void UpdateHasItems()
         {
-            OnPropertyChanged("HasDevices");
-            OnPropertyChanged("HasZones");
-            OnPropertyChanged("HasSubPlans");
-            OnPropertyChanged("HasElements");
-        }
-
-        bool _isDevicesVisible;
-        public bool IsDevicesVisible
-        {
-            get { return _isDevicesVisible; }
-            set
+            foreach (var elementGroup in ElementGroups)
             {
-                _isDevicesVisible = value;
-                foreach (var device in Devices)
-                    device.IsVisible = value;
-                OnPropertyChanged("IsDevicesVisible");
-            }
-        }
-
-        bool _isZonesVisible;
-        public bool IsZonesVisible
-        {
-            get { return _isZonesVisible; }
-            set
-            {
-                _isZonesVisible = value;
-                foreach (var zone in Zones)
-                    zone.IsVisible = value;
-                OnPropertyChanged("IsZonesVisible");
-            }
-        }
-
-        bool _isSubPlansVisible;
-        public bool IsSubPlansVisible
-        {
-            get { return _isSubPlansVisible; }
-            set
-            {
-                _isSubPlansVisible = value;
-                foreach (var subPlan in SubPlans)
-                    subPlan.IsVisible = value;
-                OnPropertyChanged("IsSubPlansVisible");
-            }
-        }
-
-        bool _isElementsVisible;
-        public bool IsElementsVisible
-        {
-            get { return _isElementsVisible; }
-            set
-            {
-                _isElementsVisible = value;
-                foreach (var element in Elements)
-                    element.IsVisible = value;
-                OnPropertyChanged("IsElementsVisible");
-            }
-        }
-
-        bool _isDevicesSelectable;
-        public bool IsDevicesSelectable
-        {
-            get { return _isDevicesSelectable; }
-            set
-            {
-                _isDevicesSelectable = value;
-                foreach (var device in Devices)
-                    device.IsSelectable = value;
-                OnPropertyChanged("IsDevicesSelectable");
-            }
-        }
-
-        bool _isZonesSelectable;
-        public bool IsZonesSelectable
-        {
-            get { return _isZonesSelectable; }
-            set
-            {
-                _isZonesSelectable = value;
-                foreach (var zone in Zones)
-                    zone.IsSelectable = value;
-                OnPropertyChanged("IsZonesSelectable");
-            }
-        }
-
-        bool _isSubPlansSelectable;
-        public bool IsSubPlansSelectable
-        {
-            get { return _isSubPlansSelectable; }
-            set
-            {
-                _isSubPlansSelectable = value;
-                foreach (var subPlan in SubPlans)
-                    subPlan.IsSelectable = value;
-                OnPropertyChanged("IsSubPlansSelectable");
-            }
-        }
-
-        bool _isElementsSelectable;
-        public bool IsElementsSelectable
-        {
-            get { return _isElementsSelectable; }
-            set
-            {
-                _isElementsSelectable = value;
-                foreach (var element in Elements)
-                    element.IsSelectable = value;
-                OnPropertyChanged("IsElementsSelectable");
+                elementGroup.UpdateHasElements();
             }
         }
 
@@ -267,41 +140,37 @@ namespace PlansModule.ViewModels
         {
             foreach(var elementBase in elements)
             {
-                var device = Devices.FirstOrDefault(x => x.DesignerItem.ElementBase.UID == elementBase.UID);
-                if (device != null)
+                foreach (var elementGroup in ElementGroups)
                 {
-                    Devices.Remove(device);
-                }
-
-                var zone = Zones.FirstOrDefault(x => x.DesignerItem.ElementBase.UID == elementBase.UID);
-                if (zone != null)
-                {
-                    Zones.Remove(zone);
-                }
-
-                var subPlan = SubPlans.FirstOrDefault(x => x.DesignerItem.ElementBase.UID == elementBase.UID);
-                if (subPlan != null)
-                {
-                    SubPlans.Remove(subPlan);
-                }
-
-                var element = Elements.FirstOrDefault(x => x.DesignerItem.ElementBase.UID == elementBase.UID);
-                if (element != null)
-                {
-                    Elements.Remove(element);
+                    var element = elementGroup.Elements.FirstOrDefault(x => x.DesignerItem.ElementBase.UID == elementBase.UID);
+                    if (element != null)
+                    {
+                        elementGroup.Elements.Remove(element);
+                    }
                 }
             }
 
             UpdateHasItems();
         }
 
-        void OnElementSelected(List<ElementBase> elements)
+        void OnElementChanged(List<ElementBase> elements)
         {
         }
 
-        void OnElementChanged(List<ElementBase> elements)
+        void OnElementSelected(Guid elementUID)
         {
-
+            foreach (var elementGroup in ElementGroups)
+            {
+                foreach (var element in elementGroup.Elements)
+                {
+                    if (element.DesignerItem.ElementBase.UID == elementUID)
+                    {
+                        elementGroup.IsExpanded = true;
+                        elementGroup.SelectedElement = element;
+                        return;
+                    }
+                }
+            }
         }
     }
 }
