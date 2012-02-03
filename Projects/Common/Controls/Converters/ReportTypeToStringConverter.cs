35a5267889f9ff018db046e630e487c7037787d9
MatazovAY <matazovay@gmail.com>
2012-02-03 17:25:55 +0400

    bugfix

--- Projects/Common/Controls/Converters/ReportTypeToStringConverter.cs
+++ Projects/Common/Controls/Converters/ReportTypeToStringConverter.cs
@@ -11,6 +11,9 @@ namespace Controls.Converters
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
+            var str = value as string;
+            if (str != null && str == "")
+                return null;
             return ((ReportType)value).ToDescription();
         }
 
