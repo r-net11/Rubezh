cfcb22a5ab72b20e861713d448326a071b0870a6
Alexander Sevastyanov <xbadcode@yandex.ru>
2012-04-06 20:29:57 +0400

    bugfix

--- Projects/FireAdministrator/Modules/DevicesModule/Devices/Views/DevicesView.xaml.cs
+++ Projects/FireAdministrator/Modules/DevicesModule/Devices/Views/DevicesView.xaml.cs
@@ -13,6 +13,7 @@ namespace DevicesModule.Views
             InitializeComponent();
             Loaded += new RoutedEventHandler(DevicesView_Loaded);
             _devicesDataGrid.SelectionChanged += new SelectionChangedEventHandler(DevicesView_SelectionChanged);
+            _devicesDataGrid.PreviewKeyDown += new KeyEventHandler(_devicesDataGrid_PreviewKeyDown);
         }
 
         void DevicesView_Loaded(object sender, RoutedEventArgs e)
@@ -27,9 +28,14 @@ namespace DevicesModule.Views
             if (_devicesDataGrid.SelectedItem != null)
                 _devicesDataGrid.ScrollIntoView(_devicesDataGrid.SelectedItem);
         }
-                {
-                    e.Handled = true;
-                }
+
+        private void _devicesDataGrid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                var dataGrid = sender as DataGrid;
+                if (dataGrid != null)
+                    dataGrid.BeginEdit();
             }
         }
 
