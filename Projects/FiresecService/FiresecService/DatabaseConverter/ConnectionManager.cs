56f18e24edfcb67bd95ad8e8befa328cc0972634
Vladimir <Sorokonenko@gmail.com>
2012-04-10 03:59:30 +0400

    Added SQL CE

---
+++ Projects/FiresecService/FiresecService/DatabaseConverter/ConnectionManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using FiresecService.Properties;
+
+namespace FiresecService.DatabaseConverter
+{
+	public static class ConnectionManager
+	{
+		public static ConnectionStringSettings ConnectionSettings
+		{
+			get { return ConfigurationManager.ConnectionStrings[Settings.Default.FiresecConnection]; }
+		}
+		public static string ConnectionString
+		{
+			get { return ConnectionSettings.ConnectionString; }
+		}
+
+		public static FiresecDbConverterDataContext CreateFiresecDataContext()
+		{
+			return new FiresecDbConverterDataContext(ConnectionString);
+		}
+	}
+}
