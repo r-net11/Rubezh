fa4149a1df98d73caca58f9dfd30019f8897fb0c
SorokonenkoV <Sorokonenko@gmail.com>
2012-04-10 19:18:54 +0400

    Moved Vlc to separated solution and dlls to third party components

---
+++ Projects/Deploy/Scripts/SKUD.sql
@@ -0,0 +1,50 @@
+USE [Firesec]
+GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Employee_Person]') AND parent_object_id = OBJECT_ID(N'[dbo].[Employee]'))
+ALTER TABLE [dbo].[Employee] DROP CONSTRAINT [FK_Employee_Person]
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Person]') AND type in (N'U'))
+DROP TABLE [dbo].[Person]
+GO
+/****** Объект:  Table [dbo].[Person]    Дата сценария: 11/11/2011 18:46:48 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Person](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[LastNmae] [nvarchar](max) NULL,
+	[FirstName] [nvarchar](max) NULL,
+	[SecondName] [nvarchar](max) NULL,
+	[Birthday] [datetime] NULL,
+	[Sex] [datetime] NULL,
+	[Comment] [nvarchar](max) NULL
+ CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Employee]') AND type in (N'U'))
+DROP TABLE [dbo].[Employee]
+GO
+/****** Объект:  Table [dbo].[Employee]    Дата сценария: 11/11/2011 18:46:48 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Employee](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[PersonId] [int] NOT NULL,
+	[Staff] [nvarchar](max) NULL,
+	[Comment] [nvarchar](max) NULL
+ CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Employee]  WITH CHECK ADD  CONSTRAINT [FK_Employee_Person] FOREIGN KEY([PersonId])
+REFERENCES [dbo].[Person] ([Id])
+GO
+ALTER TABLE [dbo].[Employee] CHECK CONSTRAINT [FK_Employee_Person]
