22d6e28fb3d98d217c024c54cf32bb365e648e5a
MatazovAY <matazovay@gmail.com>
2012-04-20 17:42:37 +0400

    custom action

---
+++ Projects/Installers/FiresecInstaller/CustomAction/CustomAction.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace CustomAction
+{
+	public class CustomActions
+	{
+		[CustomAction]
+		public static ActionResult KillFiresecProcess(Session session)
+		{
+			session.Log("Begin Kill terminate processes");
+			string _firesecServiceName = session["FIRESECSERVICENAME"];
+			string _fireMonitorName = session["FIREMONITORNAME"];
+			string _fireAdministratorName = session["FIREADMINISTRATORNAME"];
+			Process[] processes = Process.GetProcesses();
+			foreach (var process in processes)
+			{
+				if ((process.ProcessName == _firesecServiceName)
+					|| (process.ProcessName == _fireMonitorName)
+					|| (process.ProcessName == _fireAdministratorName))
+					process.Kill();
+			}
+
+			return ActionResult.Success;
+		}
+	}
+}
