32240ad224ddca8c031a18e0c0018cf27de36c83
SorokonenkoV <Sorokonenko@gmail.com>
2012-05-29 20:03:09 +0400

    Window model update

--- Projects/Common/Infrastructure.Common/Windows/ViewModels/_debugSaveCancelViewModel.cs
+++ Projects/Common/Infrastructure.Common/Windows/ViewModels/_debugSaveCancelViewModel.cs
@@ -9,10 +9,23 @@ namespace Infrastructure.Common.Windows.ViewModels
 	{
 		public _debugSaveCancelViewModel()
 		{
-			//ContentFotter = "footter...";
-			//ContentHeader = "header...";
-			//MainContent = "MAIN CONTENT";
-			//Toolbar = "main toolbar";
+			ContentFotter = new _debugReplacer("footter...");
+			ContentHeader = new _debugReplacer("header...");
+			MainContent = new _debugReplacer("MAIN CONTENT");
+			Toolbar = new _debugReplacer("main toolbar");
+		}
+	}
+
+	public class _debugReplacer : BaseViewModel
+	{
+		public string Value { get; private set; }
+		public _debugReplacer(string value)
+		{
+			Value = value;
+		}
+		public override string ToString()
+		{
+			return Value;
 		}
 	}
 }
