be08f1948c5d1dee4eac80c046ba30d76a1b7c1d
MatazovAY <matazovay@gmail.com>
2012-07-05 16:38:43 +0400

    bugfix

--- Projects/FireMonitor/Modules/ReportsModule/Reports/BaseReportGeneric.cs
+++ Projects/FireMonitor/Modules/ReportsModule/Reports/BaseReportGeneric.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using CrystalDecisions.CrystalReports.Engine;
 using FiresecClient;
+using System.Data;
+using System.Reflection;
 
 namespace ReportsModule.Reports
 {
@@ -19,5 +21,25 @@ namespace ReportsModule.Reports
 			reportDocument.FileName = FileHelper.GetReportFilePath(ReportFileName);
             reportDocument.SetDataSource(DataList);
         }
+
+		public static DataTable ListToDataTable<T>(List<T> list)
+		{
+			DataTable dt = new DataTable();
+
+			foreach (PropertyInfo info in typeof(T).GetProperties())
+			{
+				dt.Columns.Add(new DataColumn(info.Name, info.PropertyType));
+			}
+			foreach (T t in list)
+			{
+				DataRow row = dt.NewRow();
+				foreach (PropertyInfo info in typeof(T).GetProperties())
+				{
+					row[info.Name] = info.GetValue(t, null);
+				}
+				dt.Rows.Add(row);
+			}
+			return dt;
+		}
     }
 }
