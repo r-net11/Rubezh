406668983075d55713c5b3ff5bddadbaeb65e1e9
Alexander Sevastyanov <no>
2012-08-19 12:04:25 +0400

    новые стили

--- Projects/Common/Infrastructure.Common/Windows/Views/DialogHeaderView.xaml.cs
+++ Projects/Common/Infrastructure.Common/Windows/Views/DialogHeaderView.xaml.cs
@@ -4,16 +4,16 @@ using Infrastructure.Common.Windows.ViewModels;
 
 namespace Infrastructure.Common.Windows.Views
 {
-	public partial class DialogHeaderView : UserControl
-	{
-		public DialogHeaderView()
-		{
-			InitializeComponent();
-		}
+    public partial class DialogHeaderView : UserControl
+    {
+        public DialogHeaderView()
+        {
+            InitializeComponent();
+        }
 
-		private void OnCloseButton(object sender, RoutedEventArgs e)
-		{
-			(((DialogHeaderViewModel)DataContext).Content).Close(false);
-		}
-	}
+        private void OnCloseButton(object sender, RoutedEventArgs e)
+        {
+            (((DialogHeaderViewModel)DataContext).Content).Close(false);
+        }
+    }
 }
