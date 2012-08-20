216f79850466ca7ac670d7150773c04cc2fa0613
Alexander Sevastyanov <no>
2012-08-21 02:27:45 +0400

    installer refactoring

--- Projects/Installers/FiresecInstaller/CustomAction/CustomAction.cs
+++ Projects/Installers/FiresecInstaller/CustomAction/CustomAction.cs
@@ -3,33 +3,33 @@ using Microsoft.Deployment.WindowsInstaller;
 
 namespace CustomAction
 {
-	public class CustomActions
-	{
-		[CustomAction]
-		public static ActionResult KillFiresecProcess(Session session)
-		{
-			session.Log("Begin Kill terminate processes");
-			string _firesecServiceName = session["FIRESECSERVICENAME"];
-			string _fireMonitorName = session["FIREMONITORNAME"];
-			string _fireAdministratorName = session["FIREADMINISTRATORNAME"];
-			string _firesecNtService = "FiresecNTService";
-			string _socketService = "scktsrvr";
-			string _oldFiresecService = "FS_SER~1";
-			string _oldFiresecService2 = "fs_server";
-			Process[] processes = Process.GetProcesses();
-			foreach (var process in processes)
-			{
-				if ((process.ProcessName == _firesecServiceName)
-					|| (process.ProcessName == _fireMonitorName)
-					|| (process.ProcessName == _fireAdministratorName)
-					|| (process.ProcessName == _oldFiresecService)
-					|| (process.ProcessName == _oldFiresecService2)
-					|| (process.ProcessName == _firesecNtService)
-					|| (process.ProcessName == _socketService))
-					process.Kill();
-			}
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult KillFiresecProcess(Session session)
+        {
+            session.Log("Begin Kill terminate processes");
+            string _firesecServiceName = session["FIRESECSERVICENAME"];
+            string _fireMonitorName = session["FIREMONITORNAME"];
+            string _fireAdministratorName = session["FIREADMINISTRATORNAME"];
+            string _firesecNtService = "FiresecNTService";
+            string _socketService = "scktsrvr";
+            string _oldFiresecService = "FS_SER~1";
+            string _oldFiresecService2 = "fs_server";
+            Process[] processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                if ((process.ProcessName == _firesecServiceName)
+                    || (process.ProcessName == _fireMonitorName)
+                    || (process.ProcessName == _fireAdministratorName)
+                    || (process.ProcessName == _oldFiresecService)
+                    || (process.ProcessName == _oldFiresecService2)
+                    || (process.ProcessName == _firesecNtService)
+                    || (process.ProcessName == _socketService))
+                    process.Kill();
+            }
 
-			return ActionResult.Success;
-		}
-	}
+            return ActionResult.Success;
+        }
+    }
 }
--- Projects/Installers/FiresecWindowsService/FiresecWindowsService/FiresecNTService.cs
+++ Projects/Installers/FiresecWindowsService/FiresecWindowsService/FiresecNTService.cs
@@ -1,59 +1,54 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.ServiceProcess;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Security;
-using System.Management;
-using System.ComponentModel;
 
 namespace FiresecNTService
 {
-	public partial class FiresecNTService : ServiceBase
-	{
-		
+    public partial class FiresecNTService : ServiceBase
+    {
+        public FiresecNTService()
+        {
+            InitializeComponent();
+        }
 
-		public FiresecNTService()
-		{
-			InitializeComponent();
-		}
+        static void Main(string[] args)
+        {
+            var servicesToRun = new ServiceBase[] { new FiresecNTService() };
+            ServiceBase.Run(servicesToRun);
+        }
 
-		static void Main(string[] args)
-		{
-			var servicesToRun = new ServiceBase[] { new FiresecNTService() };
-			ServiceBase.Run(servicesToRun);
-		}
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+            var commandLineArgs = Environment.GetCommandLineArgs();
+            DirectoryInfo dirInfo = new DirectoryInfo(commandLineArgs[0]);
+            var nameFiresecExe = commandLineArgs[1];
+            var pathFiresecExe = dirInfo.FullName.Replace(dirInfo.Name, "") + nameFiresecExe;
+            try
+            {
+                StringBuilder output = new StringBuilder();
+                if (!Win32API.CreateProcessAsUser(pathFiresecExe, dirInfo.FullName.Replace(dirInfo.Name, ""), "winlogon", out output))
+                    Process.Start(pathFiresecExe);
+            }
+            catch (Win32Exception)
+            {
+            }
+        }
 
-		protected override void OnStart(string[] args)
-		{
-			base.OnStart(args);
-			var commandLineArgs = Environment.GetCommandLineArgs();
-			DirectoryInfo dirInfo = new DirectoryInfo(commandLineArgs[0]);
-			var nameFiresecExe = commandLineArgs[1];
-			var pathFiresecExe = dirInfo.FullName.Replace(dirInfo.Name, "") + nameFiresecExe;
-			try
-			{
-				StringBuilder output = new StringBuilder();
-				if (!Win32API.CreateProcessAsUser(pathFiresecExe, dirInfo.FullName.Replace(dirInfo.Name, ""), "winlogon", out output))
-					Process.Start(pathFiresecExe);
-			}
-			catch (Win32Exception ex)
-			{
-			}
-		}
-
-		protected override void OnStop()
-		{
-			base.OnStop();
-			var processes = Process.GetProcesses();
-			foreach (var process in processes)
-			{
-				if (process.ProcessName == "FiresecService")
-				{
-					process.Kill();
-				}
-			}
-		}
-	}
+        protected override void OnStop()
+        {
+            base.OnStop();
+            var processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                if (process.ProcessName == "FiresecService")
+                {
+                    process.Kill();
+                }
+            }
+        }
+    }
 }
