ed6d26cb1d0174fd0f2b5134cf08245303ecc198
Vlad <no>
2012-08-30 02:20:13 +0400

    журнал событий гк

---
+++ Projects/FireMonitor/Modules/GKModule/Journal/Views/JournalsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace GKModule.Views
+{
+    public partial class JournalsView : UserControl
+    {
+        public JournalsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
