3b5bd2ce90e8a7ff7024e72e84a0b8502dd39a65
SorokonenkoV <Sorokonenko@gmail.com>
2012-09-06 18:43:09 +0400

    bugfixing

--- Projects/FireAdministrator/Modules/PlansModule/ViewModels/PlansMenuViewModel.cs
+++ Projects/FireAdministrator/Modules/PlansModule/ViewModels/PlansMenuViewModel.cs
@@ -25,19 +25,19 @@ namespace PlansModule.ViewModels
 	            new MenuButtonViewModel(context.EditCommand, "/Controls;component/Images/Edit.png" , "Редактировать"),
 	            new MenuButtonViewModel(context.RemoveCommand, "/Controls;component/Images/Delete.png" , "Удалить"),
 				//new MenuButtonViewModel(context.AddSubPlanCommand, "/Controls;component/Images/AddChild.png" , "Добавить дочерний план"),
-				new MenuSeparatorViewModel(),
+				//new MenuSeparatorViewModel(),
 	            new MenuButtonViewModel(context.CopyCommand, "/Controls;component/Images/Copy.png" , "Копировать"),
 				new MenuButtonViewModel(context.CutCommand, "/Controls;component/Images/Cut.png" , "Вырезать"),
 				new MenuButtonViewModel(context.PasteCommand, "/Controls;component/Images/Paste.png" , "Вставить"),
 				new MenuButtonViewModel(context.UndoCommand, "/Controls;component/Images/Undo.png" , "Отменить"),
 				new MenuButtonViewModel(context.RedoCommand, "/Controls;component/Images/Redo.png" , "Применить"),
-				new MenuSeparatorViewModel(),
+				//new MenuSeparatorViewModel(),
 	            new MenuButtonViewModel(context.PlanDesignerViewModel.MoveToFrontCommand, "/Controls;component/Images/MoveForward.png" , "Вверх"),
 	            new MenuButtonViewModel(context.PlanDesignerViewModel.SendToBackCommand, "/Controls;component/Images/MoveBackward.png" , "Вниз"),
 	            new MenuButtonViewModel(context.PlanDesignerViewModel.MoveForwardCommand, "/Controls;component/Images/MoveFront.png" , "Выше"),
 	            new MenuButtonViewModel(context.PlanDesignerViewModel.MoveBackwardCommand, "/Controls;component/Images/MoveBack.png" , "Ниже"),
-				new MenuSeparatorViewModel(),
-				context.DesignerCanvas.Toolbox
+				//new MenuSeparatorViewModel(),
+				//context.DesignerCanvas.Toolbox
 	        };
 		}
 	}
