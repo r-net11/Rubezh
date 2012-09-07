390f34b480d1cb8eb629a58c2959f6c716945a6d
Роман Казаков <fx-romario@rambler.ru>
2012-09-07 17:58:10 +0400

    some icons and headers have changed

--- Projects/FireAdministrator/Modules/DevicesModule/Guard/Views/GuardMenuView.xaml.cs
+++ Projects/FireAdministrator/Modules/DevicesModule/Guard/Views/GuardMenuView.xaml.cs
@@ -15,31 +15,31 @@ namespace DevicesModule.Views
 
 		static GuardMenuView()
 		{
-			EventManager.RegisterClassHandler(typeof(Window), Keyboard.KeyDownEvent, new KeyEventHandler(OnKeyDownStatic), true);
+            //EventManager.RegisterClassHandler(typeof(Window), Keyboard.KeyDownEvent, new KeyEventHandler(OnKeyDownStatic), true);
 		}
 
 		public static GuardMenuView Current { get; private set; }
 		public bool AcceptKeyboard { get; set; }
 
-		static void OnKeyDownStatic(object sender, KeyEventArgs e)
-		{
-			Current.OnKeyDown(sender, e);
-		}
+        //static void OnKeyDownStatic(object sender, KeyEventArgs e)
+        //{
+        //    Current.OnKeyDown(sender, e);
+        //}
 
-		void OnKeyDown(object sender, KeyEventArgs e)
-		{
-			if (AcceptKeyboard != true)
-				return;
+        //void OnKeyDown(object sender, KeyEventArgs e)
+        //{
+        //    if (AcceptKeyboard != true)
+        //        return;
 
-			if (e.Key == Key.Delete && Keyboard.Modifiers == ModifierKeys.Control)
-				PressButton(deleteButton);
+        //    if (e.Key == Key.Delete && Keyboard.Modifiers == ModifierKeys.Control)
+        //        PressButton(deleteButton);
 
-			if (e.Key == Key.R && Keyboard.Modifiers == ModifierKeys.Control)
-				PressButton(editButton);
+        //    if (e.Key == Key.R && Keyboard.Modifiers == ModifierKeys.Control)
+        //        PressButton(editButton);
 
-			if (e.Key == Key.N && Keyboard.Modifiers == ModifierKeys.Control)
-				PressButton(addButton);
-		}
+        //    if (e.Key == Key.N && Keyboard.Modifiers == ModifierKeys.Control)
+        //        PressButton(addButton);
+        //}
 
 		void PressButton(Button button)
 		{
