2cba00ee4abbeccb8ee678ffcce17e4948433ffa
Роман Казаков <fx-romario@rambler.ru>
2012-09-10 17:50:16 +0400

    separation parameters

---
+++ Projects/FireAdministrator/Modules/DevicesModule/Devices/ViewModels/PropertyDetails/DeviceProperties/PropertiesViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using DevicesModule.ViewModels;
+using FiresecAPI.Models;
+using Infrastructure;
+using Infrastructure.Common;
+using Infrastructure.Common.Windows.ViewModels;
+
+namespace DevicesModule.DeviceProperties
+{
+	public class PropertiesViewModel : BaseViewModel
+	{
+		public Device Device { get; private set; }
+		public List<StringPropertyViewModel> StringProperties { get; set; }
+		public List<BoolPropertyViewModel> BoolProperties { get; set; }
+		public List<EnumPropertyViewModel> EnumProperties { get; set; }
+        public static DevicesViewModel Context { get; private set; }
+
+        public PropertiesViewModel(DevicesViewModel deviceViewModel)
+        {
+            Context = deviceViewModel;
+        }
+
+		public PropertiesViewModel(Device device)
+		{
+            OneCommand = new RelayCommand(OnOne);
+            TwoCommand = new RelayCommand(OnTwo);
+			Device = device;
+			StringProperties = new List<StringPropertyViewModel>();
+			BoolProperties = new List<BoolPropertyViewModel>();
+			EnumProperties = new List<EnumPropertyViewModel>();
+
+			foreach (var driverProperty in device.Driver.Properties)
+			{
+				if (driverProperty.IsHidden)
+					continue;
+
+				switch (driverProperty.DriverPropertyType)
+				{
+					case DriverPropertyTypeEnum.EnumType:
+						EnumProperties.Add(new EnumPropertyViewModel(driverProperty, device));
+						break;
+
+					case DriverPropertyTypeEnum.StringType:
+					case DriverPropertyTypeEnum.IntType:
+					case DriverPropertyTypeEnum.ByteType:
+						StringProperties.Add(new StringPropertyViewModel(driverProperty, device));
+						break;
+
+					case DriverPropertyTypeEnum.BoolType:
+						BoolProperties.Add(new BoolPropertyViewModel(driverProperty, device));
+						break;
+				}
+			}
+		}
+
+		public bool IsMonitoringDisabled
+		{
+			get { return Device.IsMonitoringDisabled; }
+			set
+			{
+				Device.IsMonitoringDisabled = value;
+				OnPropertyChanged("IsMonitoringDisabled");
+				ServiceFactory.SaveService.DevicesChanged = true;
+			}
+		}
+
+	    private bool aUParameterVis = true;
+        public bool AUParameterVis
+	    {
+            get { return aUParameterVis; }
+            set 
+            {
+                aUParameterVis = value;
+                OnPropertyChanged("AUParameterVis");
+            }
+	    }
+
+        public RelayCommand OneCommand { get; private set; }
+        private void OnOne()
+        {
+            AUParameterVis = true;
+        }
+        public RelayCommand TwoCommand { get; private set; }
+        private void OnTwo()
+        {
+            AUParameterVis = false;
+        }
+    }
+}
