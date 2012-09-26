3dbaf6a4299b3be566bf5a30e64da5982e449813
Vlad <no>
2012-09-27 00:57:10 +0400

    installer refactoring

--- Projects/FiresecOPCServer/FiresecOPCServer/App.xaml.cs
+++ Projects/FiresecOPCServer/FiresecOPCServer/App.xaml.cs
@@ -4,14 +4,19 @@ using System.ComponentModel;
 using FiresecClient;
 using System;
 using Common;
+using Infrastructure.Common;
 
 namespace FiresecOPCServer
 {
     public partial class App : Application
     {
+        private const string SignalId = "148996BE-2E10-40B3-9D8A-07CD57A2847F";
+        private const string WaitId = "9D2ADF91-2C69-493B-9809-C1F2851D4ECD";
+
         private void OnStartup(object sender, StartupEventArgs e)
         {
             AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+            using (new DoubleLaunchLocker(SignalId, WaitId, true))
             Bootstrapper.Run();
         }
 
