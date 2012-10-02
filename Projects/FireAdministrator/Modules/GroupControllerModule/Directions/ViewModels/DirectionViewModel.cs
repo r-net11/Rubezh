cc74e7bbbeda101c52443477c384b0b702785357
Vlad <no>
2012-10-03 01:56:14 +0400

    library fix

--- Projects/FireAdministrator/Modules/GroupControllerModule/Directions/ViewModels/DirectionViewModel.cs
+++ Projects/FireAdministrator/Modules/GroupControllerModule/Directions/ViewModels/DirectionViewModel.cs
@@ -6,130 +6,112 @@ using Infrastructure.Common.Windows.ViewModels;
 using XFiresecAPI;
 using Infrastructure.Common.Windows;
 using Infrastructure;
+using System.Windows.Documents;
+using System.Collections.Generic;
+using System;
 
 namespace GKModule.ViewModels
 {
     public class DirectionViewModel : BaseViewModel
     {
-		public XDirection Direction { get; set; }
+        public XDirection Direction { get; set; }
 
-		public DirectionViewModel(XDirection direction)
-		{
-			AddZoneCommand = new RelayCommand(OnAddZone, CanAdd);
-			RemoveZoneCommand = new RelayCommand(OnRemoveZone, CanRemove);
-			ChangeZonesCommand = new RelayCommand(OnChangeZones);
+        public DirectionViewModel(XDirection direction)
+        {
+            Direction = direction;
+            Zones = new ObservableCollection<ZoneViewModel>();
+            Devices = new ObservableCollection<DeviceViewModel>();
+            InitializeDirectionZones();
+            InitializeDirectionDevices();
+        }
 
-			Direction = direction;
+        void InitializeDirectionZones()
+        {
+            Zones.Clear();
+            foreach (var zone in Direction.Zones)
+            {
+                var zoneViewModel = new ZoneViewModel(zone);
+                Zones.Add(zoneViewModel);
+            }
+        }
 
-			Zones = new ObservableCollection<ZoneViewModel>();
-			SourceZones = new ObservableCollection<ZoneViewModel>();
+        void InitializeDirectionDevices()
+        {
+            Devices.Clear();
+            foreach (var directionDevice in Direction.DirectionDevices)
+            {
+                var deviceViewModel = new DeviceViewModel(directionDevice.Device, null);
+                Devices.Add(deviceViewModel);
+            }
+        }
 
-			foreach (var zone in XManager.DeviceConfiguration.Zones)
-			{
-				var zoneViewModel = new ZoneViewModel(zone);
-				if (Direction.ZoneUIDs.Contains(zone.UID))
-					Zones.Add(zoneViewModel);
-				else
-					SourceZones.Add(zoneViewModel);
-			}
+        public void Update()
+        {
+            OnPropertyChanged("Direction");
+        }
 
-			SelectedZone = Zones.FirstOrDefault();
-			SelectedSourceZone = SourceZones.FirstOrDefault();
+        public ObservableCollection<ZoneViewModel> Zones { get; private set; }
 
-			InitializeDirectionZones();
-		}
+        ZoneViewModel _selectedZone;
+        public ZoneViewModel SelectedZone
+        {
+            get { return _selectedZone; }
+            set
+            {
+                _selectedZone = value;
+                OnPropertyChanged("SelectedZone");
+            }
+        }
 
-		void InitializeDirectionZones()
-		{
-			Zones.Clear();
-			foreach (var zone in Direction.Zones)
-			{
-				var zoneViewModel = new ZoneViewModel(zone);
-				Zones.Add(zoneViewModel);
-			}
-			SelectedZone = Zones.FirstOrDefault();
-		}
+        public ObservableCollection<DeviceViewModel> Devices { get; private set; }
 
-		void InitializeDirectionDevices()
-		{
+        DeviceViewModel _selectedDevice;
+        public DeviceViewModel SelectedDevice
+        {
+            get { return _selectedDevice; }
+            set
+            {
+                _selectedDevice = value;
+                OnPropertyChanged("SelectedDevice");
+            }
+        }
 
-		}
+        public void ChangeZones()
+        {
+            var zonesSelectationViewModel = new ZonesSelectationViewModel(Direction.ZoneUIDs);
+            if (DialogService.ShowModalWindow(zonesSelectationViewModel))
+            {
+                XManager.ChangeDirectionZones(Direction, zonesSelectationViewModel.Zones);
+                InitializeDirectionZones();
+                ServiceFactory.SaveService.XDevicesChanged = true;
+            }
+        }
 
-		public void Update()
-		{
-			OnPropertyChanged("Direction");
-		}
-
-		public ObservableCollection<ZoneViewModel> Zones { get; private set; }
-
-		ZoneViewModel _selectedZone;
-		public ZoneViewModel SelectedZone
-		{
-			get { return _selectedZone; }
-			set
-			{
-				_selectedZone = value;
-				OnPropertyChanged("SelectedZone");
-			}
-		}
-
-		public ObservableCollection<ZoneViewModel> SourceZones { get; private set; }
-
-		ZoneViewModel _selectedSourceZone;
-		public ZoneViewModel SelectedSourceZone
-		{
-			get { return _selectedSourceZone; }
-			set
-			{
-				_selectedSourceZone = value;
-				OnPropertyChanged("SelectedSourceZone");
-			}
-		}
-
-		public RelayCommand ChangeZonesCommand { get; private set; }
-		void OnChangeZones()
-		{
-			var zonesSelectationViewModel = new ZonesSelectationViewModel(Direction.ZoneUIDs);
-			if (DialogService.ShowModalWindow(zonesSelectationViewModel))
-			{
-				XManager.ChangeDirectionZones(Direction, zonesSelectationViewModel.Zones);
-				InitializeDirectionZones();
-				ServiceFactory.SaveService.XDevicesChanged = true;
-			}
-		}
-
-		public RelayCommand AddZoneCommand { get; private set; }
-		void OnAddZone()
-		{
-			int oldIndex = SourceZones.IndexOf(SelectedSourceZone);
-
-			Direction.ZoneUIDs.Add(SelectedSourceZone.XZone.UID);
-			Zones.Add(SelectedSourceZone);
-			SourceZones.Remove(SelectedSourceZone);
-
-			if (SourceZones.Count > 0)
-				SelectedSourceZone = SourceZones[System.Math.Min(oldIndex, SourceZones.Count - 1)];
-		}
-		bool CanAdd()
-		{
-			return SelectedSourceZone != null;
-		}
-
-		public RelayCommand RemoveZoneCommand { get; private set; }
-		void OnRemoveZone()
-		{
-			int oldIndex = Zones.IndexOf(SelectedZone);
-
-			Direction.ZoneUIDs.Remove(SelectedZone.XZone.UID);
-			SourceZones.Add(SelectedZone);
-			Zones.Remove(SelectedZone);
-
-			if (Zones.Count > 0)
-				SelectedZone = Zones[System.Math.Min(oldIndex, Zones.Count - 1)];
-		}
-		bool CanRemove()
-		{
-			return SelectedZone != null;
-		}
-	}
+        public void ChangeDevices()
+        {
+            var deviceUIDs = new List<Guid>();
+            foreach (var directionDevice in Direction.DirectionDevices)
+            {
+                deviceUIDs.Add(directionDevice.DeviceUID);
+            }
+            var devicesSelectationViewModel = new DevicesSelectationViewModel(deviceUIDs);
+            if (DialogService.ShowModalWindow(devicesSelectationViewModel))
+            {
+                Direction.DirectionDevices = new List<DirectionDevice>();
+                foreach (var deviceUID in devicesSelectationViewModel.DevicesList)
+                {
+                    var device = XManager.DeviceConfiguration.Devices.FirstOrDefault(x => x.UID == deviceUID);
+                    var directionDevice = new DirectionDevice()
+                    {
+                        Device = device,
+                        DeviceUID = deviceUID,
+                        StateType = XStateType.TurningOn
+                    };
+                    Direction.DirectionDevices.Add(directionDevice);
+                }
+                InitializeDirectionDevices();
+                ServiceFactory.SaveService.XDevicesChanged = true;
+            }
+        }
+    }
 }
