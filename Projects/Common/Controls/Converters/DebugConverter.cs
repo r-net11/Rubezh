46af5833ae386d25d9c846bf30e38c627815cc89
Vladimir <Sorokonenko@gmail.com>
2012-10-09 03:33:19 +0400

    Bug fixing

---
+++ Projects/Common/Controls/Converters/DebugConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Controls.Converters
+{
+	public class DebugConverter : IValueConverter
+	{
+		public Object Convert(Object value, Type targetType, Object parameter, System.Globalization.CultureInfo culture)
+		{
+			return value;
+		}
+
+		public Object ConvertBack(Object value, Type targetType, Object parameter, System.Globalization.CultureInfo culture)
+		{
+			return value;
+		}
+	}
+}
