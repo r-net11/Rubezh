66b816e82adf43e4ff037743e2990c9d50908b1c
Роман Казаков <fx-romario@rambler.ru>
2012-10-23 10:26:37 +0400

    Logs %date%

--- Projects/Installers/FiresecInstaller/FiresecInstaller/Data/CopyLogs.bat
+++ Projects/Installers/FiresecInstaller/FiresecInstaller/Data/CopyLogs.bat
@@ -1,12 +1,12 @@
 @echo off
-set admPath="%cd%\..\FireAdministrator\Logs"
-set destAdm="%cd%\Logs\FireAdministrator\"
-set monPath="%cd%\..\FireMonitor\Logs"
-set destMon="%cd%\Logs\FireMonitor\"
-set servicePath="%cd%\..\FiresecService\Logs"
-set destService="%cd%\Logs\FiresecService\"
-set servicePath="%cd%\..\FiresecOPCServer\Logs"
-set destService="%cd%\Logs\FiresecOPCServer\"
+set admPath="%cd%\..\..\..\..\FireAdministrator\bin\Debug\Logs"
+set destAdm="%cd%\Logs %date%\FireAdministrator\"
+set monPath="%cd%\..\..\..\..\FireMonitor\bin\Debug\Logs"
+set destMon="%cd%\Logs %date%\FireMonitor\"
+set servicePath="%cd%\..\..\..\..\FiresecService\bin\Debug\Logs"
+set destService="%cd%\Logs %date%\FiresecService\"
+set servicePath="%cd%\..\..\..\..\FiresecOPCServer\bin\Debug\Logs"
+set destService="%cd%\Logs %date%\FiresecOPCServer\"
 echo %admPath%
 xcopy %admPath% %destAdm% /E /Q /K /Y
 xcopy %monPath% %destMon% /E /Q /K /Y
