4cc77b44f0c6a55f59ad237c1ef52ed0905b55f5
Vladimir <Sorokonenko@gmail.com>
2012-11-14 22:13:37 +0400

    Trace improvement

--- Projects/FireMonitor/FireMonitor/App.xaml.cs
+++ Projects/FireMonitor/FireMonitor/App.xaml.cs
@@ -16,130 +16,131 @@ using Microsoft.Win32;
 
 namespace FireMonitor
 {
-    public partial class App : Application
-    {
-        const string SignalId = "{B8150ECC-9433-4535-89AA-5BF6EF631575}";
-        const string WaitId = "{358D5240-9A07-4134-9EAF-8D7A54BCA81F}";
-        Bootstrapper _bootstrapper;
-        bool bootstrapperLoaded = false;
-        public static bool IsClosingOnException = false;
-        public static string Login;
-        public static string Password;
+	public partial class App : Application
+	{
+		const string SignalId = "{B8150ECC-9433-4535-89AA-5BF6EF631575}";
+		const string WaitId = "{358D5240-9A07-4134-9EAF-8D7A54BCA81F}";
+		Bootstrapper _bootstrapper;
+		bool bootstrapperLoaded = false;
+		public static bool IsClosingOnException = false;
+		public static string Login;
+		public static string Password;
 
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            try
-            {
-                var path = System.Reflection.Assembly.GetExecutingAssembly();
-                RegistryKey saveKey = Registry.LocalMachine.CreateSubKey("software\\rubezh\\Firesec-2");
-                saveKey.SetValue("FireMonitorPath", path.Location);
-                var isAutoConnect = saveKey.GetValue("isAutoConnect");
-                if (isAutoConnect != null)
-                    if (isAutoConnect.Equals("True"))
-                    {
-                        AppSettingsManager.AutoConnect = true;
-                        saveKey.SetValue("isAutoConnect", false);
-                    }
-                saveKey.Close();
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex, "App.OnStartup");
-            }
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+			try
+			{
+				var path = System.Reflection.Assembly.GetExecutingAssembly();
+				RegistryKey saveKey = Registry.LocalMachine.CreateSubKey("software\\rubezh\\Firesec-2");
+				saveKey.SetValue("FireMonitorPath", path.Location);
+				var isAutoConnect = saveKey.GetValue("isAutoConnect");
+				if (isAutoConnect != null)
+					if (isAutoConnect.Equals("True"))
+					{
+						AppSettingsManager.AutoConnect = true;
+						saveKey.SetValue("isAutoConnect", false);
+					}
+				saveKey.Close();
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(ex, "App.OnStartup");
+			}
 
-            InitializeCommandLineArguments(e.Args);
-            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
-            ApplicationService.Closing += new System.ComponentModel.CancelEventHandler(ApplicationService_Closing);
-            ThemeHelper.LoadThemeFromRegister();
-            ServerLoadHelper.Load();
+			InitializeCommandLineArguments(e.Args);
+			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+			ApplicationService.Closing += new System.ComponentModel.CancelEventHandler(ApplicationService_Closing);
+			ThemeHelper.LoadThemeFromRegister();
+			ServerLoadHelper.Load();
 #if DEBUG
-			//BindingErrorListener.Listen(m => { MessageBox.Show(m); });
+			bool trace = true;
+			BindingErrorListener.Listen(m => { if (trace) MessageBox.Show(m); });
 #endif
-            _bootstrapper = new Bootstrapper();
-            using (new DoubleLaunchLocker(SignalId, WaitId, true))
-            {
-                _bootstrapper.Initialize();
+			_bootstrapper = new Bootstrapper();
+			using (new DoubleLaunchLocker(SignalId, WaitId, true))
+			{
+				_bootstrapper.Initialize();
 
-                if (Process.GetCurrentProcess().ProcessName != "FireMonitor.vshost")
-                {
-                    RegistryKey saveKey = Registry.LocalMachine.CreateSubKey("software\\rubezh\\Firesec-2");
-                    saveKey.SetValue("isException", true);
-                }
-            }
-            bootstrapperLoaded = true;
-        }
+				if (Process.GetCurrentProcess().ProcessName != "FireMonitor.vshost")
+				{
+					RegistryKey saveKey = Registry.LocalMachine.CreateSubKey("software\\rubezh\\Firesec-2");
+					saveKey.SetValue("isException", true);
+				}
+			}
+			bootstrapperLoaded = true;
+		}
 
-        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            IsClosingOnException = true;
-            Logger.Error(e.ExceptionObject as Exception, "App.CurrentDomain_UnhandledException");
+		void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+		{
+			IsClosingOnException = true;
+			Logger.Error(e.ExceptionObject as Exception, "App.CurrentDomain_UnhandledException");
 
-            if (bootstrapperLoaded)
-            {
+			if (bootstrapperLoaded)
+			{
 #if RELEASE
                 Restart();
 #endif
-                Application.Current.MainWindow.Close();
-                Application.Current.Shutdown();
-            }
-            else
-            {
-                MessageBoxService.ShowError("Во время загрузки программы произошло исключение. Приложение будет закрыто");
-            }
-        }
+				Application.Current.MainWindow.Close();
+				Application.Current.Shutdown();
+			}
+			else
+			{
+				MessageBoxService.ShowError("Во время загрузки программы произошло исключение. Приложение будет закрыто");
+			}
+		}
 
-        void ApplicationService_Closing(object sender, CancelEventArgs e)
-        {
-            GKDBHelper.AddMessage("Выход пользователя из системы");
-            foreach (var module in ApplicationService.Modules)
-                module.Dispose();
-            AlarmPlayerHelper.Dispose();
-            ClientSettings.SaveSettings();
-            FiresecManager.Disconnect();
-            if (RegistryHelper.IsIntegrated)
-            {
-                if (!IsClosingOnException)
-                    RegistryHelper.ShutDown();
-            }
-        }
+		void ApplicationService_Closing(object sender, CancelEventArgs e)
+		{
+			GKDBHelper.AddMessage("Выход пользователя из системы");
+			foreach (var module in ApplicationService.Modules)
+				module.Dispose();
+			AlarmPlayerHelper.Dispose();
+			ClientSettings.SaveSettings();
+			FiresecManager.Disconnect();
+			if (RegistryHelper.IsIntegrated)
+			{
+				if (!IsClosingOnException)
+					RegistryHelper.ShutDown();
+			}
+		}
 
-        void Restart()
-        {
-            string commandLineArguments = null;
-            if (Login != null && Password != null)
-            {
-                commandLineArguments = "login='" + Login + "' password='" + Password + "'";
-            }
-            var processStartInfo = new ProcessStartInfo()
-            {
-                FileName = Application.ResourceAssembly.Location,
-                Arguments = commandLineArguments
-            };
-            System.Diagnostics.Process.Start(processStartInfo);
-        }
+		void Restart()
+		{
+			string commandLineArguments = null;
+			if (Login != null && Password != null)
+			{
+				commandLineArguments = "login='" + Login + "' password='" + Password + "'";
+			}
+			var processStartInfo = new ProcessStartInfo()
+			{
+				FileName = Application.ResourceAssembly.Location,
+				Arguments = commandLineArguments
+			};
+			System.Diagnostics.Process.Start(processStartInfo);
+		}
 
-        void InitializeCommandLineArguments(string[] args)
-        {
-            if (args != null)
-            {
-                if (args.Count() >= 2)
-                {
-                    foreach (var arg in args)
-                    {
-                        if(arg.StartsWith("login='") && arg.EndsWith("'"))
-                        {
-                            Login = arg.Replace("login='", "");
-                            Login = Login.Replace("'", "");
-                        }
-                        if (arg.StartsWith("password='") && arg.EndsWith("'"))
-                        {
-                            Password = arg.Replace("password='", "");
-                            Password = Password.Replace("'", "");
-                        }
-                    }
-                }
-            }
-        }
-    }
+		void InitializeCommandLineArguments(string[] args)
+		{
+			if (args != null)
+			{
+				if (args.Count() >= 2)
+				{
+					foreach (var arg in args)
+					{
+						if (arg.StartsWith("login='") && arg.EndsWith("'"))
+						{
+							Login = arg.Replace("login='", "");
+							Login = Login.Replace("'", "");
+						}
+						if (arg.StartsWith("password='") && arg.EndsWith("'"))
+						{
+							Password = arg.Replace("password='", "");
+							Password = Password.Replace("'", "");
+						}
+					}
+				}
+			}
+		}
+	}
 }
