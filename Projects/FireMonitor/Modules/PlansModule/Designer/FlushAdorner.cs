d6fb4ef429b9a3f5526472144588b2b45ffb04b0
Vlad <no>
2012-11-24 18:35:19 +0400

    merge fix

---
+++ Projects/FireMonitor/Modules/PlansModule/Designer/FlushAdorner.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using Infrustructure.Plans.Presenter;
+using PlansModule.ViewModels;
+using System.Windows.Shapes;
+
+namespace PlansModule.Designer
+{
+	public class FlushAdorner : Adorner
+	{
+		private const int Length = 2;
+		private VisualCollection _visuals;
+		private DispatcherTimer _timer;
+		private ContentPresenter _contentPresenter;
+		private FlushViewModel _flushControl;
+		protected Canvas Canvas { get; private set; }
+
+		protected override int VisualChildrenCount
+		{
+			get { return _visuals.Count; }
+		}
+		protected override Size ArrangeOverride(Size arrangeBounds)
+		{
+			_contentPresenter.Arrange(new Rect(arrangeBounds));
+			return arrangeBounds;
+		}
+		protected override Visual GetVisualChild(int index)
+		{
+			return _visuals[index];
+		}
+
+		public FlushAdorner(Canvas canvas)
+			: base(canvas)
+		{
+			Canvas = canvas;
+			_flushControl = new FlushViewModel();
+			_contentPresenter = new ContentPresenter();
+			_contentPresenter.Content = _flushControl;
+			_visuals = new VisualCollection(this);
+			_visuals.Add(_contentPresenter);
+			_timer = new DispatcherTimer();
+			_timer.Interval = TimeSpan.FromSeconds(Length);
+			_timer.Tick += (s, e) => Hide();
+		}
+
+		public void Show(PresenterItem presenterItem)
+		{
+			if (_timer.IsEnabled || Parent != null)
+				Hide();
+			_flushControl.SetPresenterItem(presenterItem);
+			AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(Canvas);
+			if (adornerLayer != null)
+				adornerLayer.Add(this);
+			_timer.Start();
+		}
+		public void Hide()
+		{
+			_timer.Stop();
+			AdornerLayer adornerLayer = Parent as AdornerLayer;
+			if (adornerLayer != null)
+				adornerLayer.Remove(this);
+		}
+
+		public void UpdateZoom()
+		{
+		}
+	}
+}
---
+++ Projects/FireMonitor/Modules/PlansModule/ViewModels/FlushViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Infrastructure.Common.Windows.ViewModels;
+using Infrustructure.Plans.Presenter;
+using System.Windows;
+
+namespace PlansModule.ViewModels
+{
+	public class FlushViewModel : BaseViewModel
+	{
+		public PresenterItem PresenterItem { get; private set; }
+		public double CenterX { get; private set; }
+		public double CenterY { get; private set; }
+		public double Width { get; private set; }
+		public double Height { get; private set; }
+		public double Left { get; private set; }
+		public double Top { get; private set; }
+
+		public void SetPresenterItem(PresenterItem presenterItem)
+		{
+			PresenterItem = presenterItem;
+			var position = presenterItem.Element.Position;
+			CenterX = position.X;
+			CenterY = position.Y;
+			var rect = presenterItem.Element.GetRectangle();
+			Width = rect.Width;
+			Height = rect.Height;
+			Left = rect.Left;
+			Top = rect.Top;
+			OnPropertyChanged(() => PresenterItem);
+			OnPropertyChanged(() => CenterX);
+			OnPropertyChanged(() => CenterY);
+			OnPropertyChanged(() => Width);
+			OnPropertyChanged(() => Height);
+			OnPropertyChanged(() => Left);
+			OnPropertyChanged(() => Top);
+		}
+	}
+}
