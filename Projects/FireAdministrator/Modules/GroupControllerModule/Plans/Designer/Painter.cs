c5ce8be48e9d9efb8dabfaa0e19b156885885e0b
Maxim Obycheff <obycheff.maxim@yandex.ru>
2012-12-11 15:25:56 +0400

    Balloontooltip resource dictionary

--- Projects/FireAdministrator/Modules/GroupControllerModule/Plans/Designer/Painter.cs
+++ Projects/FireAdministrator/Modules/GroupControllerModule/Plans/Designer/Painter.cs
@@ -9,7 +9,7 @@ namespace GKModule.Plans.Designer
 {
 	public class Painter : IPainter
 	{
-		public FrameworkElement Draw(ElementBase element)
+		public System.Windows.Media.Visual Draw(ElementBase element)
 		{
 			var device = Helper.GetXDevice((ElementXDevice)element);
 			Guid driverUID = device == null ? Guid.Empty : device.DriverUID;
