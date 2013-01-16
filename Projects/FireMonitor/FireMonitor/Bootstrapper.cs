4c48b2c54a099475d298d9a3f4808c2bb9fd929e
Vladimir <Sorokonenko@gmail.com>
2013-01-17 02:36:41 +0400

    Fix

--- Projects/FireMonitor/FireMonitor/Bootstrapper.cs
+++ Projects/FireMonitor/FireMonitor/Bootstrapper.cs
@@ -14,94 +14,94 @@ using Microsoft.Win32;
 
 namespace FireMonitor
 {
-    public class Bootstrapper : BaseBootstrapper
-    {
-        public void Initialize()
-        {
-            LoadingErrorManager.Clear();
-            AppConfigHelper.InitializeAppSettings();
-            VideoService.Initialize(ServiceFactory.AppSettings.LibVlcDllsPath);
-            ServiceFactory.Initialize(new LayoutService(), new SecurityService());
-            ServiceFactory.ResourceService.AddResource(new ResourceDescription(GetType().Assembly, "DataTemplates/Dictionary.xaml"));
+	public class Bootstrapper : BaseBootstrapper
+	{
+		public void Initialize()
+		{
+			LoadingErrorManager.Clear();
+			AppConfigHelper.InitializeAppSettings();
+			VideoService.Initialize(ServiceFactory.AppSettings.LibVlcDllsPath);
+			ServiceFactory.Initialize(new LayoutService(), new SecurityService());
+			ServiceFactory.ResourceService.AddResource(new ResourceDescription(GetType().Assembly, "DataTemplates/Dictionary.xaml"));
 
-            if (ServiceFactory.LoginService.ExecuteConnect(App.Login, App.Password))
-            {
-                App.Login = ServiceFactory.LoginService.Login;
-                App.Password = ServiceFactory.LoginService.Password;
-                try
-                {
-                    CreateModules();
+			if (ServiceFactory.LoginService.ExecuteConnect(App.Login, App.Password))
+			{
+				App.Login = ServiceFactory.LoginService.Login;
+				App.Password = ServiceFactory.LoginService.Password;
+				try
+				{
+					CreateModules();
 
-                    LoadingService.Show("Чтение конфигурации", 15);
-                    LoadingService.AddCount(GetModuleCount());
+					LoadingService.Show("Чтение конфигурации", 15);
+					LoadingService.AddCount(GetModuleCount());
 
-					//if (!App.IsMulticlient)
-					//{
-					//    LoadingService.DoStep("Синхронизация файлов");
-					//    FiresecManager.UpdateFiles();
-					//}
+					if (!App.IsMulticlient)
+					{
+						LoadingService.DoStep("Синхронизация файлов");
+						FiresecManager.UpdateFiles();
+					}
 
 					LoadingService.DoStep("Загрузка конфигурации с сервера");
 					FiresecManager.GetConfiguration();
 
-                    BeforeInitialize(true);
+					BeforeInitialize(true);
 
-					//LoadingService.DoStep("Старт полинга сервера");
-					//FiresecManager.StartPoll(false);
+					LoadingService.DoStep("Старт полинга сервера");
+					FiresecManager.StartPoll(false);
 
-                    LoadingService.DoStep("Проверка прав пользователя");
-                    if (FiresecManager.CheckPermission(PermissionType.Oper_Login))
-                    {
-                        LoadingService.DoStep("Загрузка клиентских настроек");
-                        ClientSettings.LoadSettings();
+					LoadingService.DoStep("Проверка прав пользователя");
+					if (FiresecManager.CheckPermission(PermissionType.Oper_Login))
+					{
+						LoadingService.DoStep("Загрузка клиентских настроек");
+						ClientSettings.LoadSettings();
 
-                        var shell = new MonitorShellViewModel();
-                        ((LayoutService)ServiceFactory.Layout).SetToolbarViewModel((ToolbarViewModel)shell.Toolbar);
-                        ((LayoutService)ServiceFactory.Layout).AddToolbarItem(new SoundViewModel());
-                        RunShell(shell);
-                        ((LayoutService)ServiceFactory.Layout).AddToolbarItem(new UserViewModel());
-                        ((LayoutService)ServiceFactory.Layout).AddToolbarItem(new AutoActivationViewModel());
+						var shell = new MonitorShellViewModel();
+						((LayoutService)ServiceFactory.Layout).SetToolbarViewModel((ToolbarViewModel)shell.Toolbar);
+						((LayoutService)ServiceFactory.Layout).AddToolbarItem(new SoundViewModel());
+						RunShell(shell);
+						((LayoutService)ServiceFactory.Layout).AddToolbarItem(new UserViewModel());
+						((LayoutService)ServiceFactory.Layout).AddToolbarItem(new AutoActivationViewModel());
 
-                        SafeFiresecService.ConfigurationChangedEvent += () => { ApplicationService.Invoke(OnConfigurationChanged); };
-                    }
-                    else
-                    {
-                        MessageBoxService.Show("Нет прав на работу с программой");
-                        FiresecManager.Disconnect();
-                    }
-                    LoadingService.Close();
+						SafeFiresecService.ConfigurationChangedEvent += () => { ApplicationService.Invoke(OnConfigurationChanged); };
+					}
+					else
+					{
+						MessageBoxService.Show("Нет прав на работу с программой");
+						FiresecManager.Disconnect();
+					}
+					LoadingService.Close();
 
-                    AterInitialize();
+					AterInitialize();
 
-                    //MutexHelper.KeepAlive();
-                    ProgressWatcher.Run();
-                    if (Process.GetCurrentProcess().ProcessName != "FireMonitor.vshost")
-                    {
-                        RegistryKey saveKey = Registry.LocalMachine.CreateSubKey("software\\rubezh\\Firesec-2");
-                        saveKey.SetValue("isException", true);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Logger.Error(e, "Bootstrapper.InitializeFs");
-                    MessageBoxService.ShowException(e);
-                    if (Application.Current != null)
-                        Application.Current.Shutdown();
-                    return;
-                }
-            }
-            else
-            {
-                if (Application.Current != null)
-                    Application.Current.Shutdown();
-                return;
-            }
-        }
+					//MutexHelper.KeepAlive();
+					ProgressWatcher.Run();
+					if (Process.GetCurrentProcess().ProcessName != "FireMonitor.vshost")
+					{
+						RegistryKey saveKey = Registry.LocalMachine.CreateSubKey("software\\rubezh\\Firesec-2");
+						saveKey.SetValue("isException", true);
+					}
+				}
+				catch (Exception e)
+				{
+					Logger.Error(e, "Bootstrapper.InitializeFs");
+					MessageBoxService.ShowException(e);
+					if (Application.Current != null)
+						Application.Current.Shutdown();
+					return;
+				}
+			}
+			else
+			{
+				if (Application.Current != null)
+					Application.Current.Shutdown();
+				return;
+			}
+		}
 
-        bool IsRestarting = false;
+		bool IsRestarting = false;
 
-        private void OnConfigurationChanged()
-        {
+		private void OnConfigurationChanged()
+		{
 			try
 			{
 				if (IsRestarting)
@@ -134,18 +134,18 @@ namespace FireMonitor
 			{
 				Logger.Error(e, "Bootstrapper.OnConfigurationChanged");
 			}
-            finally
-            {
-                IsRestarting = false;
-                FiresecManager.FiresecService.SuspendPoll = false;
-                FiresecManager.FSAgent.SuspendPoll = false;
-            }
-        }
+			finally
+			{
+				IsRestarting = false;
+				FiresecManager.FiresecService.SuspendPoll = false;
+				FiresecManager.FSAgent.SuspendPoll = false;
+			}
+		}
 
-        private void CloseOnException(string message)
-        {
-            MessageBoxService.ShowError(message);
-            Application.Current.Shutdown();
-        }
-    }
+		private void CloseOnException(string message)
+		{
+			MessageBoxService.ShowError(message);
+			Application.Current.Shutdown();
+		}
+	}
 }
