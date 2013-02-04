a6da9f5b0da5aee47b33eea13eecaac7813e1f73
testing room 2 <testing_room_2@mail.ru>
2013-02-04 16:20:06 +0400

    Mail Sender

--- Projects/FireMonitor/FireMonitor/Notifier.cs
+++ Projects/FireMonitor/FireMonitor/Notifier.cs
@@ -6,6 +6,7 @@ using FiresecClient;
 using Infrastructure;
 using Infrastructure.Common.Mail;
 using Infrastructure.Events;
+using System.Linq;
 
 namespace FireMonitor.ViewModels
 {
@@ -15,43 +16,61 @@ namespace FireMonitor.ViewModels
 		{
 			ServiceFactory.Events.GetEvent<ZoneStateChangedEvent>().Unsubscribe(OnZonesStateChanged);
 			ServiceFactory.Events.GetEvent<ZoneStateChangedEvent>().Subscribe(OnZonesStateChanged);
-
 			OnZonesStateChanged(Guid.Empty);
-
-			foreach (var email in FiresecManager.SystemConfiguration.EmailData.Emails)
-			{
-				Trace.WriteLine(email.Address + " " + MailHelper.PresentStates(email));
-			}
 		}
 
 		public static void OnZonesStateChanged(System.Guid guid)
 		{
+			foreach (var email in FiresecManager.SystemConfiguration.EmailData.Emails)
+			{
+				if (email.IsActivated)
+					CheckForDeactivate(email);
+				else
+					CheckForNotify(email);
+			}
+		}
+
+		static void CheckForNotify(Email email)
+		{
 			foreach (var zone in FiresecManager.Zones)
 			{
-				foreach (var email in FiresecManager.SystemConfiguration.EmailData.Emails)
+				if (email.Zones.Contains(zone.UID) && 
+					email.States.Contains(zone.ZoneState.StateType))
 				{
-					if (email.Zones.Contains(zone.UID))
-					{
-						if (!email.States.Contains(zone.ZoneState.StateType))
-							email.IsActivated = false;
-						else if (!email.IsActivated)
-						{
-							Notify(zone, email);
-							email.IsActivated = true;
-						}
-					}
+					Notify(zone, email);
+					email.IsActivated = true;
+					break;
 				}
 			}
 		}
 
-		private static void Notify(Zone zone, Email email)
+		static void CheckForDeactivate(Email email)
+		{
+			bool shouldDeactivate = true;
+			foreach (var zoneGuid in email.Zones)
+			{
+				var zone = FiresecManager.Zones.FirstOrDefault(x => x.UID == zoneGuid);
+				if (email.States.Contains(zone.ZoneState.StateType))
+				{
+					shouldDeactivate = false;
+					break;
+				}
+			}
+			if (shouldDeactivate)
+			{
+				Trace.WriteLine("Email Reset");
+				email.IsActivated = false;
+			}
+		}
+
+		static void Notify(Zone zone, Email email)
 		{
 			string message = " Изменение состояния зоны " +
 				zone.PresentationName +
 				" на состояние " +
 				zone.ZoneState.StateType.ToDescription();
-			//MailHelper.Send(FiresecManager.SystemConfiguration.EmailData.EmailSettings, email.Address, message, email.MessageTitle);
-			Trace.WriteLine(message);
+			MailHelper.Send(FiresecManager.SystemConfiguration.EmailData.EmailSettings, email.Address, message, email.MessageTitle);
+			//Trace.WriteLine(message);
 		}
 	}
 }
