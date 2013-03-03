ac64138ae3055e24ea855d810c27392eecceab94
Vlad <no>
2013-03-03 14:23:02 +0400

    mc fix

--- Projects/ManagementConsole/ManagementConsole/Views/ManagementConsoleView.xaml.cs
+++ Projects/ManagementConsole/ManagementConsole/Views/ManagementConsoleView.xaml.cs
@@ -1,12 +1,27 @@
-﻿namespace ManagementConsole
+﻿using Infrastructure.Common.Windows;
+using System.Windows.Forms;
+namespace ManagementConsole
 {
 	public partial class ManagementConsoleView
 	{
+		ManagementConsoleViewModel ManagementConsoleViewModel;
+
 		public ManagementConsoleView()
 		{
 			InitializeComponent();
-			var managementConsoleViewModel = new ManagementConsoleViewModel();
-			DataContext = managementConsoleViewModel;
+			ManagementConsoleViewModel = new ManagementConsoleViewModel();
+			DataContext = ManagementConsoleViewModel;
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			if (ManagementConsoleViewModel.HasChanges)
+			{
+				if (MessageBox.Show("Сохранить изменения?", "Сохранить изменения?", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+				{
+					ManagementConsoleViewModel.GlobalSettingsViewModel.SaveCommand.Execute();
+				}
+			}
 		}
 	}
 }
