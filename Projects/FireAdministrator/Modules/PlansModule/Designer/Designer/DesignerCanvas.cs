163a5caf0548778c4d885c80ea894b85cbeb27e3
Vladimir S <Sorokonenko@gmail.com>
2013-03-25 12:23:24 +0400

    Fix

--- Projects/FireAdministrator/Modules/PlansModule/Designer/Designer/DesignerCanvas.cs
+++ Projects/FireAdministrator/Modules/PlansModule/Designer/Designer/DesignerCanvas.cs
@@ -231,7 +231,7 @@ namespace PlansModule.Designer
 
 		public void UpdateZoom()
 		{
-			Margin = new Thickness(25 / (Zoom > 0 ? Zoom : 1));
+			Margin = new Thickness(25 / (!double.IsNaN(Zoom) && Zoom > 0 ? Zoom : 1));
 			ZoomChanged();
 			Toolbox.UpdateZoom();
 			foreach (DesignerItem designerItem in Items)
