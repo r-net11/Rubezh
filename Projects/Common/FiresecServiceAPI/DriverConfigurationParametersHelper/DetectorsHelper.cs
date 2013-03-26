b40788c86c23855276b228f9873705e51c04667f
Alexander Sevastyanov <sevastyanovaa@rubezh.ru>
2013-03-26 11:27:17 +0400

    fix

--- Projects/Common/FiresecServiceAPI/DriverConfigurationParametersHelper/DetectorsHelper.cs
+++ Projects/Common/FiresecServiceAPI/DriverConfigurationParametersHelper/DetectorsHelper.cs
@@ -8,27 +8,25 @@ namespace FiresecAPI.Models
 		{
 			foreach (var driver in drivers)
 			{
-				if (driver.DriverType == DriverType.SmokeDetector)
+				switch (driver.DriverType)
 				{
-					driver.HasConfigurationProperties = true;
-					ConfigurationDriverHelper.AddIntProprety(driver, 0x84, "Порог срабатывания по дыму, 0.01 дб/м", "AU_Smoke", 0, 18, 5, 20);
-					//return;
+					case DriverType.SmokeDetector:
+						driver.HasConfigurationProperties = true;
+						ConfigurationDriverHelper.AddIntProprety(driver, 0x84, "Порог срабатывания по дыму, 0.01 дб/м", "AU_Smoke", 0, 18, 5, 20);
+						break;
+
+					case DriverType.HeatDetector:
+						driver.HasConfigurationProperties = true;
+						ConfigurationDriverHelper.AddIntProprety(driver, 0x8B, "Порог срабатывания по температуре, C", "AU_Temperature", 0, 0, 0, 85);
+						break;
+
+					case DriverType.CombinedDetector:
+						driver.HasConfigurationProperties = true;
+						ConfigurationDriverHelper.AddIntProprety(driver, 0x84, "Порог срабатывания по дыму, 0.01 дб/м", "AU_Smoke", 0, 18, 5, 20);
+						ConfigurationDriverHelper.AddIntProprety(driver, 0x8B, "Порог срабатывания по температуре, C", "AU_Temperature", 0, 0, 0, 85);
+						//ConfigurationDriverHelper.AddIntProprety(driver, 0x8C, "Порог срабатывания по градиенту температуры", 0, 0, 0, 255);
+						break;
 				}
-				if (driver.DriverType == DriverType.HeatDetector)
-				{
-					driver.HasConfigurationProperties = true;
-					ConfigurationDriverHelper.AddIntProprety(driver, 0x8B, "Порог срабатывания по температуре, C", "AU_Temperature", 0, 0, 0, 85);
-					//return;
-				}
-				if (driver.DriverType == DriverType.CombinedDetector)
-				{
-					driver.HasConfigurationProperties = true;
-					ConfigurationDriverHelper.AddIntProprety(driver, 0x84, "Порог срабатывания по дыму, 0.01 дб/м", "AU_Smoke", 0, 18, 5, 20);
-					ConfigurationDriverHelper.AddIntProprety(driver, 0x8B, "Порог срабатывания по температуре, C", "AU_Temperature", 0, 0, 0, 85);
-					//return;
-					//ConfigurationDriverHelper.AddIntProprety(driver, 0x8C, "Порог срабатывания по градиенту температуры", 0, 0, 0, 255);
-				}
-				ConfigurationDriverHelper.AddIntProprety(driver, 0x80, "Слово состояние", "AU_State", 0, 0, 0, 255);
 			}
 		}
 	}
