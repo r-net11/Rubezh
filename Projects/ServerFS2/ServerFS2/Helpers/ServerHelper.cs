50fc18832b9c339f1479ef6176b0172e6c1f00b3
Maxim Obycheff <obycheff.maxim@yandex.ru>
2013-05-13 15:17:17 +0400

    fs2

--- Projects/ServerFS2/ServerFS2/Helpers/ServerHelper.cs
+++ Projects/ServerFS2/ServerFS2/Helpers/ServerHelper.cs
@@ -9,11 +9,11 @@ using Device = FiresecAPI.Models.Device;
 namespace ServerFS2
 {
 	public static partial class ServerHelper
-    {
+	{
 		public static event Action<int, int, string> Progress;
 		public static List<Driver> Drivers;
-        static readonly object Locker = new object();
-        static readonly UsbRunner UsbRunner;
+		static readonly object Locker = new object();
+		static readonly UsbRunner UsbRunner;
 		static int _usbRequestNo;
 		static ServerHelper()
 		{
@@ -28,48 +28,48 @@ namespace ServerFS2
 			}
 			catch { }
 		}
-        public static void Initialize()
-        {
-            ;
-        }
-        public static OperationResult<List<Response>> SendCode(List<List<byte>> bytesList, int maxDelay = 1000, int maxTimeout = 1000, bool IsWrite = false)
-        {
-            return UsbRunner.AddRequest(bytesList, maxDelay, maxTimeout, IsWrite);
-        }
-        public static OperationResult<List<Response>> SendCode(List<byte> bytes, int maxDelay = 1000, int maxTimeout = 1000, bool IsWrite = false)
-        {
-            return UsbRunner.AddRequest(new List<List<byte>> { bytes }, maxDelay, maxTimeout, IsWrite);
-        }
-        public static bool IsExtendedMode { get; set; }
-        public static void IsExtendedModeMethod()
-        {
-            //var bytes = CreateBytesArray(BitConverter.GetBytes(++_usbRequestNo).Reverse(), 0x01, 0x01, 0x37);
-            //var res = SendCode(bytes).Result;
-            //IsExtendedMode = res.FirstOrDefault().Data[6] == 1;
-            IsExtendedMode = true;
-        }
-	    public static bool IsUsbDevice { get; set; }
-        public static List<byte> SendRequest(List<byte> bytes)
-        {
-            return SendCode(bytes, 100000, 100000).Result.FirstOrDefault().Data;
-        }
-        public static void SynchronizeTime(Device device)
-        {
-            var bytes = CreateBytesArray(BitConverter.GetBytes(++_usbRequestNo).Reverse(), Convert.ToByte(device.Parent.IntAddress + 2),
-            device.IntAddress, 0x02, 0x11, DateConverter.ConvertToBytes(DateTime.Now));
-            SendCode(bytes);
-        }
-        static List<byte> CreateBytesArray(params object[] values)
-        {
-            var bytes = new List<byte>();
-            foreach (var value in values)
-            {
-                if (value as IEnumerable<Byte> != null)
-                    bytes.AddRange((IEnumerable<Byte>)value);
-                else
-                    bytes.Add(Convert.ToByte(value));
-            }
-            return bytes;
-        }
-    }
+		public static void Initialize()
+		{
+			;
+		}
+		public static OperationResult<List<Response>> SendCode(List<List<byte>> bytesList, int maxDelay = 1000, int maxTimeout = 1000, bool IsWrite = false)
+		{
+			return UsbRunner.AddRequest(bytesList, maxDelay, maxTimeout, IsWrite);
+		}
+		public static OperationResult<List<Response>> SendCode(List<byte> bytes, int maxDelay = 1000, int maxTimeout = 1000, bool IsWrite = false)
+		{
+			return UsbRunner.AddRequest(new List<List<byte>> { bytes }, maxDelay, maxTimeout, IsWrite);
+		}
+		public static bool IsExtendedMode { get; set; }
+		public static void IsExtendedModeMethod()
+		{
+			var bytes = CreateBytesArray(BitConverter.GetBytes(++_usbRequestNo).Reverse(), 0x01, 0x01, 0x37);
+			var res = SendCode(bytes).Result;
+			IsExtendedMode = res.FirstOrDefault().Data[6] == 1;
+			//IsExtendedMode = true;
+		}
+		public static bool IsUsbDevice { get; set; }
+		public static List<byte> SendRequest(List<byte> bytes)
+		{
+			return SendCode(bytes, 100000, 100000).Result.FirstOrDefault().Data;
+		}
+		public static void SynchronizeTime(Device device)
+		{
+			var bytes = CreateBytesArray(BitConverter.GetBytes(++_usbRequestNo).Reverse(), Convert.ToByte(device.Parent.IntAddress + 2),
+			device.IntAddress, 0x02, 0x11, DateConverter.ConvertToBytes(DateTime.Now));
+			SendCode(bytes);
+		}
+		static List<byte> CreateBytesArray(params object[] values)
+		{
+			var bytes = new List<byte>();
+			foreach (var value in values)
+			{
+				if (value as IEnumerable<Byte> != null)
+					bytes.AddRange((IEnumerable<Byte>)value);
+				else
+					bytes.Add(Convert.ToByte(value));
+			}
+			return bytes;
+		}
+	}
 }
