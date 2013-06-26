67cc908d27b2a97fc718fb1be37a9a45ec61ddd3
Роман Казаков <fx-romario@rambler.ru>
2013-06-26 18:39:30 +0400

    SetPassword

--- Projects/ServerFS2/ServerFS2/Operations/SetConfigurationOperationHelper.cs
+++ Projects/ServerFS2/ServerFS2/Operations/SetConfigurationOperationHelper.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using FiresecAPI.Models;
 using Infrastructure.Common;
 using Ionic.Zip;
 using Device = FiresecAPI.Models.Device;
@@ -112,6 +114,23 @@ namespace ServerFS2
 			var functionCode = USBManager.Send(device, 0x3C).FunctionCode;
 			if (functionCode != 0x7c)
 				ConfirmLongTermOperation(device);
+		}
+
+		public static void SetPassword(Device device, DevicePasswordType devicePasswordType, string password)
+		{
+			for (int i = password.Length; i < 6; i++)
+			{
+				password += 'F';
+			}
+			password = password.Insert(2, " ");
+			password = password.Insert(5, " ");
+			var bytes = password.Split().Select(t => byte.Parse(t, NumberStyles.AllowHexSpecifier)).ToList();
+			if (devicePasswordType == DevicePasswordType.Administrator)
+				USBManager.Send(device, 0x02, 0x52, BytesHelper.IntToBytes(0x0A), 0x02, bytes);
+			if (devicePasswordType == DevicePasswordType.Installator)
+				USBManager.Send(device, 0x02, 0x52, BytesHelper.IntToBytes(0x07), 0x02, bytes);
+			if (devicePasswordType == DevicePasswordType.Operator)
+				USBManager.Send(device, 0x02, 0x52, BytesHelper.IntToBytes(0x04), 0x02, bytes);
 		}
 	}
 }
