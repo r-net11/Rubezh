2aea692a9a3af798177961f0f820a9f579015b79
Vladimir S <Sorokonenko@gmail.com>
2013-07-09 19:58:26 +0400

    Ribbon

--- Projects/FireAdministrator/Modules/DiagnosticsModule/ViewModels/RibbonMenuItem.cs
+++ Projects/FireAdministrator/Modules/DiagnosticsModule/ViewModels/RibbonMenuItem.cs
@@ -5,9 +5,67 @@ using System.Text;
 using Infrastructure.Common.Windows.ViewModels;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
 
 namespace DiagnosticsModule.ViewModels
 {
+	public class RibbonMenu : TabControl
+	{
+		public RibbonMenu()
+		{
+			TabStripPlacement = Dock.Left;
+			ItemContainerGenerator.StatusChanged += ItemContainerGeneratorStatusChanged;
+			Loaded += new RoutedEventHandler(RibbonMenu_Loaded);
+		}
+
+		private void RibbonMenu_Loaded(object sender, RoutedEventArgs e)
+		{
+			var collection = CollectionViewSource.GetDefaultView(Items);
+
+		}
+		protected override DependencyObject GetContainerForItemOverride()
+		{
+			return new RibbonItem();
+		}
+		protected override bool IsItemItsOwnContainerOverride(object item)
+		{
+			return item is RibbonItem;
+		}
+		protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+		{
+			base.PrepareContainerForItemOverride(element, item);
+		}
+		private void ItemContainerGeneratorStatusChanged(object sender, EventArgs e)
+		{
+			//if (((ItemContainerGenerator)sender).Status == GeneratorStatus.ContainersGenerated && SelectedIndex != -1)
+			//    SelectedIndex = -1;
+		}
+	}
+	public class RibbonItem : TabItem
+	{
+		public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register("ImageSource", typeof(string), typeof(RibbonItem));
+		public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(RibbonItem));
+		public static readonly DependencyProperty CommandProperty = DependencyProperty.Register("Command", typeof(ICommand), typeof(RibbonItem));
+
+		public string ImageSource
+		{
+			get { return (string)GetValue(ImageSourceProperty); }
+			set { SetValue(ImageSourceProperty, value); }
+		}
+		public string Text
+		{
+			get { return (string)GetValue(TextProperty); }
+			set { SetValue(TextProperty, value); }
+		}
+		public ICommand Command
+		{
+			get { return (ICommand)GetValue(CommandProperty); }
+			set { SetValue(CommandProperty, value); }
+		}
+	}
 	public class RibbonMenuItem : BaseViewModel
 	{
 		private string _text;
@@ -20,6 +78,7 @@ namespace DiagnosticsModule.ViewModels
 				OnPropertyChanged(() => Text);
 			}
 		}
+
 		private string _imageSource;
 		public string ImageSource
 		{
@@ -30,6 +89,7 @@ namespace DiagnosticsModule.ViewModels
 				OnPropertyChanged(() => ImageSource);
 			}
 		}
+
 		private ICommand _command;
 		public ICommand Command
 		{
@@ -40,6 +100,16 @@ namespace DiagnosticsModule.ViewModels
 				OnPropertyChanged(() => Command);
 			}
 		}
-				
+
+		private ObservableCollection<RibbonMenuItem> _children;
+		public ObservableCollection<RibbonMenuItem> Children
+		{
+			get { return _children; }
+			set
+			{
+				_children = value;
+				OnPropertyChanged(() => Children);
+			}
+		}
 	}
 }
