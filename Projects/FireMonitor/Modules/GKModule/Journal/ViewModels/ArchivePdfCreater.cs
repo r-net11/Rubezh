463f851971cf4f72a58aa4519ebb3e9e12e2e099
Vladimir <Sorokonenko@gmail.com>
2013-09-08 03:45:46 +0400

    iTextSharp

--- Projects/FireMonitor/Modules/GKModule/Journal/ViewModels/ArchivePdfCreater.cs
+++ Projects/FireMonitor/Modules/GKModule/Journal/ViewModels/ArchivePdfCreater.cs
@@ -2,29 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using MigraDoc.DocumentObjectModel;
-using MigraDoc.Rendering;
 using System.Diagnostics;
-using MigraDoc.DocumentObjectModel.Shapes;
-using MigraDoc.DocumentObjectModel.Tables;
-using PdfSharp.Pdf;
 using GKModule.ViewModels;
 using FiresecAPI;
 using XFiresecAPI;
 using Infrastructure.Common;
 using System.IO;
 using Microsoft.Win32;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace GKModule.Journal.ViewModels
 {
 	internal class ArchivePdfCreater
 	{
+		private BaseFont BaseFont;
+		private Font NormalFont;
+		private Font BoldFont;
 		private IEnumerable<JournalItemViewModel> _source;
-		private string _imagePath;
+		private Dictionary<string, Image> _cache;
 
 		private ArchivePdfCreater()
 		{
-			_imagePath = AppDataFolderHelper.GetTempFolder();
+			var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "Tahoma.TTF");
+			BaseFont = BaseFont.CreateFont(path, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+			NormalFont = new Font(BaseFont, Font.DEFAULTSIZE, Font.NORMAL);
+			BoldFont = new Font(BaseFont, Font.DEFAULTSIZE, Font.BOLD);
 		}
 
 		public static void Create(IEnumerable<JournalItemViewModel> source)
@@ -35,7 +38,7 @@ namespace GKModule.Journal.ViewModels
 				DefaultExt = "Файлы PDF|*.pdf"
 			};
 			if (saveDialog.ShowDialog().Value)
-				WaitHelper.Execute(() => 
+				WaitHelper.Execute(() =>
 				{
 					var creater = new ArchivePdfCreater();
 					creater._source = source;
@@ -44,16 +47,18 @@ namespace GKModule.Journal.ViewModels
 		}
 		private void CreatePdf(string fileName)
 		{
-			if (!Directory.Exists(_imagePath))
-				Directory.CreateDirectory(_imagePath);
-			PdfDocumentRenderer pdfRenderer = new PdfDocumentRenderer(true, PdfFontEmbedding.Always)
+			using (var fs = new FileStream(fileName, FileMode.Create))
 			{
-				Document = CreateDocument()
-			};
-			pdfRenderer.RenderDocument();
-			pdfRenderer.Save(fileName);
-			//if (Directory.Exists(_imagePath))
-			//    Directory.Delete(_imagePath, true);
+				Document doc = CreateDocument();
+				var pdfWriter = PdfWriter.GetInstance(doc, fs);
+				PageEventHelper pageEventHelper = new PageEventHelper(BaseFont);
+				pdfWriter.PageEvent = pageEventHelper;
+				var table = CreateTable(doc);
+				_cache = new Dictionary<string, Image>();
+				CreateHeader(table);
+				FillContent(table);
+				_cache.Clear();
+			}
 #if DEBUG
 			Process.Start(fileName);
 #endif
@@ -61,200 +66,159 @@ namespace GKModule.Journal.ViewModels
 
 		private Document CreateDocument()
 		{
-			var document = new Document()
-			{
-				Info = new DocumentInfo()
-				{
-					Title = "Архив",
-					Subject = "",
-					Author = "Рубеж / Оперативные задачи",
-				},
-				UseCmykColor = true,
-			};
-			DefineStyles(document);
-			var section = CreatePage(document);
-			var table = CreateTable(section);
-			FillContent(table);
-			return document;
+			var doc = new Document(PageSize.A4);
+			doc.Open();
+			doc.AddAuthor("Рубеж / Оперативные задачи");
+			doc.AddTitle("Архив");
+			doc.AddCreator("ОЗ");
+			//doc.AddKeywords("AddKeywords");
+			//doc.AddSubject("AddSubject");
+			doc.AddCreationDate();
+			return doc;
 		}
-		private void DefineStyles(Document document)
+		private PdfPTable CreateTable(Document doc)
 		{
-			Style style = document.Styles["Normal"];
-			style.Font.Name = "Verdana";
-
-			style = document.Styles[StyleNames.Header];
-			style.ParagraphFormat.AddTabStop("16cm", TabAlignment.Right);
-
-			style = document.Styles[StyleNames.Footer];
-			style.ParagraphFormat.AddTabStop("8cm", TabAlignment.Center);
-
-			style = document.Styles.AddStyle("Table", "Normal");
-			style.Font.Name = "Verdana";
-			style.Font.Name = "Times New Roman";
-			style.Font.Size = 9;
-
-			style = document.Styles.AddStyle("Reference", "Normal");
-			style.ParagraphFormat.SpaceBefore = "5mm";
-			style.ParagraphFormat.SpaceAfter = "5mm";
-			style.ParagraphFormat.TabStops.AddTabStop("16cm", TabAlignment.Right);
-		}
-		private Section CreatePage(Document document)
-		{
-			Section section = document.AddSection();
-			section.PageSetup.Orientation = Orientation.Landscape;
-			section.PageSetup.StartingNumber = 1;
-
-			//var image = section.Headers.Primary.AddImage("../../PowerBooks.png");
-			//image.Height = "2.5cm";
-			//image.LockAspectRatio = true;
-			//image.RelativeVertical = RelativeVertical.Line;
-			//image.RelativeHorizontal = RelativeHorizontal.Margin;
-			//image.Top = ShapePosition.Top;
-			//image.Left = ShapePosition.Right;
-			//image.WrapFormat.Style = WrapStyle.Through;
-
-			var paragraph = section.Footers.Primary.AddParagraph();
-			paragraph.AddText("Сгенерировано ");
-			paragraph.AddDateField("dd.MM.yyyy");
-			paragraph.Format.Font.Size = 9;
-			paragraph.Format.Alignment = ParagraphAlignment.Left;
-
-			paragraph = section.Footers.Primary.AddParagraph();
-			paragraph.AddText("Страница ");
-			paragraph.AddPageField();
-			paragraph.Format.Font.Size = 9;
-			paragraph.Format.Alignment = ParagraphAlignment.Right;
-
-			//paragraph = section.Footers.Primary.AddParagraph();
-			//paragraph.AddText(string.Format("Всего записей {0}", _source.Count()));
-			//paragraph.Format.Font.Size = 9;
-			//paragraph.Format.Alignment = ParagraphAlignment.Center;
-			
-			//paragraph = section.AddParagraph();
-			//paragraph.Format.Alignment = ParagraphAlignment.Center;
-			//paragraph.Style = "Reference";
-			//paragraph.AddFormattedText("Архив", TextFormat.Bold);
-
-			return section;
-		}
-		private Table CreateTable(Section section)
-		{
-			var table = section.AddTable();
-			table.Style = "Table";
-			table.Borders.Color = Colors.Black;
-			table.Borders.Width = 0.25;
-			table.Borders.Left.Width = 0.5;
-			table.Borders.Right.Width = 0.5;
-			table.Rows.LeftIndent = 0;
-			CreateColumns(table);
-			CreateHeader(table);
+			var table = new PdfPTable(8);
+			//table.SplitLate = false;
+			table.HeaderRows = 1;
+			table.TotalWidth = doc.PageSize.Width - doc.LeftMargin - doc.RightMargin;
+			table.LockedWidth = true;
+			float[] widths = new float[] { 30f, 30f, 45f, 10f, 60f, 15f, 30f, 30f };
+			table.SetWidths(widths);
+			table.HorizontalAlignment = 1;
+			table.SpacingBefore = 20f;
+			table.SpacingAfter = 30f;
 			return table;
 		}
-		private void CreateColumns(Table table)
+		private void CreateHeader(PdfPTable table)
 		{
-			var column = table.AddColumn(Unit.FromMillimeter(30));
-			column.Format.Alignment = ParagraphAlignment.Center;
-			column = table.AddColumn(Unit.FromMillimeter(30));
-			column.Format.Alignment = ParagraphAlignment.Center;
-			column = table.AddColumn(Unit.FromMillimeter(45));
-			column = table.AddColumn(Unit.FromMillimeter(10));
-			column = table.AddColumn(Unit.FromMillimeter(60));
-			column = table.AddColumn(Unit.FromMillimeter(12));
-			column = table.AddColumn(Unit.FromMillimeter(30));
-			column = table.AddColumn(Unit.FromMillimeter(30));
-		}
-		private void CreateHeader(Table table)
-		{
-			var row = table.AddRow();
-			row.HeadingFormat = true;
-			row.Format.Alignment = ParagraphAlignment.Center;
-			row.Format.Font.Bold = true;
-			row.Shading.Color = Colors.LightBlue;
-			row.Cells[0].AddParagraph("Дата в приборе");
-			row.Cells[0].VerticalAlignment = VerticalAlignment.Center;
-			row.Cells[1].AddParagraph("Системная дата");
-			row.Cells[1].VerticalAlignment = VerticalAlignment.Center;
-			row.Cells[2].AddParagraph("Событие");
-			row.Cells[2].VerticalAlignment = VerticalAlignment.Center;
-			row.Cells[3].AddParagraph("");
-			row.Cells[3].VerticalAlignment = VerticalAlignment.Center;
-			row.Cells[4].AddParagraph("Уточнение");
-			row.Cells[4].VerticalAlignment = VerticalAlignment.Center;
-			row.Cells[5].AddParagraph("Объект");
-			row.Cells[5].VerticalAlignment = VerticalAlignment.Center;
-			row.Cells[6].AddParagraph("Класс состояния");
-			row.Cells[6].VerticalAlignment = VerticalAlignment.Center;
-			row.Cells[7].AddParagraph("Пользователь");
-			row.Cells[7].VerticalAlignment = VerticalAlignment.Center;
-		}
-		private void FillContent(Table table)
-		{
-			var imageSize = Unit.FromMillimeter(4);
-			Image image;
-			string source;
-			Paragraph paragraph;
-			foreach (var item in _source)
+			var background = new BaseColor(System.Windows.Media.Colors.LightBlue.R, System.Windows.Media.Colors.LightBlue.G, System.Windows.Media.Colors.LightBlue.B, System.Windows.Media.Colors.LightBlue.A);
+			var headers = new string[]
 			{
-				Row row = table.AddRow();
-				row.Cells[0].AddParagraph(item.JournalItem.DeviceDateTime.ToString());
-				row.Cells[0].VerticalAlignment = VerticalAlignment.Center;
-				row.Cells[1].AddParagraph(item.JournalItem.SystemDateTime.ToString());
-				row.Cells[1].VerticalAlignment = VerticalAlignment.Center;
-				row.Cells[2].AddParagraph(item.JournalItem.Name ?? string.Empty);
-				row.Cells[2].VerticalAlignment = VerticalAlignment.Center;
-				row.Cells[3].AddParagraph(item.JournalItem.YesNo.ToDescription());
-				row.Cells[3].VerticalAlignment = VerticalAlignment.Center;
-				row.Cells[4].AddParagraph(item.JournalItem.Description ?? string.Empty);
-				row.Cells[4].VerticalAlignment = VerticalAlignment.Center;
-				paragraph = row.Cells[5].AddParagraph();
-				source = GetImage(item.ImageSource);
-				if (source != null)
-				{
-					image = paragraph.AddImage(source);
-					image.Width = imageSize;
-					image.Height = imageSize;
-				}
-				paragraph.AddText(item.PresentationName ?? string.Empty);
-				row.Cells[5].VerticalAlignment = VerticalAlignment.Center;
-				paragraph = row.Cells[6].AddParagraph();
-				if (item.JournalItem.StateClass != XStateClass.Norm && item.JournalItem.StateClass != XStateClass.Off)
-				{
-					source = GetImage("/Controls;component/StateClassIcons/" + item.JournalItem.StateClass.ToString() + ".png");
-					if (source != null)
-					{
-						image = paragraph.AddImage(source);
-						image.Width = imageSize;
-						image.Height = imageSize;
-					}
-				}
-				paragraph.AddText(item.JournalItem.StateClass.ToDescription());
-				row.Cells[6].VerticalAlignment = VerticalAlignment.Center;
-				row.Cells[7].AddParagraph(item.JournalItem.UserName ?? string.Empty);
-				row.Cells[7].VerticalAlignment = VerticalAlignment.Center;
+				"Дата в приборе",
+				"Системная дата",
+				"Событие",
+				"",
+				"Уточнение",
+				"Объект",
+				"Класс состояния",
+				"Пользователь"
+			};
+			foreach (var header in headers)
+			{
+				var cell = new PdfPCell(new Phrase(header, BoldFont));
+				cell.BackgroundColor = background;
+				table.AddCell(cell);
 			}
 		}
-		private string GetImage(string source)
+		private void FillContent(PdfPTable table)
 		{
-			var resource = System.Windows.Application.GetResourceStream(new Uri(source, UriKind.RelativeOrAbsolute));
+			foreach (var item in _source)
+			{
+				table.AddCell(new Phrase(item.JournalItem.DeviceDateTime.ToString(), NormalFont));
+				table.AddCell(new Phrase(item.JournalItem.SystemDateTime.ToString(), NormalFont));
+				table.AddCell(new Phrase(item.JournalItem.Name ?? string.Empty, NormalFont));
+				table.AddCell(new Phrase(item.JournalItem.YesNo.ToDescription(), NormalFont));
+				table.AddCell(new Phrase(item.JournalItem.Description ?? string.Empty, NormalFont));//4
+				AddImageTextCell(table, item.ImageSource, item.PresentationName ?? string.Empty);
+				AddImageTextCell(table, item.JournalItem.StateClass != XStateClass.Norm && item.JournalItem.StateClass != XStateClass.Off ? "/Controls;component/StateClassIcons/" + item.JournalItem.StateClass.ToString() + ".png" : null, item.JournalItem.StateClass.ToDescription());
+				table.AddCell(new Phrase(item.JournalItem.UserName ?? string.Empty, NormalFont));//7
+			}
+		}
+		private void AddImageTextCell(PdfPTable table, string imageSource, string text)
+		{
+			var image = GetImage(imageSource);
+			var nestedTable = new PdfPTable(2);
+			nestedTable.SetWidths(new float[] { 1f, 12f });
+			nestedTable.DefaultCell.Border = 0;
+			if (image == null)
+				nestedTable.AddCell("");
+			else
+				nestedTable.AddCell(image);
+			nestedTable.AddCell(new Phrase(text, NormalFont));
+
+			var cell = new PdfPCell(nestedTable);
+			cell.Padding = 0;
+			table.AddCell(cell);
+		}
+		private Image GetImage(string source)
+		{
+			if (_cache.ContainsKey(source))
+				return _cache[source];
+
+			var uri = new Uri(source, UriKind.RelativeOrAbsolute);
+			var resource = System.Windows.Application.GetResourceStream(uri);
 			if (resource == null || resource.Stream == null)
 				return null;
 
-			var location = Path.GetInvalidFileNameChars().Aggregate(source, (current, c) => current.Replace(c.ToString(), string.Empty));
-			location = Path.Combine(_imagePath, location);
+			var image = Image.GetInstance(resource.Stream);
+			if (image == null)
+				return null;
 
-			if (!File.Exists(location))
-				using (var br = new BinaryReader(resource.Stream))
-				using (FileStream fs = new FileStream(location, FileMode.Create))
-				using (BinaryWriter bw = new BinaryWriter(fs))
-				{
-					byte[] ba = new byte[resource.Stream.Length];
-					resource.Stream.Read(ba, 0, ba.Length);
-					bw.Write(ba);
-					br.Close();
-					bw.Close();
-					resource.Stream.Close();
-				}
-			return location;
+			_cache.Add(source, image);
+			return image;
+		}
+	}
+
+	public class PageEventHelper : PdfPageEventHelper
+	{
+		private PdfContentByte cb;
+		private PdfTemplate template;
+		private BaseFont bf;
+		private DateTime printTime;
+
+		public int FontSize { get; set; }
+		public int FotterShift { get; set; }
+
+		public PageEventHelper(BaseFont baseFont)
+		{
+			bf = baseFont;
+			FontSize = 9;
+			FotterShift = 10;
+		}
+
+		public override void OnOpenDocument(PdfWriter writer, Document document)
+		{
+			printTime = DateTime.Now;
+			cb = writer.DirectContent;
+			template = cb.CreateTemplate(50, 50);
+		}
+
+		public override void OnEndPage(PdfWriter writer, Document document)
+		{
+			base.OnEndPage(writer, document);
+
+			int pageN = writer.PageNumber;
+			String text = "Страница " + pageN + " из ";
+
+			float len = bf.GetWidthPoint(text, 8);
+			Rectangle pageSize = document.PageSize;
+
+			cb.SetRGBColorFill(100, 100, 100);
+
+			cb.BeginText();
+			cb.SetFontAndSize(bf, 8);
+			cb.SetTextMatrix(pageSize.GetLeft(document.LeftMargin), pageSize.GetBottom(document.BottomMargin - FotterShift));
+			cb.ShowText(text);
+			cb.EndText();
+
+			cb.AddTemplate(template, pageSize.GetLeft(document.LeftMargin) + len, pageSize.GetBottom(document.BottomMargin - FotterShift));
+
+			cb.BeginText();
+			cb.SetFontAndSize(bf, 8);
+			cb.ShowTextAligned(PdfContentByte.ALIGN_RIGHT, "Сгенерировано " + printTime.ToString(), pageSize.GetRight(document.RightMargin), pageSize.GetBottom(document.BottomMargin - FotterShift), 0);
+			cb.EndText();
+		}
+
+		public override void OnCloseDocument(PdfWriter writer, Document document)
+		{
+			base.OnCloseDocument(writer, document);
+
+			template.BeginText();
+			template.SetFontAndSize(bf, FontSize);
+			template.SetTextMatrix(0, 0);
+			template.ShowText((writer.PageNumber - 1).ToString());
+			template.EndText();
 		}
 	}
 }
