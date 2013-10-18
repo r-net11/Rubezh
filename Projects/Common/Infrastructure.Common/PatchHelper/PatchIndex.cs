876223e8a5a17c628ad65ef8e88126659711fe2e
Maxim Obycheff <obycheff.maxim@yandex.ru>
2013-10-18 10:43:00 +0400

    patching

---
+++ Projects/Common/Infrastructure.Common/PatchHelper/PatchIndex.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Infrastructure.Common
+{
+	public class PatchIndex
+    {
+        public string Type { get; private set; }
+        public string Id { get; private set; }
+
+        public PatchIndex(string type, string id)
+        {
+            Type = type;
+            Id = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var patchIndex = (PatchIndex)obj;
+            return Type == patchIndex.Type && Id == patchIndex.Id;
+        }
+    }
+}
