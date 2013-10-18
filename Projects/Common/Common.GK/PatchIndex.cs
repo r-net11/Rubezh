86b1c0c94e51dfeea61df73780f50331fcbc9fcb
Maxim Obycheff <obycheff.maxim@yandex.ru>
2013-10-18 13:36:55 +0400

    patching

--- Projects/Common/Common.GK/PatchIndex.cs
+++ Projects/Common/Common.GK/PatchIndex.cs
@@ -1,7 +1,11 @@
-﻿
-namespace Infrastructure.Common
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common.GK
 {
-	public class PatchIndex
+    public class PatchIndex
     {
         public string Type { get; private set; }
         public string Id { get; private set; }
