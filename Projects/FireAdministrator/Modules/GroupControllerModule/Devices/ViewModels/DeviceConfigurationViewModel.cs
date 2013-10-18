0a607beaabc9e21fc21af4e8409598e4fdf962bd
Роман Казаков <fx-romario@rambler.ru>
2013-10-18 17:54:09 +0400

    GetDeviceConfig 2.0 beta

--- Projects/FireAdministrator/Modules/GroupControllerModule/Devices/ViewModels/DeviceConfigurationViewModel.cs
+++ Projects/FireAdministrator/Modules/GroupControllerModule/Devices/ViewModels/DeviceConfigurationViewModel.cs
@@ -12,26 +12,38 @@ namespace GKModule.ViewModels
 		private XDevice RemoteDevice { get; set; }
 		private XDeviceConfiguration LocalConfiguration { get; set; }
 		private XDeviceConfiguration RemoteConfiguration { get; set; }
-		public DeviceConfigurationViewModel(XDeviceConfiguration localConfiguration, XDeviceConfiguration remoteConfiguration)
+		public DeviceConfigurationViewModel(XDeviceConfiguration localConfiguration, XDeviceConfiguration remoteConfiguration, XDevice device)
 		{
 			Title = "Конфигурация устройств";
 			LocalConfiguration = localConfiguration;
 			RemoteConfiguration = remoteConfiguration;
 
-			LocalDevice = localConfiguration.Devices.FirstOrDefault(x => x.Driver.DriverType == XDriverType.GK);
-			RemoteDevice = remoteConfiguration.Devices.FirstOrDefault(x => x.Driver.DriverType == XDriverType.GK);
-			RemoteDevice.Properties.Add(LocalDevice.Properties.FirstOrDefault(x => x.Name == "IPAddress")); // Клонируем IP адрес
+			LocalDevice = localConfiguration.Devices.FirstOrDefault(x => (x.Driver.DriverType == device.Driver.DriverType)&&(x.Address == device.Address));
+			RemoteDevice = remoteConfiguration.Devices.FirstOrDefault(x => (x.Driver.DriverType == device.Driver.DriverType) && (x.Address == device.Address));
 
 			var localDeviceClone = (XDevice) LocalDevice.Clone();
 			var remoteDeviceClone = (XDevice) RemoteDevice.Clone();
 
-			LocalDeviceViewModel = new DeviceTreeViewModel(localDeviceClone, localConfiguration.Zones, localConfiguration.Directions);
-			RemoteDeviceViewModel = new DeviceTreeViewModel(remoteDeviceClone, remoteConfiguration.Zones, remoteConfiguration.Directions);
-
+			if (device.Driver.DriverType == XDriverType.GK)
+			{
+				LocalDeviceViewModel = new DeviceTreeViewModel(localDeviceClone, localConfiguration.Zones,
+															   localConfiguration.Directions);
+				RemoteDeviceViewModel = new DeviceTreeViewModel(remoteDeviceClone, remoteConfiguration.Zones,
+																remoteConfiguration.Directions);
+			}
+			else
+			{
+				LocalDeviceViewModel = new DeviceTreeViewModel(localDeviceClone, null, null);
+				RemoteDeviceViewModel = new DeviceTreeViewModel(remoteDeviceClone, null, null);
+			}
 			DeviceTreeViewModel.CompareTrees(LocalDeviceViewModel.Devices, RemoteDeviceViewModel.Devices);
-			DeviceTreeViewModel.CompareTrees(LocalDeviceViewModel.Zones, RemoteDeviceViewModel.Zones);
-			DeviceTreeViewModel.CompareTrees(LocalDeviceViewModel.Directions, RemoteDeviceViewModel.Directions);
-
+			//LocalDeviceViewModel.Devices = LocalDeviceViewModel.Devices.OrderBy(x => x.ParentPath).ToList().OrderBy(x => x.Address).ToList().OrderBy(x => x.Name).ToList();
+			//objects2 = objects2.OrderBy(x => x.ParentPath).ToList().OrderBy(x => x.Address).ToList().OrderBy(x => x.Name).ToList();
+			if (device.Driver.DriverType == XDriverType.GK)
+			{
+				DeviceTreeViewModel.CompareTrees(LocalDeviceViewModel.Zones, RemoteDeviceViewModel.Zones);
+				DeviceTreeViewModel.CompareTrees(LocalDeviceViewModel.Directions, RemoteDeviceViewModel.Directions);
+			}
 			ChangeCommand = new RelayCommand(OnChange);
 		}
 		public DeviceTreeViewModel LocalDeviceViewModel { get; set; }
@@ -43,8 +55,11 @@ namespace GKModule.ViewModels
 			var rootDevice = LocalConfiguration.Devices.FirstOrDefault(x => x.UID == LocalDevice.Parent.UID);
 			rootDevice.Children.Remove(LocalDevice);
 			rootDevice.Children.Add(RemoteDevice);
-			LocalConfiguration.Zones = RemoteConfiguration.Zones;
-			LocalConfiguration.Directions = RemoteConfiguration.Directions;
+			if (LocalDevice.Driver.DriverType == XDriverType.GK)
+			{
+				LocalConfiguration.Zones = RemoteConfiguration.Zones;
+				LocalConfiguration.Directions = RemoteConfiguration.Directions;
+			}
 			ServiceFactory.SaveService.GKChanged = true;
 			Close(true);
 		}
