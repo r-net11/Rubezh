401dba449b9a1f13b6692fdb5d0eb2930946d8e0
Maxim Obycheff <no>
2013-11-20 11:26:02 +0400

    library

--- Projects/Common/GKProcessor/Drivers/AddressDevices/Battery_Helper.cs
+++ Projects/Common/GKProcessor/Drivers/AddressDevices/Battery_Helper.cs
@@ -39,7 +39,13 @@ namespace GKProcessor
 			property1.Parameters.Add(property1Parameter1);
 			property1.Parameters.Add(property1Parameter2);
 			driver.Properties.Add(property1);
-			
+
+			driver.AUParameters.Add(new XAUParameter() { No = 0x81, Name = "Напряжение на АКБ1", InternalName = "InnerVoltage_1" });
+			driver.AUParameters.Add(new XAUParameter() { No = 0x81, Name = "Напряжение на АКБ2", InternalName = "InnerVoltage_2" });
+			driver.AUParameters.Add(new XAUParameter() { No = 0x82, Name = "Напряжение на выходе 1", InternalName = "OuterVoltage_1" });
+			driver.AUParameters.Add(new XAUParameter() { No = 0x82, Name = "Напряжение на выходе 2", InternalName = "OuterVoltage_2" });
+			driver.AUParameters.Add(new XAUParameter() { No = 0x82, Name = "Напряжение в сети", InternalName = "CircuitVoltage_2" });
+
 			return driver;
 		}
 	}
