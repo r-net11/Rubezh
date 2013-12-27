844a0a371c1364c545db2767fa39fbee0995735c
Maxim Obycheff <no>
2013-12-27 13:31:36 +0400

    fix

---
+++ Projects/FireAdministrator/Modules/GroupControllerModule/Instructions/Converters/DirectionsToLongStringConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using FiresecAPI;
+using FiresecClient;
+using XFiresecAPI;
+
+namespace GKModule.Converters
+{
+	public class DirectionsToLongStringConverter : IValueConverter
+	{
+		public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			var directionUIDs = value as ICollection<Guid>;
+			if (directionUIDs == null)
+				return "";
+			var directions = new List<XDirection>();
+			foreach (var uid in directionUIDs)
+			{
+				var direction = XManager.Directions.FirstOrDefault(x => x.UID == uid);
+				if (direction != null)
+					directions.Add(direction);
+			}
+			var stringBuilder = new StringBuilder();
+			var count = 0;
+			foreach (var direction in directions)
+			{
+				if (count > 0)
+					stringBuilder.Append(", ");
+				stringBuilder.Append(direction.PresentationName);
+				count++;
+			}
+			return stringBuilder.ToString();
+		}
+
+		public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
--- Projects/FireAdministrator/Modules/GroupControllerModule/Instructions/ViewModels/InstructionViewModel.cs
+++ Projects/FireAdministrator/Modules/GroupControllerModule/Instructions/ViewModels/InstructionViewModel.cs
@@ -26,6 +26,16 @@ namespace GKModule.ViewModels
 		{
 			get { return Instruction.Devices.Count > 0; }
 		}
+
+		public bool HasZones
+		{
+			get { return Instruction.ZoneUIDs.Count > 0; }
+		}
+
+		public bool HasDirections
+		{
+			get { return Instruction.Directions.Count > 0; }
+		}
 		
 		public void Update()
 		{
