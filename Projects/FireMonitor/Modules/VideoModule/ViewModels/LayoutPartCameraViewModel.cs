f01bbd7091f27baa62e8c9f95fd63b7842f442e8
Roman Kazakov <kazakovrb@rubezh.ru>
2014-02-28 20:07:57 +0300

    fix

--- Projects/FireMonitor/Modules/VideoModule/ViewModels/LayoutPartCameraViewModel.cs
+++ Projects/FireMonitor/Modules/VideoModule/ViewModels/LayoutPartCameraViewModel.cs
@@ -3,27 +3,52 @@ using FiresecAPI.Models;
 using FiresecAPI.Models.Layouts;
 using FiresecClient;
 using Infrastructure.Common.Windows.ViewModels;
+using Infrastructure.Common.Windows;
 
 namespace VideoModule.ViewModels
 {
 	public class LayoutPartCameraViewModel : BaseViewModel
 	{
 		public CameraViewModel CameraViewModel { get; set; }
-		public Camera Camera { get; set; }
 
-		public LayoutPartCameraViewModel(Camera camera)
-		{
-			Camera = camera;
-			CameraViewModel = new CameraViewModel(Camera);
-		}
+        public LayoutPartCameraViewModel(Camera camera)
+        {
+            Camera = camera;
+            ShowPropertiesCommand = new RelayCommand(OnShowProperties);
+        }
 
-		public LayoutPartCameraViewModel(LayoutPartCameraProperties properties)
+        public LayoutPartCameraViewModel()
+        {
+            //CameraViewModel = new CameraViewModel(FiresecManager.SystemConfiguration.Cameras.FirstOrDefault());
+            ShowPropertiesCommand = new RelayCommand(OnShowProperties);
+        }
+
+        Camera _camera;
+        public Camera Camera
+        {
+            get { return _camera; }
+            set
+            {
+                _camera = value;
+                CameraViewModel = new CameraViewModel(value);
+                CameraViewModel.StartVideo();
+            }
+        }
+
+        public LayoutPartCameraViewModel(LayoutPartCameraProperties properties)
 		{
 			if (properties != null)
 			{
 				Camera = FiresecManager.SystemConfiguration.Cameras.FirstOrDefault(item => item.UID == properties.SourceUID);
-				CameraViewModel = new CameraViewModel(Camera);
 			}
 		}
+
+        public RelayCommand ShowPropertiesCommand { get; private set; }
+        void OnShowProperties()
+        {
+            Camera = FiresecManager.SystemConfiguration.Cameras.FirstOrDefault();
+            //var layoutPartPropertyCameraPageViewModel = new LayoutPartPropertyCameraPageViewModel(this);
+            //DialogService.ShowModalWindow(layoutPartPropertyCameraPageViewModel);            
+        }
 	}
 }
---
+++ Projects/FireMonitor/Modules/VideoModule/ViewModels/LayoutPartPropertyCameraPageViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using Infrastructure.Common.Windows.ViewModels;
+using XFiresecAPI;
+using FiresecAPI.Models;
+using FiresecClient;
+using FiresecAPI.Models.Layouts;
+using Infrastructure.Common.Services.Layout;
+
+namespace VideoModule.ViewModels
+{
+    public class LayoutPartPropertyCameraPageViewModel : SaveCancelDialogViewModel
+    {
+        LayoutPartCameraViewModel _layoutPartCameraViewModel { get; set; }
+        public LayoutPartPropertyCameraPageViewModel(LayoutPartCameraViewModel layoutPartCameraViewModel)
+        {
+            _layoutPartCameraViewModel = layoutPartCameraViewModel;
+            Cameras = new ObservableCollection<Camera>(FiresecManager.SystemConfiguration.Cameras);
+        }
+
+        private ObservableCollection<Camera> _cameras;
+        public ObservableCollection<Camera> Cameras
+        {
+            get { return _cameras; }
+            set
+            {
+                _cameras = value;
+                OnPropertyChanged(() => Cameras);
+            }
+        }
+
+        private Camera _selectedCamera;
+        public Camera SelectedCamera
+        {
+            get { return _selectedCamera; }
+            set
+            {
+                _selectedCamera = value;
+                OnPropertyChanged(() => SelectedCamera);
+            }
+        }
+        
+        protected override bool CanSave()
+        {
+            return true;
+        }
+
+        protected override bool Save()
+        {
+            if (SelectedCamera != null)
+            {
+                _layoutPartCameraViewModel.Camera = SelectedCamera;
+                _layoutPartCameraViewModel.CameraViewModel.StartVideo();
+                return true;
+            }
+            return false;
+        }
+    }
+}
