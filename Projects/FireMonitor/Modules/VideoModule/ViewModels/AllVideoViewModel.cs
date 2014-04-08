9896b384da68d2970c706a8a8b702c5f5f880d86
Sevastyanov Alexander <none>
2014-04-08 12:30:54 +0400

    VideoView

---
+++ Projects/FireMonitor/Modules/VideoModule/ViewModels/AllVideoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Infrastructure.Common.Windows.ViewModels;
+
+namespace VideoModule.ViewModels
+{
+	public class AllVideoViewModel : ViewPartViewModel
+	{
+		public CamerasViewModel CamerasViewModel { get; private set; }
+		public LayoutMultiCameraViewModel LayoutMultiCameraViewModel { get; private set; }
+
+		public AllVideoViewModel()
+		{
+			CamerasViewModel = new CamerasViewModel();
+			LayoutMultiCameraViewModel = new LayoutMultiCameraViewModel();
+		}
+
+		public void Initialize()
+		{
+			CamerasViewModel.Initialize();
+		}
+	}
+}
