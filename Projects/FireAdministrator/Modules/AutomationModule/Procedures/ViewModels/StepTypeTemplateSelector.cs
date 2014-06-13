88c94dcb809b9a355898d7ff2c02781ce8b7edc9
Sevastyanov Alexander <none>
2014-06-13 16:52:46 +0400

    procedure step type selector

---
+++ Projects/FireAdministrator/Modules/AutomationModule/Procedures/ViewModels/StepTypeTemplateSelector.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using AutomationModule.ViewModels;
+
+namespace AutomationModule.ViewModels
+{
+	public class StepTypeTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate SoundsTemplate { get; set; }
+		public DataTemplate JournalTemplate { get; set; }
+
+		public override DataTemplate SelectTemplate(object item, DependencyObject container)
+		{
+			if (item is SoundStepViewModel)
+			{
+				return SoundsTemplate;
+			}
+			else if (item is JournalStepViewModel)
+			{
+				return JournalTemplate;
+			}
+
+			return null;
+		}
+	}
+}
--- Projects/FireAdministrator/Modules/AutomationModule/Procedures/ViewModels/StepViewModel.cs
+++ Projects/FireAdministrator/Modules/AutomationModule/Procedures/ViewModels/StepViewModel.cs
@@ -20,12 +20,23 @@ namespace AutomationModule.ViewModels
 
 		public StepViewModel(StepsViewModel stepsViewModel, ProcedureStep step)
 		{
-			AddCommand = new RelayCommand(OnAdd);
-			AddToParentCommand = new RelayCommand(OnAddToParent, CanAddToParent);
-			RemoveCommand = new RelayCommand(OnRemove);
+			//AddCommand = new RelayCommand(OnAdd);
+			//AddToParentCommand = new RelayCommand(OnAddToParent, CanAddToParent);
+			//RemoveCommand = new RelayCommand(OnRemove);
 
 			StepsViewModel = stepsViewModel;
 			Step = step;
+
+			switch(step.ProcedureStepType)
+			{
+				case ProcedureStepType.PlaySound:
+					Content = new SoundStepViewModel(step);
+					break;
+
+				case ProcedureStepType.SendMessage:
+					Content = new JournalStepViewModel(step);
+					break;
+			}
 		}
 
 		void OnChanged()
@@ -58,63 +69,65 @@ namespace AutomationModule.ViewModels
 			get { return ""; }
 		}
 
-		public ProcedureStep AddChildStep()
-		{
-			var stepTypeSelectationViewModel = new StepTypeSelectationViewModel();
-			if (DialogService.ShowModalWindow(stepTypeSelectationViewModel))
-			{
-				if (stepTypeSelectationViewModel.SelectedStepType != null && !stepTypeSelectationViewModel.SelectedStepType.IsFolder)
-				{
-					var procedureStep = new ProcedureStep();
-					procedureStep.ProcedureStepType = stepTypeSelectationViewModel.SelectedStepType.ProcedureStepType;
-					var stepViewModel = new StepViewModel(StepsViewModel, procedureStep);
-					this.Step.Children.Add(procedureStep);
-					this.AddChild(stepViewModel);
-					IsExpanded = true;
-					ServiceFactory.SaveService.AutomationChanged = true;
-					Update();
-					return procedureStep;
-				}
-			}
-			return null;
-		}
+		//public ProcedureStep AddChildStep()
+		//{
+		//    var stepTypeSelectationViewModel = new StepTypeSelectationViewModel();
+		//    if (DialogService.ShowModalWindow(stepTypeSelectationViewModel))
+		//    {
+		//        if (stepTypeSelectationViewModel.SelectedStepType != null && !stepTypeSelectationViewModel.SelectedStepType.IsFolder)
+		//        {
+		//            var procedureStep = new ProcedureStep();
+		//            procedureStep.ProcedureStepType = stepTypeSelectationViewModel.SelectedStepType.ProcedureStepType;
+		//            var stepViewModel = new StepViewModel(StepsViewModel, procedureStep);
+		//            this.Step.Children.Add(procedureStep);
+		//            this.AddChild(stepViewModel);
+		//            IsExpanded = true;
+		//            ServiceFactory.SaveService.AutomationChanged = true;
+		//            Update();
+		//            return procedureStep;
+		//        }
+		//    }
+		//    return null;
+		//}
 
-		public RelayCommand AddCommand { get; private set; }
-		void OnAdd()
-		{
-			AddChildStep();
-		}
+		//public RelayCommand AddCommand { get; private set; }
+		//void OnAdd()
+		//{
+		//    AddChildStep();
+		//}
 
-		public RelayCommand AddToParentCommand { get; private set; }
-		void OnAddToParent()
-		{
-			Parent.AddCommand.Execute();
-		}
-		public bool CanAddToParent()
-		{
-			return Parent != null;
-		}
+		//public RelayCommand AddToParentCommand { get; private set; }
+		//void OnAddToParent()
+		//{
+		//    Parent.AddCommand.Execute();
+		//}
+		//public bool CanAddToParent()
+		//{
+		//    return Parent != null;
+		//}
 
-		public RelayCommand RemoveCommand { get; private set; }
-		void OnRemove()
-		{
-			var parent = Parent;
-			if (parent != null)
-			{
-				var index = StepsViewModel.SelectedStep.VisualIndex;
-				parent.Nodes.Remove(this);
-				parent.Update();
+		//public RelayCommand RemoveCommand { get; private set; }
+		//void OnRemove()
+		//{
+		//    var parent = Parent;
+		//    if (parent != null)
+		//    {
+		//        var index = StepsViewModel.SelectedStep.VisualIndex;
+		//        parent.Nodes.Remove(this);
+		//        parent.Update();
 
-				index = Math.Min(index, parent.ChildrenCount - 1);
-				StepsViewModel.AllSteps.Remove(this);
-				StepsViewModel.SelectedStep = index >= 0 ? parent.GetChildByVisualIndex(index) : parent;
-			}
-			if (Step.Parent != null)
-			{
-				Step.Parent.Children.Remove(Step);
-			}
-			ServiceFactory.SaveService.AutomationChanged = true;
-		}
+		//        index = Math.Min(index, parent.ChildrenCount - 1);
+		//        StepsViewModel.AllSteps.Remove(this);
+		//        StepsViewModel.SelectedStep = index >= 0 ? parent.GetChildByVisualIndex(index) : parent;
+		//    }
+		//    if (Step.Parent != null)
+		//    {
+		//        Step.Parent.Children.Remove(Step);
+		//    }
+		//    ServiceFactory.SaveService.AutomationChanged = true;
+		//}
+
+		public object Content { get; private set; }
 
 		public bool IsVirtual
 		{
