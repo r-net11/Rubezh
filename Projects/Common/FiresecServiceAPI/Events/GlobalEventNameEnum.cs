af3ddcd8184876f472735d5c237b1cd8c85a00a4
Sevastyanov Alexander <none>
2014-07-07 01:10:52 +0400

    monitor archive

--- Projects/Common/FiresecServiceAPI/Events/GlobalEventNameEnum.cs
+++ Projects/Common/FiresecServiceAPI/Events/GlobalEventNameEnum.cs
@@ -12,10 +12,10 @@ namespace FiresecAPI.Events
 		[DescriptionAttribute("")]
 		NULL,
 
-		[EventDescription(GlobalSubsystemType.System, "Неизвестное событие", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.System, "Неизвестное событие", XStateClass.No)]
 		Неизвестное_событие,
 
-		[EventDescription(GlobalSubsystemType.System, "Подтверждение тревоги", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.System, "Подтверждение тревоги", XStateClass.Fire1)]
 		Подтверждение_тревоги,
 
 		[EventDescription(GlobalSubsystemType.System, "Вход пользователя в систему", XStateClass.Info)]
@@ -30,16 +30,16 @@ namespace FiresecAPI.Events
 		[EventDescription(GlobalSubsystemType.System, "Дежурство принял", XStateClass.Info)]
 		Дежурство_принял,
 
-		[EventDescription(GlobalSubsystemType.System, "Зависание процесса отпроса", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.System, "Зависание процесса отпроса", XStateClass.Unknown)]
 		Зависание_процесса_отпроса,
 
-		[EventDescription(GlobalSubsystemType.System, "Отсутствует лицензия", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.System, "Отсутствует лицензия", XStateClass.HasNoLicense)]
 		Отсутствует_лицензия,
 
-		[EventDescription(GlobalSubsystemType.System, "Лицензия обнаружена", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.System, "Лицензия обнаружена", XStateClass.HasNoLicense)]
 		Лицензия_обнаружена,
 
-		[EventDescription(GlobalSubsystemType.System, "Ошибка инициализации мониторинга", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.System, "Ошибка инициализации мониторинга", XStateClass.Unknown)]
 		Ошибка_инициализации_мониторинга,
 
 		[EventDescription(GlobalSubsystemType.GK, "Обновление ПО прибора", XStateClass.Info)]
@@ -60,43 +60,43 @@ namespace FiresecAPI.Events
 		[EventDescription(GlobalSubsystemType.GK, "Команда оператора", XStateClass.Info)]
 		Команда_оператора,
 
-		[EventDescription(GlobalSubsystemType.GK, "Ошибка при выполнении команды", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Ошибка при выполнении команды", XStateClass.Failure)]
 		Ошибка_при_выполнении_команды,
 
-		[EventDescription(GlobalSubsystemType.GK, "Ошибка при выполнении команды над устройством", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Ошибка при выполнении команды над устройством", XStateClass.Failure)]
 		Ошибка_при_выполнении_команды_над_устройством,
 
-		[EventDescription(GlobalSubsystemType.GK, "Нет связи с ГК", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Нет связи с ГК", XStateClass.ConnectionLost)]
 		Нет_связи_с_ГК,
 
-		[EventDescription(GlobalSubsystemType.GK, "Связь с ГК восстановлена", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Связь с ГК восстановлена", XStateClass.ConnectionLost)]
 		Связь_с_ГК_восстановлена,
 
-		[EventDescription(GlobalSubsystemType.GK, "Конфигурация прибора не соответствует конфигурации ПК", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Конфигурация прибора не соответствует конфигурации ПК", XStateClass.Unknown)]
 		Конфигурация_прибора_не_соответствует_конфигурации_ПК,
 
-		[EventDescription(GlobalSubsystemType.GK, "Конфигурация прибора соответствует конфигурации ПК", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Конфигурация прибора соответствует конфигурации ПК", XStateClass.Unknown)]
 		Конфигурация_прибора_соответствует_конфигурации_ПК,
 
-		[EventDescription(GlobalSubsystemType.GK, "Ошибка при синхронизации журнала", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Ошибка при синхронизации журнала", XStateClass.Unknown)]
 		Ошибка_при_синхронизации_журнала,
 
-		[EventDescription(GlobalSubsystemType.GK, "Ошибка при опросе состояний компонентов ГК", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Ошибка при опросе состояний компонентов ГК", XStateClass.Unknown)]
 		Ошибка_при_опросе_состояний_компонентов_ГК,
 
-		[EventDescription(GlobalSubsystemType.GK, "Устранена ошибка при опросе состояний компонентов ГК", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Устранена ошибка при опросе состояний компонентов ГК", XStateClass.Unknown)]
 		Устранена_ошибка_при_опросе_состояний_компонентов_ГК,
 
-		[EventDescription(GlobalSubsystemType.GK, "Восстановление связи с прибором", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Восстановление связи с прибором", XStateClass.ConnectionLost)]
 		Восстановление_связи_с_прибором,
 
-		[EventDescription(GlobalSubsystemType.GK, "Потеря связи с прибором", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Потеря связи с прибором", XStateClass.ConnectionLost)]
 		Потеря_связи_с_прибором,
 
-		[EventDescription(GlobalSubsystemType.System, "База данных прибора не соответствует базе данных ПК", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.System, "База данных прибора не соответствует базе данных ПК", XStateClass.Unknown)]
 		База_данных_прибора_не_соответствует_базе_данных_ПК,
 
-		[EventDescription(GlobalSubsystemType.GK, "База данных прибора соответствует базе данных ПК", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "База данных прибора соответствует базе данных ПК", XStateClass.Unknown)]
 		База_данных_прибора_соответствует_базе_данных_ПК,
 
 		[EventDescription(GlobalSubsystemType.GK, "Применение конфигурации", XStateClass.Info)]
@@ -105,7 +105,7 @@ namespace FiresecAPI.Events
 		[EventDescription(GlobalSubsystemType.GK, "Отмена операции", XStateClass.Info)]
 		Отмена_операции,
 
-		[EventDescription(GlobalSubsystemType.GK, "ГК в технологическом режиме", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "ГК в технологическом режиме", XStateClass.TechnologicalRegime)]
 		ГК_в_технологическом_режиме,
 
 		[EventDescription(GlobalSubsystemType.GK, "ГК в рабочем режиме", XStateClass.Info)]
@@ -114,16 +114,16 @@ namespace FiresecAPI.Events
 		[EventDescription(GlobalSubsystemType.GK, "Запись всех идентификаторов", XStateClass.Info)]
 		Запись_всех_идентификаторов,
 
-		[EventDescription(GlobalSubsystemType.GK, "Перевод в технологический режим", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Перевод в технологический режим", XStateClass.TechnologicalRegime)]
 		Перевод_в_технологический_режим,
 
 		[EventDescription(GlobalSubsystemType.GK, "Синхронизация времени прибора с временем ПК", XStateClass.Info)]
 		Синхронизация_времени_прибора_с_временем_ПК,
 
-		[EventDescription(GlobalSubsystemType.GK, "Смена ПО", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Смена ПО", XStateClass.TechnologicalRegime)]
 		Смена_ПО,
 
-		[EventDescription(GlobalSubsystemType.GK, "Смена БД", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Смена БД", XStateClass.TechnologicalRegime)]
 		Смена_БД,
 
 		[EventDescription(GlobalSubsystemType.GK, "Перевод в рабочий режим", XStateClass.Info)]
@@ -135,7 +135,7 @@ namespace FiresecAPI.Events
 		[EventDescription(GlobalSubsystemType.GK, "Выход пользователя из прибора", XStateClass.Info)]
 		Выход_пользователя_из_прибора,
 
-		[EventDescription(GlobalSubsystemType.GK, "Ошибка управления", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Ошибка управления", XStateClass.Failure)]
 		Ошибка_управления,
 
 		[EventDescription(GlobalSubsystemType.GK, "Введен новый пользователь", XStateClass.Info)]
@@ -147,37 +147,37 @@ namespace FiresecAPI.Events
 		[EventDescription(GlobalSubsystemType.GK, "Произведена настройка сети", XStateClass.Info)]
 		Произведена_настройка_сети,
 
-		[EventDescription(GlobalSubsystemType.GK, "Неизвестный код события контроллекра", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Неизвестный код события контроллекра", XStateClass.Unknown)]
 		Неизвестный_код_события_контроллекра,
 
-		[EventDescription(GlobalSubsystemType.GK, "Неизвестный тип", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Неизвестный тип", XStateClass.Unknown)]
 		Неизвестный_тип,
 
-		[EventDescription(GlobalSubsystemType.GK, "Устройство с таким адресом не описано при конфигурации", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Устройство с таким адресом не описано при конфигурации", XStateClass.Unknown)]
 		Устройство_с_таким_адресом_не_описано_при_конфигурации,
 
-		[EventDescription(GlobalSubsystemType.GK, "Неизвестный код события устройства", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Неизвестный код события устройства", XStateClass.Unknown)]
 		Неизвестный_код_события_устройства,
 
-		[EventDescription(GlobalSubsystemType.GK, "При конфигурации описан другой тип", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "При конфигурации описан другой тип", XStateClass.Unknown)]
 		При_конфигурации_описан_другой_тип,
 
 		[EventDescription(GlobalSubsystemType.GK, "Изменился заводской номер", XStateClass.Info)]
 		Изменился_заводской_номер,
 
-		[EventDescription(GlobalSubsystemType.GK, "Пожар-1", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Пожар-1", XStateClass.Fire1)]
 		Пожар_1,
 
-		[EventDescription(GlobalSubsystemType.GK, "Сработка-1", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Сработка-1", XStateClass.Fire1)]
 		Сработка_1,
 
-		[EventDescription(GlobalSubsystemType.GK, "Пожар-2", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Пожар-2", XStateClass.Fire2)]
 		Пожар_2,
 
-		[EventDescription(GlobalSubsystemType.GK, "Сработка-2", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Сработка-2", XStateClass.Fire2)]
 		Сработка_2,
 
-		[EventDescription(GlobalSubsystemType.GK, "Внимание", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Внимание", XStateClass.Attention)]
 		Внимание,
 
 		[EventDescription(GlobalSubsystemType.GK, "Неисправность", XStateClass.Failure)]
@@ -186,16 +186,16 @@ namespace FiresecAPI.Events
 		[EventDescription(GlobalSubsystemType.GK, "Неисправность устранена", XStateClass.Norm)]
 		Неисправность_устранена,
 
-		[EventDescription(GlobalSubsystemType.GK, "Тест", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Тест", XStateClass.Test)]
 		Тест,
 
-		[EventDescription(GlobalSubsystemType.GK, "Тест устранен", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Тест устранен", XStateClass.Test)]
 		Тест_устранен,
 
-		[EventDescription(GlobalSubsystemType.GK, "Запыленность", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Запыленность", XStateClass.Service)]
 		Запыленность,
 
-		[EventDescription(GlobalSubsystemType.GK, "Запыленность устранена", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Запыленность устранена", XStateClass.Service)]
 		Запыленность_устранена,
 
 		[EventDescription(GlobalSubsystemType.GK, "Информация", XStateClass.Info)]
@@ -204,46 +204,46 @@ namespace FiresecAPI.Events
 		[EventDescription(GlobalSubsystemType.GK, "Отсчет задержки", XStateClass.Info)]
 		Отсчет_задержки,
 
-		[EventDescription(GlobalSubsystemType.GK, "Включено", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Включено", XStateClass.On)]
 		Включено,
 
-		[EventDescription(GlobalSubsystemType.GK, "Выключено", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Выключено", XStateClass.Off)]
 		Выключено,
 
-		[EventDescription(GlobalSubsystemType.GK, "Включается", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Включается", XStateClass.TurningOn)]
 		Включается,
 
-		[EventDescription(GlobalSubsystemType.GK, "Выключается", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Выключается", XStateClass.TurningOff)]
 		Выключается,
 
 		[EventDescription(GlobalSubsystemType.GK, "Кнопка", XStateClass.Info)]
 		Кнопка,
 
-		[EventDescription(GlobalSubsystemType.GK, "Изменение автоматики по неисправности", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Изменение автоматики по неисправности", XStateClass.AutoOff)]
 		Изменение_автоматики_по_неисправности,
 
-		[EventDescription(GlobalSubsystemType.GK, "Изменение автоматики по кнопке СТОП", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Изменение автоматики по кнопке СТОП", XStateClass.AutoOff)]
 		Изменение_автоматики_по_кнопке_СТОП,
 
-		[EventDescription(GlobalSubsystemType.GK, "Изменение автоматики по датчику ДВЕРИ-ОКНА", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Изменение автоматики по датчику ДВЕРИ-ОКНА", XStateClass.AutoOff)]
 		Изменение_автоматики_по_датчику_ДВЕРИ_ОКНА,
 
-		[EventDescription(GlobalSubsystemType.GK, "Изменение автоматики по ТМ", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Изменение автоматики по ТМ", XStateClass.AutoOff)]
 		Изменение_автоматики_по_ТМ,
 
-		[EventDescription(GlobalSubsystemType.GK, "Автоматика включена", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Автоматика включена", XStateClass.AutoOff)]
 		Автоматика_включена,
 
-		[EventDescription(GlobalSubsystemType.GK, "Ручной пуск АУП от ИПР", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Ручной пуск АУП от ИПР", XStateClass.On)]
 		Ручной_пуск_АУП_от_ИПР,
 
-		[EventDescription(GlobalSubsystemType.GK, "Отложенный пуск АУП по датчику ДВЕРИ-ОКНА", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Отложенный пуск АУП по датчику ДВЕРИ-ОКНА", XStateClass.On)]
 		Отложенный_пуск_АУП_по_датчику_ДВЕРИ_ОКНА,
 
-		[EventDescription(GlobalSubsystemType.GK, "Пуск АУП завершен", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Пуск АУП завершен", XStateClass.On)]
 		Пуск_АУП_завершен,
 
-		[EventDescription(GlobalSubsystemType.GK, "Останов тушения по кнопке СТОП", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Останов тушения по кнопке СТОП", XStateClass.Off)]
 		Останов_тушения_по_кнопке_СТОП,
 
 		[EventDescription(GlobalSubsystemType.GK, "Программирование мастер-ключа", XStateClass.Info)]
@@ -258,79 +258,79 @@ namespace FiresecAPI.Events
 		[EventDescription(GlobalSubsystemType.GK, "Уровень низкий", XStateClass.Info)]
 		Уровень_низкий,
 
-		[EventDescription(GlobalSubsystemType.GK, "Ход по команде с УЗЗ", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Ход по команде с УЗЗ", XStateClass.On)]
 		Ход_по_команде_с_УЗЗ,
 
-		[EventDescription(GlobalSubsystemType.GK, "У ДУ сообщение ПУСК НЕВОЗМОЖЕН", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "У ДУ сообщение ПУСК НЕВОЗМОЖЕН", XStateClass.Failure)]
 		У_ДУ_сообщение_ПУСК_НЕВОЗМОЖЕН,
 
-		[EventDescription(GlobalSubsystemType.GK, "Авария пневмоемкости", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Авария пневмоемкости", XStateClass.Failure)]
 		Авария_пневмоемкости,
 
-		[EventDescription(GlobalSubsystemType.GK, "Уровень аварийный", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Уровень аварийный", XStateClass.Failure)]
 		Уровень_аварийный,
 
-		[EventDescription(GlobalSubsystemType.GK, "Запрет пуска НС", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Запрет пуска НС", XStateClass.Off)]
 		Запрет_пуска_НС,
 
-		[EventDescription(GlobalSubsystemType.GK, "Запрет пуска компрессора", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Запрет пуска компрессора", XStateClass.Off)]
 		Запрет_пуска_компрессора,
 
 		[EventDescription(GlobalSubsystemType.GK, "Команда с УЗН", XStateClass.Info)]
 		Команда_с_УЗН,
 
-		[EventDescription(GlobalSubsystemType.GK, "Перевод в режим ручного управления", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Перевод в режим ручного управления", XStateClass.AutoOff)]
 		Перевод_в_режим_ручного_управления,
 
-		[EventDescription(GlobalSubsystemType.GK, "Состояние не определено", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Состояние не определено", XStateClass.Unknown)]
 		Состояние_не_определено,
 
-		[EventDescription(GlobalSubsystemType.GK, "Остановлено", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Остановлено", XStateClass.Off)]
 		Остановлено,
 
-		[EventDescription(GlobalSubsystemType.GK, "Состояние Неизвестно", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Состояние Неизвестно", XStateClass.Unknown)]
 		Состояние_Неизвестно,
 
-		[EventDescription(GlobalSubsystemType.GK, "Перевод в автоматический режим", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Перевод в автоматический режим", XStateClass.Norm)]
 		Перевод_в_автоматический_режим,
 
-		[EventDescription(GlobalSubsystemType.GK, "Перевод в ручной режим", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Перевод в ручной режим", XStateClass.AutoOff)]
 		Перевод_в_ручной_режим,
 
-		[EventDescription(GlobalSubsystemType.GK, "Перевод в отключенный режим", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Перевод в отключенный режим", XStateClass.Ignore)]
 		Перевод_в_отключенный_режим,
 
-		[EventDescription(GlobalSubsystemType.GK, "Перевод в неопределенный режим", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Перевод в неопределенный режим", XStateClass.Unknown)]
 		Перевод_в_неопределенный_режим,
 
 		[EventDescription(GlobalSubsystemType.GK, "Запись параметра", XStateClass.Info)]
 		Запись_параметра,
 
-		[EventDescription(GlobalSubsystemType.GK, "Норма", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Норма", XStateClass.Norm)]
 		Норма,
 
-		[EventDescription(GlobalSubsystemType.GK, "Неизвестный код события объекта", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.GK, "Неизвестный код события объекта", XStateClass.Unknown)]
 		Неизвестный_код_события_объекта,
 
-		[EventDescription(GlobalSubsystemType.SKD, "Потеря связи", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.SKD, "Потеря связи", XStateClass.ConnectionLost)]
 		Потеря_связи,
 
-		[EventDescription(GlobalSubsystemType.SKD, "Восстановление связи", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.SKD, "Восстановление связи", XStateClass.ConnectionLost)]
 		Восстановление_связи,
 
 		[EventDescription(GlobalSubsystemType.SKD, "Проход", XStateClass.Info)]
 		Проход,
 
-		[EventDescription(GlobalSubsystemType.SKD, "Дверь не закрыта", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.SKD, "Дверь не закрыта", XStateClass.Failure)]
 		Дверь_не_закрыта,
 
-		[EventDescription(GlobalSubsystemType.SKD, "Взлом", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.SKD, "Взлом", XStateClass.Attention)]
 		Взлом,
 
-		[EventDescription(GlobalSubsystemType.SKD, "Повторный_проход", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.SKD, "Повторный_проход", XStateClass.Attention)]
 		Повторный_проход,
 
-		[EventDescription(GlobalSubsystemType.SKD, "Принуждение", XStateClass.Info)]
+		[EventDescription(GlobalSubsystemType.SKD, "Принуждение", XStateClass.Attention)]
 		Принуждение,
 
 		[EventDescription(GlobalSubsystemType.SKD, "Запрос пароля", XStateClass.Info)]
---
+++ Projects/FireMonitor/Modules/JournalModule/ViewModels/ArchiveFilter/FilterObjectsViewModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Infrastructure.Common.Windows.ViewModels;
+using System.Collections.ObjectModel;
+using FiresecAPI.Events;
+using FiresecAPI.Automation;
+using FiresecAPI.Models;
+using FiresecAPI.SKD;
+
+namespace JournalModule.ViewModels
+{
+	public class FilterObjectsViewModel : BaseViewModel
+	{
+		public FilterObjectsViewModel(SKDArchiveFilter filter)
+		{
+			BuildTree();
+			Initialize(filter);
+		}
+
+		public void Initialize(SKDArchiveFilter filter)
+		{
+			foreach (var eventName in filter.EventNames)
+			{
+				var filterNameViewModel = AllFilters.FirstOrDefault(x => x.GlobalEventNameEnum == eventName);
+				if (filterNameViewModel != null)
+				{
+					filterNameViewModel.IsChecked = true;
+				}
+			}
+			foreach (var subsystemType in filter.SubsystemTypes)
+			{
+				var filterNameViewModel = AllFilters.FirstOrDefault(x => x.IsSubsystem && x.SubsystemType == subsystemType);
+				if (filterNameViewModel != null)
+				{
+					filterNameViewModel.IsChecked = true;
+				}
+			}
+		}
+
+		public SKDArchiveFilter GetModel()
+		{
+			var filter = new SKDArchiveFilter();
+			foreach (var rootFilter in RootFilters)
+			{
+				if (rootFilter.IsChecked)
+				{
+					filter.SubsystemTypes.Add(rootFilter.SubsystemType);
+				}
+				else
+				{
+					foreach (var filterViewModel in rootFilter.Children)
+					{
+						if (filterViewModel.IsChecked)
+						{
+							filter.EventNames.Add(filterViewModel.GlobalEventNameEnum);
+						}
+					}
+				}
+			}
+			return filter;
+		}
+
+		public List<FilterObjectViewModel> AllFilters;
+
+		public ObservableCollection<FilterObjectViewModel> RootFilters { get; private set; }
+
+		FilterObjectViewModel _selectedFilter;
+		public FilterObjectViewModel SelectedFilter
+		{
+			get { return _selectedFilter; }
+			set
+			{
+				_selectedFilter = value;
+				OnPropertyChanged(() => SelectedFilter);
+			}
+		}
+
+		void BuildTree()
+		{
+			RootFilters = new ObservableCollection<FilterObjectViewModel>();
+			AllFilters = new List<FilterObjectViewModel>();
+
+			var gkViewModel = new FilterObjectViewModel(GlobalSubsystemType.GK);
+			gkViewModel.IsExpanded = true;
+			RootFilters.Add(gkViewModel);
+
+			var gkDevicesViewModel = new FilterObjectViewModel(GlobalSubsystemType.GK);
+			gkViewModel.AddChild(gkDevicesViewModel);
+
+			foreach (var childDevice in FiresecClient.XManager.DeviceConfiguration.RootDevice.Children)
+			{
+				AddDeviceInternal(childDevice, gkDevicesViewModel);
+			}
+
+			var skdViewModel = new FilterObjectViewModel(GlobalSubsystemType.SKD);
+			skdViewModel.IsExpanded = true;
+			RootFilters.Add(skdViewModel);
+			
+		}
+
+		FilterObjectViewModel AddDeviceInternal(FiresecAPI.GK.XDevice device, FilterObjectViewModel parentDeviceViewModel)
+		{
+			var deviceViewModel = new FilterObjectViewModel(device);
+			if (parentDeviceViewModel != null)
+				parentDeviceViewModel.AddChild(deviceViewModel);
+
+			foreach (var childDevice in device.Children)
+			{
+				if (!childDevice.IsNotUsed)
+				{
+					AddDeviceInternal(childDevice, deviceViewModel);
+				}
+			}
+			return deviceViewModel;
+		}
+	}
+}
