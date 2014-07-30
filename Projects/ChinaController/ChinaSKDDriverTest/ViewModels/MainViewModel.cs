b4924a8f2e5da707c32c504a1bd4c7f59f606c60
Sevastyanov Alexander <none>
2014-07-30 23:59:37 +0400

    remove unused prohects

--- Projects/ChinaController/ChinaSKDDriverTest/ViewModels/MainViewModel.cs
+++ Projects/ChinaController/ChinaSKDDriverTest/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using ChinaSKDDriver;
 using Infrastructure.Common.Windows.ViewModels;
+using Infrastructure.Common;
 
 namespace ControllerSDK.ViewModels
 {
@@ -32,6 +33,32 @@ namespace ControllerSDK.ViewModels
 			CommonViewModel = new CommonViewModel();
 			DoorConfigurationViewModel = new DoorConfigurationViewModel();
 			LogItemsViewModel = new LogItemsViewModel();
+
+			ConnectCommand = new RelayCommand(OnConnect);
+			DisconnectCommand = new RelayCommand(OnDisconnect);
+			OnConnect();
+		}
+
+		public RelayCommand ConnectCommand { get; private set; }
+		void OnConnect()
+		{
+			Wrapper.Initialize();
+			string error;
+			MainViewModel.Wrapper.Connect("172.16.6.54", 37777, "system", "123456", out error);
+
+			//string error;
+			//var loginID = MainViewModel.Wrapper.Connect("172.16.6.54", 37777, "system", "123456", out error);
+			//_textBox.Text += "LoginID = " + loginID + " " + error + "\n";
+			//Wrapper.Start();
+		}
+
+		public RelayCommand DisconnectCommand { get; private set; }
+		void OnDisconnect()
+		{
+			MainViewModel.Wrapper.Disconnect();
+			//Wrapper.WrapStop();
+			//var result = MainViewModel.Wrapper.WrapDisconnect();
+			//_textBox.Text += "result = " + result + "\n";
 		}
 	}
 }
--- Projects/ChinaController/ChinaSKDDriverTest/Views/MainWindow.xaml.cs
+++ Projects/ChinaController/ChinaSKDDriverTest/Views/MainWindow.xaml.cs
@@ -16,30 +16,9 @@ namespace ControllerSDK.Views
 			MainViewModel.Wrapper = new Wrapper();
 			MainViewModel = new MainViewModel();
 			DataContext = MainViewModel;
-			OnConnect(this, null);
 		}
 
 		public MainViewModel MainViewModel { get; private set; }
-
-		void OnConnect(object sender, RoutedEventArgs e)
-		{
-			Wrapper.Initialize();
-			string error;
-			MainViewModel.Wrapper.Connect("172.16.6.54", 37777, "system", "123456", out error);
-
-			//string error;
-			//var loginID = MainViewModel.Wrapper.Connect("172.16.6.54", 37777, "system", "123456", out error);
-			//_textBox.Text += "LoginID = " + loginID + " " + error + "\n";
-			//Wrapper.Start();
-		}
-
-		void OnDisconnect(object sender, RoutedEventArgs e)
-		{
-			MainViewModel.Wrapper.Disconnect();
-			//Wrapper.WrapStop();
-			//var result = MainViewModel.Wrapper.WrapDisconnect();
-			//_textBox.Text += "result = " + result + "\n";
-		}
 
 		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
 		{
