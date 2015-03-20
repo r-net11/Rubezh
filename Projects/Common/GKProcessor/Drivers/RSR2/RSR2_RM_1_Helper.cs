e4532ff405408b40389a5afa703b80bdf19237b1
Roman Kazakov <kazakovrb@rubezh.ru>
2015-03-20 10:45:32 +0300

    fix

--- Projects/Common/GKProcessor/Drivers/RSR2/RSR2_RM_1_Helper.cs
+++ Projects/Common/GKProcessor/Drivers/RSR2/RSR2_RM_1_Helper.cs
@@ -22,6 +22,7 @@ namespace GKProcessor
 			GKDriversHelper.AddControlAvailableStates(driver);
 			driver.AvailableStateBits.Add(GKStateBit.Off);
 			driver.AvailableStateBits.Add(GKStateBit.TurningOn);
+			driver.AvailableStateBits.Add(GKStateBit.TurningOff);
 			GKDriversHelper.AddAvailableStateClasses(driver, XStateClass.AutoOff);
 			GKDriversHelper.AddAvailableStateClasses(driver, XStateClass.On);
 			GKDriversHelper.AddAvailableStateClasses(driver, XStateClass.TurningOn);
