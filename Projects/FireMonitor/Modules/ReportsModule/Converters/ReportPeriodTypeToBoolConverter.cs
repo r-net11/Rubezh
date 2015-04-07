b64483ee22bbeaf10a83a4d3e6f05883a346dff6
Alexey Oleynichenko <oleynichenkoal@rvigroup.ru>
2015-04-07 13:52:49 +0300

    Change Reports DateTime component behavior

---
+++ Projects/FireMonitor/Modules/ReportsModule/Converters/ReportPeriodTypeToBoolConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using FiresecAPI.SKD.ReportFilters;
+
+namespace ReportsModule.Converters
+{
+	public class ReportPeriodTypeToBoolConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return (ReportPeriodType)value == ReportPeriodType.Arbitrary;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
