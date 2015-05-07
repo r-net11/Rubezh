e534a9f22024f34a561685c8af5361b21cc6a5b0
KonstantinDavidov <konstantin.davidov1@gmail.com>
2015-05-07 08:07:31 +0300

    add missing file

---
+++ Projects/FireMonitor/Modules/VideoModule/Converters/Comparers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Infrastructure.Common.TreeList;
+using VideoModule.ViewModels;
+
+namespace VideoModule.Converters
+{
+	public class CameraViewModelNameComparer : TreeNodeComparer<CameraViewModel>
+	{
+		protected override int Compare(CameraViewModel x, CameraViewModel y)
+		{
+			if (x.Camera != null && y.Camera != null)
+				return string.Compare(x.Camera.Name, y.Camera.Name);
+			else 
+				return Compare(x, y);
+		}
+	}
+
+	public class CamerasViewModelAddressComparer : TreeNodeComparer<CameraViewModel>
+	{
+		protected override int Compare(CameraViewModel x, CameraViewModel y)
+		{
+			return string.Compare(x.PresentationAddress, y.PresentationAddress);
+		}
+	}
+
+}
