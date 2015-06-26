52f07389ed3bdd55fe4fd4471be0c030ff0f8587
Mikhail Zemskov <dreamwalkerzmm@gmail.com>
2015-06-26 13:49:12 +0300

    Fixed the name of configuration properties

--- Projects/GKOPCServer/GKOPCServer/Bootstrapper.cs
+++ Projects/GKOPCServer/GKOPCServer/Bootstrapper.cs
@@ -37,7 +37,7 @@ namespace GKOPCServer
 			UILogger.Log("Соединение с сервером");
 			for (int i = 1; i <= 10; i++)
 			{
-				var message = FiresecManager.Connect(ClientType.OPC, ConnectionSettingsManager.ServerAddress, GlobalSettingsHelper.GlobalSettings.Login, GlobalSettingsHelper.GlobalSettings.Password);
+				var message = FiresecManager.Connect(ClientType.OPC, ConnectionSettingsManager.ServerAddress, GlobalSettingsHelper.GlobalSettings.AdminLogin, GlobalSettingsHelper.GlobalSettings.AdminPassword);
 				if (message == null)
 					break;
 				Thread.Sleep(5000);
