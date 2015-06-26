3045c735dca94ff31c9f7be65c6acb88592b835d
zabe110 <zabe110@yandex.ru>
2015-06-26 16:27:13 +0300

    Defender

---
+++ Projects/Common/Defender/Defender/InitialKey.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Defender
+{
+    public class InitialKey
+    {
+        public string Value { get; private set; }
+        public byte[] HashCode { get; private set; }
+
+        public InitialKey(string value)
+        {
+            this.Value = value;
+            if (value != null)
+                this.HashCode = GenerateHashCode(value);
+        }
+
+        byte[] GenerateHashCode(string key)
+        {
+            byte[] keyBytes = new byte[key.Length * sizeof(char)];
+            Buffer.BlockCopy(key.ToCharArray(), 0, keyBytes, 0, keyBytes.Length);
+            return new SHA256Managed().ComputeHash(keyBytes);
+        }
+
+        public bool SaveToFile(string fileName)
+        {
+            try
+            {
+                using (var file = File.OpenWrite(fileName))
+                {
+                    file.Write(HashCode, 0, HashCode.Length);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static InitialKey Generate()
+        {
+            string value = HardwareInfo.CpuId + HardwareInfo.HardDiskId;
+            return new InitialKey(value);
+        }
+
+        public static InitialKey FromFile(string fileName)
+        {
+            try
+            {
+                InitialKey loadedKey = new InitialKey(null);
+
+                using (var file = File.OpenRead(fileName))
+                {
+                    loadedKey.HashCode = new byte[file.Length];
+                    file.Read(loadedKey.HashCode, 0, loadedKey.HashCode.Length);
+                }
+                return loadedKey;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static bool operator ==(InitialKey key1, InitialKey key2)
+        {
+            if (Equals(key1, null) && Equals(key2, null))
+                return true;
+
+            if (Equals(key1, null) || Equals(key2, null) || key1.HashCode == null || key2.HashCode == null || key1.HashCode.Length != key2.HashCode.Length)
+                return false;
+
+            for (int i = 0; i < key1.HashCode.Length; i++)
+                if (key1.HashCode[i] != key2.HashCode[i])
+                    return false;
+
+            return true;
+        }
+
+        public static bool operator !=(InitialKey key1, InitialKey key2)
+        {
+            return !(key1 == key2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is InitialKey)
+                return this == (InitialKey)obj;
+            else
+                return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode == null ? base.GetHashCode() : HashCode.Sum(x => (int)x);
+        }
+
+        public override string ToString()
+        {
+            return HashCode == null || HashCode.Length == 0 ? "-" : "0x" + BitConverter.ToString(HashCode).Replace("-", String.Empty);
+        }
+    }
+}
---
+++ Projects/Common/Defender/Defender/LicenseParameter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Defender
+{
+    public class LicenseParameter
+    {
+        public string Name { get; set; }
+        
+        public LicenseParameterType Type
+        { 
+            get
+            {
+                return GetLicenseParameterType(_value);
+            }
+        }
+
+        object _value;
+        public object Value 
+        { 
+            get { return _value; }
+            set
+            {
+                if (value == null)
+                    value = String.Empty;
+
+                switch (GetLicenseParameterType(value))
+                {
+                    case LicenseParameterType.Boolean:
+                    case LicenseParameterType.DateTime:
+                        _value = value;
+                        break;
+
+                    case LicenseParameterType.Float:
+                        _value = Convert.ToDouble(value);
+                        break;
+
+                    case LicenseParameterType.Integer:
+                        _value = Convert.ToInt32(value);
+                        break;
+                    
+                    case LicenseParameterType.String:
+                        _value = value.ToString();
+                        break;
+                }
+            }
+        }
+
+        public LicenseParameter()
+        {
+
+        }
+
+        public LicenseParameter(string name, object value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+
+        LicenseParameterType GetLicenseParameterType(object value)
+        {
+            if (value.GetType() == System.Type.GetType("System.Boolean"))
+                    return LicenseParameterType.Boolean;
+
+            if (value.GetType() == System.Type.GetType("System.DateTime"))
+                return LicenseParameterType.DateTime;
+
+            if (value.GetType() == System.Type.GetType("System.Int16") ||
+                value.GetType() == System.Type.GetType("System.Int32") ||
+                value.GetType() == System.Type.GetType("System.UInt16") ||
+                value.GetType() == System.Type.GetType("System.UInt32"))
+                return LicenseParameterType.Integer;
+
+            if (value.GetType() == System.Type.GetType("System.Single") ||
+                 value.GetType() == System.Type.GetType("System.Double") ||
+                 value.GetType() == System.Type.GetType("System.Decimal"))
+                return LicenseParameterType.Float;
+
+            return LicenseParameterType.String;
+        }
+    }
+}
