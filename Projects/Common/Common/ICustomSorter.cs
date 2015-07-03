6763e0dd8803a99eae1e396fd929b0af44f3414b
KonstantinDavidov <konstantin.davidov1@gmail.com>
2015-07-03 09:29:41 +0300

    SKDDEV-597

---
+++ Projects/Common/Common/ICustomSorter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Common
+{
+	public interface ICustomSorter : IComparer
+	{
+		ListSortDirection SortDirection { get; set; }
+	}
+}
---
+++ Projects/FireMonitor/Modules/SKDModule/Employees/Comparers/DoorsComparer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Common;
+using SKDModule.ViewModels;
+
+namespace SKDModule.Employees.Comparers
+{
+	public class DoorsComparer : ICustomSorter
+	{
+		public DoorsComparer()
+		{
+		}
+
+		private const string Always = "<Всегда>";
+		private const string Never = "<Никогда>";
+		public ListSortDirection SortDirection { get; set; }
+
+		public int Compare(object x, object y)
+		{
+			var firstModel = x as ReadOnlyAccessDoorViewModel;
+			var secondModel = y as ReadOnlyAccessDoorViewModel;
+			if (firstModel == null || secondModel == null) return 0;
+
+			if (SortDirection == ListSortDirection.Ascending)
+			{
+				if (string.Equals(firstModel.EnerScheduleName, secondModel.EnerScheduleName)) return 0;
+				if (string.Equals(firstModel.EnerScheduleName, Always) && string.Equals(secondModel.EnerScheduleName, Never)) return -1;
+				if (string.Equals(firstModel.EnerScheduleName, Never) && string.Equals(secondModel.EnerScheduleName, Always)) return 1;
+			}
+
+			if (SortDirection == ListSortDirection.Descending)
+			{
+				if (string.Equals(firstModel.EnerScheduleName, secondModel.EnerScheduleName)) return 0;
+				if (string.Equals(firstModel.EnerScheduleName, Always) && string.Equals(secondModel.EnerScheduleName, Never)) return 1;
+				if (string.Equals(firstModel.EnerScheduleName, Never) && string.Equals(secondModel.EnerScheduleName, Always)) return -1;
+			}
+
+			return string.Compare(firstModel.EnerScheduleName, secondModel.EnerScheduleName);
+		}
+	}
+}
