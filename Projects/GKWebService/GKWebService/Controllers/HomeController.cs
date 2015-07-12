cdba68cbaaa565d349860814d7230d49b858af32
Влад <vlad.gornostaev@list.r>
2015-07-13 00:54:45 +0400

    Логин и грид

--- Projects/GKWebService/GKWebService/Controllers/HomeController.cs
+++ Projects/GKWebService/GKWebService/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GKWebService.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -22,6 +23,45 @@ namespace GKWebService.Controllers
         public ActionResult Device()
         {
             return View();
+        }
+
+        public ActionResult Report()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Logon(string login, string password)
+        {
+            string error = null;
+
+            if(!login.Equals("admin") || !password.Equals("admin"))
+            {
+                error = "Неверный логин или пароль";
+            }
+
+            return Json(new { Success = error == null, Message = error });
+        }
+
+        [HttpPost]
+        [HttpGet]
+        public JsonResult GetReports()
+        {
+            List<ReportModel> list = new List<ReportModel>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(new ReportModel()
+                {
+                    Desc = "Описание" + i.ToString(),
+                    DeviceDate = DateTime.Now,
+                    Name = "Назваине" + i.ToString(),
+                    Object = "Объект" + i.ToString(),
+                    SystemDate = DateTime.Now
+                });
+            }
+
+            return Json(list);
         }
     }
 }
---
+++ Projects/GKWebService/GKWebService/Scripts/report.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+    $("#jqGrid").jqGrid({
+        url: '/Home/GetReports/',
+        datatype: "json",
+        colModel: [
+           { label: 'Дата в приборе', name: 'DeviceDate', width: 75, sortable: false },
+           { label: 'Дата в системе', name: 'SystemDate', width: 90, sortable: false },
+           { label: 'Название', name: 'Name', width: 100, sortable: false },
+           { label: 'Уточнение', name: 'Desc', width: 80, sortable: false },
+           { label: 'Объект', name: 'Object', width: 80, sortable: false }
+        ],
+        //viewrecords: true, // show the current page, data rang and total records on the toolbar
+        width: 900,
+        height: 400,
+        rowNum: 30,
+        pager: "#jqGridPager"
+    });
+});
