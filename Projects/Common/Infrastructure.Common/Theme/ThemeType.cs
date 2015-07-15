5ae0c632b4b1c9499aa910d87e5119bc0fd6efd1
Mikhail Zemskov <dreamwalkerzmm@gmail.com>
2015-07-15 12:56:03 +0300

    Revert "Theme changes (Some inconvenient resource names changed)"
    This reverts commit 5847c5d987a11e25f4c8fdf0176d5ef9cdb5b7f4.
    
    Conflicts:
    	Projects/Common/Controls/Themes/ActiveXDataGridStyle.xaml
    	Projects/Common/Controls/Themes/ActiveXScrollBarStyle.xaml
    	Projects/Common/Controls/Themes/BlueTheme.xaml
    	Projects/Common/Controls/Themes/GrayTheme.xaml
    	Projects/Common/Controls/Themes/LightTheme.xaml
    	Projects/Common/Controls/Themes/StrazhTheme.xaml
    	Projects/Common/Controls/XamlImages/logo.xaml

--- Projects/Common/Infrastructure.Common/Theme/ThemeType.cs
+++ Projects/Common/Infrastructure.Common/Theme/ThemeType.cs
@@ -10,8 +10,8 @@ namespace Infrastructure.Common.Theme
 		[DescriptionAttribute("Серая тема")]
 		GrayTheme,
 
-		[DescriptionAttribute("Голубая тема")]
-		SkyblueTheme,
+		[DescriptionAttribute("Страж тема")]
+		StrazhTheme,
 
 		[DescriptionAttribute("Светлая тема")]
 		LightTheme,
