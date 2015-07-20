6c9eaa33766c654c65757b09b2b188c5b27d81e8
KovalevRA <KovalevRA@rubezh.ru>
2015-07-20 11:07:47 +0300

    Common files

---
+++ Projects/Common/SKDDriver/DataClasses/GKSchedule/GKDaySchedule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SKDDriver.DataClasses
+{
+	public class GKDaySchedule
+	{
+		public GKDaySchedule()
+		{
+			ScheduleGKDaySchedules = new List<ScheduleGKDaySchedule>();
+			GKDayScheduleParts = new List<GKDaySchedulePart>();
+		}
+		
+		[Key]
+		public Guid UID { get; set; }
+
+		public ICollection<ScheduleGKDaySchedule> ScheduleGKDaySchedules { get; set; }
+
+		public ICollection<GKDaySchedulePart> GKDayScheduleParts { get; set; }
+
+		public int No { get; set; }
+		[MaxLength(50)]
+		public string Name { get; set; }
+		[MaxLength(50)]
+		public string Description { get; set; }
+	}
+}
