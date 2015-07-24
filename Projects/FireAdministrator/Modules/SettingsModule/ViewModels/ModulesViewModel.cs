54febe4a59e44663bc968393715d093dfbfe4077
KonstantinDavidov <konstantin.davidov1@gmail.com>
2015-07-24 09:43:19 +0300

    SKDDEV-649

--- Projects/FireAdministrator/Modules/SettingsModule/ViewModels/ModulesViewModel.cs
+++ Projects/FireAdministrator/Modules/SettingsModule/ViewModels/ModulesViewModel.cs
@@ -12,9 +12,7 @@ namespace SettingsModule.ViewModels
 			Modules = new List<ModuleViewModel>();
 
 			Modules.Add(new ModuleViewModel("PlansModule.dll", "Планы"));
-			Modules.Add(new ModuleViewModel("PlansModule.Kursk.dll", "Планы. Курск"));
 			Modules.Add(new ModuleViewModel("SecurityModule.dll", "Пользователи"));
-			Modules.Add(new ModuleViewModel("GKModule.dll", "ГК"));
 			Modules.Add(new ModuleViewModel("SKDModule.dll", "СКД"));
 			Modules.Add(new ModuleViewModel("StrazhModule.dll", "Страж"));
 			Modules.Add(new ModuleViewModel("VideoModule.dll", "Видео"));
@@ -23,7 +21,6 @@ namespace SettingsModule.ViewModels
 			Modules.Add(new ModuleViewModel("ReportsModule.dll", "Отчеты"));
 			Modules.Add(new ModuleViewModel("FiltersModule.dll", "Фильтры"));
 			Modules.Add(new ModuleViewModel("JournalModule.dll", "Журнал событий"));
-			Modules.Add(new ModuleViewModel("SoundsModule.dll", "Звуки"));
 			Modules.Add(new ModuleViewModel("SettingsModule.dll", "Настройки", false));
 			Modules.Add(new ModuleViewModel("DiagnosticsModule.dll", "Диагностика"));
 
