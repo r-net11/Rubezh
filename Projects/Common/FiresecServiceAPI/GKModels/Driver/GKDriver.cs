9aa7956da845a0e6e187a9fa5bdb2d0f93c649ee
Kiselev Andrey <none>
2015-07-27 14:28:39 +0300

    .

--- Projects/Common/FiresecServiceAPI/GKModels/Driver/GKDriver.cs
+++ Projects/Common/FiresecServiceAPI/GKModels/Driver/GKDriver.cs
@@ -45,7 +45,7 @@ namespace FiresecAPI.GK
 		public bool IsRangeEnabled { get; set; }
 		public bool IsAutoCreate { get; set; }
 		public byte MinAddress { get; set; }
-		public byte MaxAddress { get; set; }
+		public int MaxAddress { get; set; }
 		public byte MinAddress2 { get; set; }
 		public byte MaxAddress2 { get; set; }
 		public byte MaxAddressOnShleif { get; set; }
