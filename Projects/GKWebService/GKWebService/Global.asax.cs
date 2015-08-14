3c06a9efe670bd51a4059da08f5a8d4ecd5e89ac
Sergey Durnov <durnov@outlook.com>
2015-08-14 19:32:04 +0300

    Интеграция страницы планов: 1) Обновил nuget пакеты (кроме ASP.net), добавил недостающие. 2) Часть скриптов перенесена из неотслеживаемых скриптов в пакеты 3) Добавил страницу планов, webapi контроллер, модель

--- Projects/GKWebService/GKWebService/Global.asax.cs
+++ Projects/GKWebService/GKWebService/Global.asax.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace GKWebService
@@ -11,9 +13,12 @@ namespace GKWebService
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            Bootstrapper.Run();
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
+            //Bootstrapper.Run();
         }
     }
 }
---
+++ Projects/GKWebService/GKWebService/Scripts/app/controllers/planCtrl.js
@@ -0,0 +1,37 @@
+﻿(function ()
+{
+    'use strict';
+
+    var app = angular.module('canvasApp.controllers', ['ui.bootstrap']).controller('PlanCtrl', [
+        '$scope', 'dataFactory', '$modal', function ($scope, dataFactory, $modal)
+        {
+            // Задаем модальное окно для контекстного меню фигуры
+            $scope.modal = {
+                instance: null
+            };
+
+            $scope.ShowModal = function (size, html)
+            {
+                $scope.modal.instance = $modal.open({
+                    template: html,
+                    scope: $scope
+                });
+            };
+
+            // Получаем данные для отображения плана
+            dataFactory.getShapesData(function (results)
+            {
+                $scope.message = "Request for shapes recieved!";
+                $scope.d3Data = results;
+            });
+            //$scope.d3OnClick = function() { alert("Click"); };
+        }
+    ]);
+
+    app.factory('dataFactory', function ($http)
+    {
+        return {
+            getShapesData: function (callback) { $http.get('../api/Shapes').success(callback); }
+        };
+    });
+}());
