d9e0f3632e1802adecbd399e4b442263855226f2
KonstantinDavidov <konstantin.davidov1@gmail.com>
2015-08-26 08:37:26 +0300

    SKDDEV-659

--- Projects/Common/Controls/ListViewLayoutManager/DynamicProportionalColumn.cs
+++ Projects/Common/Controls/ListViewLayoutManager/DynamicProportionalColumn.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace Controls.ListViewLayoutManager
@@ -23,34 +19,28 @@ namespace Controls.ListViewLayoutManager
 
 		private DynamicProportionalColumn()
 		{
-		} // DynamicProportionalColumn
+		}
 
-		// ----------------------------------------------------------------------
 		public static double GetWidth(DependencyObject obj)
 		{
 			return (double)obj.GetValue(WidthProperty);
-		} // GetWidth
+		}
 
-		// ----------------------------------------------------------------------
 		public static void SetWidth(DependencyObject obj, double width)
 		{
 			obj.SetValue(WidthProperty, width);
-		} // SetWidth
+		}
 
-		// ----------------------------------------------------------------------
 		public static double GetMinWidth(DependencyObject obj)
 		{
 			return (double)obj.GetValue(MinWidthProperty);
-		} // GetMinWidth
+		}
 
-		// ----------------------------------------------------------------------
 		public static void SetMinWidth(DependencyObject obj, double minWidth)
 		{
 			obj.SetValue(MinWidthProperty, minWidth);
-		} // SetMinWidth
+		}
 
-
-		// ----------------------------------------------------------------------
 		public static bool IsDynamicProportionalColumn(GridViewColumn column)
 		{
 			if (column == null)
@@ -58,21 +48,18 @@ namespace Controls.ListViewLayoutManager
 				return false;
 			}
 			return HasPropertyValue(column, WidthProperty);
-		} // IsProportionalColumn
+		}
 
-		// ----------------------------------------------------------------------
 		public static double? GetProportionalWidth(GridViewColumn column)
 		{
 
 			return GetColumnWidth(column, WidthProperty);
-		} // GetProportionalWidth
+		}
 
-		// ----------------------------------------------------------------------
 		public static GridViewColumn ApplyWidth(GridViewColumn gridViewColumn, double width)
 		{
 			SetWidth(gridViewColumn, width);
 			return gridViewColumn;
-		} // ApplyWidth
-
-	} // class DynamicProportionalColumn
+		}
+	}
 }
