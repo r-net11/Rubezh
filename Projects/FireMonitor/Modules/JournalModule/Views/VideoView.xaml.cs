ea21f4fdd08d475048038e2af7d0c68fb919f456
Kovalev Roman <KovalevRA@rubezh.ru>
2015-09-08 12:24:54 +0300

    ОЗ. Видеоячейка добавлена в Журнал событий.

--- Projects/FireMonitor/Modules/JournalModule/Views/VideoView.xaml.cs
+++ Projects/FireMonitor/Modules/JournalModule/Views/VideoView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using JournalModule.ViewModels;
+using MediaSourcePlayer.MediaSource;
+using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace JournalModule.Views
 {
@@ -7,6 +11,24 @@ namespace JournalModule.Views
 		public VideoView()
 		{
 			InitializeComponent();
+			Loaded += OnLoaded;
+		}
+		private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+		{
+			var videoViewModel = DataContext as VideoViewModel;
+			{
+				if (videoViewModel.VideoPath != null)
+				{
+					videoViewModel.OnClose += Close;
+					MediaSourcePlayer.Open(MediaSourceFactory.GetMediaSource(new Uri(videoViewModel.VideoPath)));
+					MediaSourcePlayer.Play();
+				}
+			}
+		}
+		private void Close()
+		{
+			MediaSourcePlayer.Stop();
+			MediaSourcePlayer.Close();
 		}
 	}
 }
