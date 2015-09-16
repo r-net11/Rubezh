0dc0995697b75e3158c2ecdc3bd108a12b87973d
Petrov D <petrovdy@rubezh.ru>
2015-09-16 14:37:30 +0300

    Создал общий каркас сетевых служб

---
+++ Projects/ResourceAccountingSystem/Devices/OSI/Messages/IMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubezhResurs.OSI.Messages
+{
+    /// <summary>
+    /// Сетевое сообщение 
+    /// </summary>
+    public interface IMessage
+    {
+        /// <summary>
+        /// Уникальный идентификатор сообщения
+        /// </summary>
+        Guid MessageId { get; }
+        /// <summary>
+        /// Время отравки сообщения. Если сообщение не отпрвлено содержит null
+        /// </summary>
+        DateTime? SendingTime { get; set; }
+        /// <summary>
+        /// Возвращает сообщение в виде массива байт
+        /// </summary>
+        /// <returns></returns>
+        Byte[] ToArray();
+    }
+}
