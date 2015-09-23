b1219d5ea3afc83999801728aa3e5a7cb5c68b60
Kovalev Roman <KovalevRA@rubezh.ru>
2015-09-23 14:47:59 +0300

    ОЗ. Отчеты. Дисциплинарный отчет. Исправлены замечания.

--- Projects/FiresecService/FiresecService.Report/Templates/DisciplineReport.cs
+++ Projects/FiresecService/FiresecService.Report/Templates/DisciplineReport.cs
@@ -47,57 +47,81 @@ namespace FiresecService.Report.Templates
 					foreach (var dayTimeTrack in timeTrackEmployeeResult.DayTimeTracks)
 					{
 						dayTimeTrack.Calculate();
-
-						var dataRow = dataSet.Data.NewDataRow();
-						dataRow.Employee = employee.Name;
-						dataRow.Organisation = employee.Organisation;
-						dataRow.Department = employee.Department;
-
-						dataRow.Date = dayTimeTrack.Date;
-
-						if (dayTimeTrack.RealTimeTrackParts.Count > 0)
+						bool hasEarlyLive;
+						bool hasLate;
+						if (filter.ShowWithoutTolerance)
 						{
-							dataRow.FirstEnter = dayTimeTrack.RealTimeTrackParts.Min(x => x.StartTime);
-							dataRow.LastExit = dayTimeTrack.RealTimeTrackParts.Max(x => x.EndTime);
+							hasEarlyLive = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.EarlyLeave).TimeSpan > TimeSpan.Zero;
+							hasLate = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.Late).TimeSpan > TimeSpan.Zero;
 						}
-
-						var absence = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.Absence);
-						if (absence != null)
+						else
 						{
-							dataRow.Absence = absence.TimeSpan;
+							hasEarlyLive = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.EarlyLeave).TimeSpan > dayTimeTrack.AllowedEarlyLeave;
+							hasLate = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.Late).TimeSpan > dayTimeTrack.AllowedLate;
 						}
-
-						var late = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.Late);
-						if (late != null)
+						var hasAbcence = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.Absence).TimeSpan > TimeSpan.Zero;
+						var hasOvertime = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.Overtime).TimeSpan > TimeSpan.Zero;
+						var hasDocument = dayTimeTrack.Totals.Where(x => x.TimeTrackType == TimeTrackType.DocumentPresence ||
+							x.TimeTrackType == TimeTrackType.DocumentOvertime ||
+							x.TimeTrackType == TimeTrackType.DocumentAbsence).Any(x=>x.TimeSpan > TimeSpan.Zero);
+						var isShow = filter.ShowEarlуRetirement && hasEarlyLive ||
+								filter.ShowDelay && hasLate ||
+								filter.ShowAbsence && hasAbcence ||
+								filter.ShowOvertime && hasOvertime ||
+								filter.ShowConfirmed && hasDocument;
+						if (isShow)
 						{
-							dataRow.Late = late.TimeSpan;
-						}
+							var dataRow = dataSet.Data.NewDataRow();
+							dataRow.Employee = employee.Name;
+							dataRow.Organisation = employee.Organisation;
+							dataRow.Department = employee.Department;
 
-						var earlyLeave = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.EarlyLeave);
-						if (earlyLeave != null)
-						{
-							dataRow.EarlyLeave = earlyLeave.TimeSpan;
-						}
+							dataRow.Date = dayTimeTrack.Date;
 
-						var overtime = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.Overtime);
-						if (overtime != null)
-						{
-							dataRow.Overtime = overtime.TimeSpan;
-						}
+							if (dayTimeTrack.RealTimeTrackParts.Count > 0)
+							{
+								dataRow.FirstEnter = dayTimeTrack.RealTimeTrackParts.Min(x => x.StartTime);
+								dataRow.LastExit = dayTimeTrack.RealTimeTrackParts.Max(x => x.EndTime);
+							}
 
-						var isHoliday = dayTimeTrack.IsHoliday;
+							var absence = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.Absence);
+							if (absence != null)
+							{
+								dataRow.Absence = absence.TimeSpan;
+							}
 
-						var document = timeTrackEmployeeResult.Documents.FirstOrDefault(x => (dayTimeTrack.Date >= x.StartDateTime.Date && dayTimeTrack.Date <= x.EndDateTime.Date) && x.TimeTrackDocumentType.ShortName == dayTimeTrack.LetterCode);
-						if (document != null)
-						{
-							dataRow.DocumentDate = document.DocumentDateTime;
-							dataRow.DocumentName = document.TimeTrackDocumentType.Name;
-							dataRow.DocumentNo = document.DocumentNumber.ToString();
-						}
-						
-						if (absence.TimeSpan.TotalSeconds > 0 || late.TimeSpan.TotalSeconds > 0 || earlyLeave.TimeSpan.TotalSeconds > 0 || overtime.TimeSpan.TotalSeconds > 0 || document != null)
-						{
-							dataSet.Data.Rows.Add(dataRow);
+							var late = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.Late);
+							if (late != null)
+							{
+								dataRow.Late = late.TimeSpan;
+							}
+
+							var earlyLeave = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.EarlyLeave);
+							if (earlyLeave != null)
+							{
+								dataRow.EarlyLeave = earlyLeave.TimeSpan;
+							}
+
+							var overtime = dayTimeTrack.Totals.FirstOrDefault(x => x.TimeTrackType == TimeTrackType.Overtime);
+							if (overtime != null)
+							{
+								dataRow.Overtime = overtime.TimeSpan;
+							}
+
+							var isHoliday = dayTimeTrack.IsHoliday;
+
+							var document = timeTrackEmployeeResult.Documents.FirstOrDefault(x => (dayTimeTrack.Date >= x.StartDateTime.Date && dayTimeTrack.Date <= x.EndDateTime.Date) && x.TimeTrackDocumentType.ShortName == dayTimeTrack.LetterCode);
+							if (document != null)
+							{
+								dataRow.DocumentDate = document.DocumentDateTime;
+								dataRow.DocumentName = document.TimeTrackDocumentType.Name;
+								dataRow.DocumentNo = document.DocumentNumber.ToString();
+							}
+
+							if (absence.TimeSpan.TotalSeconds > 0 || late.TimeSpan.TotalSeconds > 0 || earlyLeave.TimeSpan.TotalSeconds > 0 || overtime.TimeSpan.TotalSeconds > 0 || document != null)
+							{
+								dataSet.Data.Rows.Add(dataRow);
+							}
 						}
 					}
 				}
