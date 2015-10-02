e739f3a2dafca6a0255ba5f887d361d08e218c65
Dmitry Zabello <zabe110@yandex.ru>
2015-10-02 14:25:34 +0300

    Resurs Apartments

--- Projects/Resurs/Resurs/Apartments/ViewModels/ApartmentDetailsViewModel.Common.cs
+++ Projects/Resurs/Resurs/Apartments/ViewModels/ApartmentDetailsViewModel.Common.cs
@@ -41,5 +41,71 @@ namespace Resurs.ViewModels
 				OnPropertyChanged(() => Description);
 			}
 		}
+
+		//string _fio;
+		//public string FIO
+		//{
+		//	get { return _fio; }
+		//	set
+		//	{
+		//		_fio = value;
+		//		OnPropertyChanged(() => FIO);
+		//	}
+		//}
+
+		string _phone;
+		public string Phone
+		{
+			get { return _phone; }
+			set
+			{
+				_phone = value;
+				OnPropertyChanged(() => Phone);
+			}
+		}
+
+		string _email;
+		public string Email
+		{
+			get { return _email; }
+			set
+			{
+				_email = value;
+				OnPropertyChanged(() => Email);
+			}
+		}
+
+		string _login;
+		public string Login
+		{
+			get { return _login; }
+			set
+			{
+				_login = value;
+				OnPropertyChanged(() => Login);
+			}
+		}
+
+		string _password;
+		public string Password
+		{
+			get { return _password; }
+			set
+			{
+				_password = value;
+				OnPropertyChanged(() => Password);
+			}
+		}
+
+		bool _isSendEmail;
+		public bool IsSendEmail
+		{
+			get { return _isSendEmail; }
+			set
+			{
+				_isSendEmail = value;
+				OnPropertyChanged(() => IsSendEmail);
+			}
+		}
 	}
 }
