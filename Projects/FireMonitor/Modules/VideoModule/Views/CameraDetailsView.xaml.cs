1fce72f9f40ab85231c16afba5889dc4baa517a1
Alexey Oleynichenko <oleynichenkoal@rvigroup.ru>
2015-10-09 16:34:19 +0300

    SKDDEV-714 В ОЗ в разделе Видео если открыть свойства Камеры и не закрывая окна завершить приложение, останется висеть в фоне процесс декодера и самого приложения. Исправлено.

--- Projects/FireMonitor/Modules/VideoModule/Views/CameraDetailsView.xaml.cs
+++ Projects/FireMonitor/Modules/VideoModule/Views/CameraDetailsView.xaml.cs
@@ -8,28 +8,54 @@ namespace VideoModule.Views
 {
 	public partial class CameraDetailsView
 	{
-		Camera Camera { get; set; }
+		private bool _needStopPlaying;
+
+		private Camera Camera { get; set; }
+
 		public CameraDetailsView()
 		{
 			InitializeComponent();
 
+			_needStopPlaying = false;
+
 			Loaded += OnLoaded;
 			Unloaded += OnUnloaded;
-		}
-
-		private void OnUnloaded(object sender, RoutedEventArgs routedEventArgs)
-		{
-			MediaSourcePlayer.Stop();
-			MediaSourcePlayer.Close();
+			Dispatcher.ShutdownStarted += Dispatcher_ShutdownStarted;
 		}
 
 		private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
 		{
 			var viewModel = DataContext as CameraDetailsViewModel;
+			
+			// При загрузке формы сразу стартуем просмотр видео ячейки
 			if (viewModel != null)
 			{
 				MediaSourcePlayer.Open(MediaSourceFactory.CreateFromRtspStream(viewModel.RviRTSP));
 				MediaSourcePlayer.Play();
+				_needStopPlaying = true;
+			}
+		}
+
+		private void OnUnloaded(object sender, RoutedEventArgs routedEventArgs)
+		{
+			// Останавливаем видео ячейку и освобождаем занятые ей ресурсы
+			if (_needStopPlaying)
+			{
+				MediaSourcePlayer.Stop();
+				MediaSourcePlayer.Close();
+				_needStopPlaying = false;
+			}
+			Dispatcher.ShutdownStarted -= Dispatcher_ShutdownStarted;
+		}
+
+		private void Dispatcher_ShutdownStarted(object sender, EventArgs e)
+		{
+			// Останавливаем видео ячейку и освобождаем занятые ей ресурсы, если данное окно не закрывали, а вышли из приложения (т.к. окно не модальное)
+			if (_needStopPlaying)
+			{
+				MediaSourcePlayer.Stop();
+				MediaSourcePlayer.Close();
+				_needStopPlaying = false;
 			}
 		}
 	}
