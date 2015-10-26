0aa5ee2c63d558f7ed6a79ba258f4c78b472f799
Obycheff Maxim <obyichevma@rubezh.ru>
2015-10-26 16:11:32 +0300

    RG-117 Поменять номер сборки

--- Projects/FireMonitor/Modules/VideoModule/Properties/AssemblyInfo.cs
+++ Projects/FireMonitor/Modules/VideoModule/Properties/AssemblyInfo.cs
@@ -5,15 +5,6 @@ using System.Windows;
 [assembly: AssemblyTitle("VideoModule")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VideoModule")]
-[assembly: AssemblyCopyright("Copyright © ООО 'КБ Пожарной Автоматики' 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: ThemeInfo(
-	ResourceDictionaryLocation.None,
-	ResourceDictionaryLocation.SourceAssembly
-)]
-[assembly: AssemblyVersion("2.1.5.0")]
-[assembly: AssemblyFileVersion("2.1.5.0")]
+
