a972e57142dbea835f869cc4b7f5367b2b186d03
Dmitry Zabello <zabelloda@rubezh.ru>
2015-11-09 18:05:52 +0300

    RG-256 Автоматизация. Не работает функция "Запуск программы"

--- Projects/Common/Infrastructure.Automation/ProcedureThread.Steps.cs
+++ Projects/Common/Infrastructure.Automation/ProcedureThread.Steps.cs
@@ -14,6 +14,7 @@ using RubezhClient;
 using Property = RubezhAPI.Automation.Property;
 using RubezhAPI.SKD;
 using RubezhClient.SKDHelpers;
+using System.Diagnostics;
 
 namespace Infrastructure.Automation
 {
@@ -811,6 +812,13 @@ namespace Infrastructure.Automation
 		{
 			var nowArguments = procedureStep.NowArguments;
 			SetValue(nowArguments.ResultArgument, DateTime.Now);
+		}
+
+		public void RunProgram(ProcedureStep procedureStep)
+		{
+			var processName = GetValue<string>(procedureStep.RunProgramArguments.PathArgument);
+			var parameters = GetValue<string>(procedureStep.RunProgramArguments.ParametersArgument);
+			Process.Start(processName, parameters);
 		}
 
 		void ControlFireZone(ProcedureStep procedureStep)
