10e739e3413c9c48dc7cb88201d6a0e41f354acf
Andrey Ivanov <goapsy2008@yandex.ru>
2015-11-11 19:22:58 +0300

    GKWebService. Картотека. Вкладка "Сотрудники/Посетители". Редактирование пропусков

--- Projects/GKWebService/GKWebService/Scripts/Main.js
+++ Projects/GKWebService/GKWebService/Scripts/Main.js
@@ -110,6 +110,7 @@ var app = new function AppViewModel() {
     self.Menu.HR.Filter = FilterViewModel(self.Menu.HR);
     self.Menu.HR.Employees = EmployeesViewModel(self.Menu.HR);
     self.Menu.HR.Employees.EmployeeDetails = EmployeeDetailsViewModel();
+    self.Menu.HR.Employees.EmployeeCardDetails = EmployeeCardDetailsViewModel(self.Menu.HR.Employees);
     self.Menu.HR.Employees.EmployeeCards = EmployeeCardsViewModel(self.Menu.HR.Employees);
 
     return self;
