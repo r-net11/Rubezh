8121cdbaae879ee39bc6f3f8d123c6eb84fee3e7
Kovalev Roman <KovalevRA@rubezh.ru>
2015-11-30 11:08:11 +0300

    RG-500 Отчет со списком устройств(part 1)

---
+++ Projects/Common/RubezhAPI/SKD/ReportFilters/DevicesReportFilter.cs
@@ -0,0 +1,14 @@
+﻿using RubezhAPI.GK;
+using System.Collections.Generic;
+
+namespace RubezhAPI.SKD.ReportFilters
+{
+	public class DevicesReportFilter : SKDReportFilter
+	{
+		public List<GKDevice> SelectedDevices { get; set; }
+		public DevicesReportFilter()
+		{
+			SelectedDevices = new List<GKDevice>();
+		}
+	}
+}
---
+++ Projects/FireMonitor/Modules/GKModule/Reports/ViewModels/ReportFilterDeviceViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Common.TreeList;
+using RubezhAPI.GK;
+
+namespace GKModule.ViewModels
+{
+	class ReportFilterDeviceViewModel: TreeNodeViewModel<ReportFilterDeviceViewModel>
+	{
+		public GKDevice Device { get; private set; }
+		
+		public ReportFilterDeviceViewModel(GKDevice device)
+		{
+			Device = device;
+		}
+		bool _isChecked;
+		public bool IsChecked
+		{
+			get { return _isChecked; }
+			set
+			{
+				_isChecked = value;
+				OnPropertyChanged(() => IsChecked);
+			}
+		}
+	}
+}
