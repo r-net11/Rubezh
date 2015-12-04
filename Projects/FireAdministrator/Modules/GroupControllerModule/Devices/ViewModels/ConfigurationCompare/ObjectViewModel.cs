d1202137be30d8b2a5f9de3caaecc35a7ecdadb2
Kovalev Roman <KovalevRA@rubezh.ru>
2015-12-04 12:44:59 +0300

    RG-520  Ускорение построения окна сравнения конфигураций

--- Projects/FireAdministrator/Modules/GroupControllerModule/Devices/ViewModels/ConfigurationCompare/ObjectViewModel.cs
+++ Projects/FireAdministrator/Modules/GroupControllerModule/Devices/ViewModels/ConfigurationCompare/ObjectViewModel.cs
@@ -12,7 +12,7 @@ namespace GKModule.ViewModels
 		public string PresentationZoneOrLogic { get; set; }
 		public bool IsAbsent { get; set; }
 		public bool IsPresent { get; set; }
-		public bool IsDevice { get;private set; }
+		public bool IsDevice { get; private set; }
 		public bool HasNonStructureDifferences
 		{
 			get
@@ -53,6 +53,7 @@ namespace GKModule.ViewModels
 		public GKDoor Door;
 		public string ImageSource { get; private set; }
 		public ObjectType ObjectType { get; private set; }
+		public GKDevice KAUParent { get; private set; }
 		public object Clone()
 		{
 			return MemberwiseClone();
@@ -67,6 +68,7 @@ namespace GKModule.ViewModels
 			PresentationZoneOrLogic = device.IsNotUsed ? "" : GKManager.GetPresentationZoneAndGuardZoneOrLogic(Device);
 			ImageSource = "/Controls;component/GKIcons/" + device.DriverType + ".png";
 			ObjectType = ObjectType.Device;
+			KAUParent = device.KAUParent;
 			SortingName = "a " +
 				(device.KAUParent != null ? device.KAUParent.IntAddress * 256 * 256 * 256 : 0) +
 				(device.ShleifNo * 256 * 256) +
@@ -179,12 +181,12 @@ namespace GKModule.ViewModels
 			if (object1.ObjectType == ObjectType.Device)
 			{
 				var orderNo1 =
-					(object1.Device.KAUParent != null ? object1.Device.KAUParent.IntAddress * 256 * 256 * 256 : 0) +
+					(object1.KAUParent != null ? object1.KAUParent.IntAddress * 256 * 256 * 256 : 0) +
 					(object1.Device.ShleifNo * 256 * 256) +
 					(!object1.Device.Driver.IsKau ? object1.Device.IntAddress * 256 : 0)
 					+ object1.Device.Driver.DriverType;
 				var orderNo2 =
-					(object2.Device.KAUParent != null ? object2.Device.KAUParent.IntAddress * 256 * 256 * 256 : 0) +
+					(object2.KAUParent != null ? object2.KAUParent.IntAddress * 256 * 256 * 256 : 0) +
 					(object2.Device.ShleifNo * 256 * 256) +
 					(!object2.Device.Driver.IsKau ? object2.Device.IntAddress * 256 : 0)
 					+ object2.Device.Driver.DriverType;
