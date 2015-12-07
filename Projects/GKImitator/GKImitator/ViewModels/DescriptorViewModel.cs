5e061d8bbc1e54346e66d68267d9539006059116
Kovalev Roman <KovalevRA@rubezh.ru>
2015-12-07 08:17:42 +0300

    GKImitator. Remove using RubezhClient.

--- Projects/GKImitator/GKImitator/ViewModels/DescriptorViewModel.cs
+++ Projects/GKImitator/GKImitator/ViewModels/DescriptorViewModel.cs
@@ -2,14 +2,11 @@
 using System.Collections.ObjectModel;
 using System.Linq;
 using RubezhAPI.GK;
-using RubezhClient;
 using GKImitator.Processor;
 using GKProcessor;
 using Infrastructure.Common;
 using Infrastructure.Common.Windows.ViewModels;
-using RubezhAPI.Journal;
 using System.Collections.Generic;
-using System.Windows.Input;
 using Infrastructure.Common.Windows;
 using RubezhDAL.DataClasses;
 
