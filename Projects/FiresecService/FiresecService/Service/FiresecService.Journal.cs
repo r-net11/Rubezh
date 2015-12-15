94312d780a076daed6353ea6d383d12d38e66ff6
Dmitry Zabello <zabelloda@rubezh.ru>
2015-12-15 12:27:22 +0300

    RG-632 СУ. Работа функции "Проверки прав" при запуске сценария через условия запуска.
    RG-637 Метод FiresecService.AddJournalMessage должен получать получать пользователя для записи в журнал

--- Projects/FiresecService/FiresecService/Service/FiresecService.Journal.cs
+++ Projects/FiresecService/FiresecService/Service/FiresecService.Journal.cs
@@ -4,6 +4,7 @@ using RubezhAPI.Journal;
 using RubezhDAL.DataClasses;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace FiresecService.Service
@@ -13,32 +14,20 @@ namespace FiresecService.Service
 		public static Thread CurrentThread;
 
 		#region Add
-		void AddJournalMessage(JournalEventNameType journalEventNameType, string objectName, JournalEventDescriptionType journalEventDescriptionType = JournalEventDescriptionType.NULL, string userName = null, Guid? uid = null)
+		public static void AddJournalMessage(JournalEventNameType journalEventNameType, string objectName, Guid? objectUID, Guid? clientUID, JournalEventDescriptionType journalEventDescriptionType = JournalEventDescriptionType.NULL)
 		{
-			var journalItem = CreateJournalItem(journalEventNameType, objectName, journalEventDescriptionType, uid);
-			journalItem.UserName = userName;
-			AddCommonJournalItems(new List<JournalItem>() { journalItem });
-		}
-
-		public static void InsertJournalMessage(JournalEventNameType journalEventNameType, string objectName, JournalEventDescriptionType journalEventDescriptionType = JournalEventDescriptionType.NULL, string userName = null, Guid? uid = null)
-		{
-			var journalItem = CreateJournalItem(journalEventNameType, objectName, journalEventDescriptionType, uid);
-			journalItem.UserName = userName;
-			AddCommonJournalItems(new List<JournalItem>() { journalItem });
-		}
-
-		static JournalItem CreateJournalItem(JournalEventNameType journalEventNameType, string objectName, JournalEventDescriptionType journalEventDescriptionType = JournalEventDescriptionType.NULL, Guid? uid = null)
-		{
-			return new JournalItem()
+			var journalItem = new JournalItem()
 			{
 				SystemDateTime = DateTime.Now,
 				JournalEventNameType = journalEventNameType,
 				JournalEventDescriptionType = journalEventDescriptionType,
 				JournalSubsystemType = EventDescriptionAttributeHelper.ToSubsystem(journalEventNameType),
 				JournalObjectType = JournalObjectType.None,
-				ObjectUID = uid != null ? uid.Value : Guid.Empty,
+				ObjectUID = objectUID != null ? objectUID.Value : Guid.Empty,
 				ObjectName = objectName,
+				UserName = GetUserName(clientUID)
 			};
+			AddCommonJournalItems(new List<JournalItem>() { journalItem }, clientUID);
 		}
 
 		public static void AddCommonJournalItems(List<JournalItem> journalItems, Guid? clientUID)
@@ -61,7 +50,7 @@ namespace FiresecService.Service
 			{
 				journalItem.UserName = GetUserName(clientUID);
 				journalItem.JournalSubsystemType = EventDescriptionAttributeHelper.ToSubsystem(journalItem.JournalEventNameType);
-				AddCommonJournalItems(new List<JournalItem>() { journalItem });
+				AddCommonJournalItems(new List<JournalItem>() { journalItem }, clientUID);
 			}
 			catch (Exception e)
 			{
