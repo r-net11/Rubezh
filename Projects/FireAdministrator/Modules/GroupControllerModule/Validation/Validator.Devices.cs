d027b58ad9ced74a384460967eaadda9d5e8f7f9
Kiselev Andrey <kiselevay@rubezh.ru>
2015-12-24 10:12:45 +0300

    fix

--- Projects/FireAdministrator/Modules/GroupControllerModule/Validation/Validator.Devices.cs
+++ Projects/FireAdministrator/Modules/GroupControllerModule/Validation/Validator.Devices.cs
@@ -48,7 +48,6 @@ namespace GKModule.Validation
 					ValidateLogic(device);
 				ValidateGKNotEmptyChildren(device);
 				ValidateParametersMinMax(device);
-				//ValidateNotUsedLogic(device);
 				ValidateRSR2AddressFollowing(device);
 				ValidateKAUAddressFollowing(device);
 				ValidateGuardDevice(device);
@@ -185,18 +184,6 @@ namespace GKModule.Validation
 				}
 			}
 		}
-
-		//void ValidateNotUsedLogic(GKDevice device)
-		//{
-		//	foreach (var clause in device.Logic.OnClausesGroup.Clauses)
-		//	{
-		//		foreach (var clauseDevices in clause.Devices)
-		//		{
-		//			if (clauseDevices.IsNotUsed)
-		//				AddError(device, "В логике задействованы неиспользуемые устройства", ValidationErrorLevel.CannotSave);
-		//		}
-		//	}
-		//}
 
 		void ValidateDeviceRangeAddress(GKDevice device)
 		{
