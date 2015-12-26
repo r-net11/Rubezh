91806fc2383f045565e143ec2b4fcd5c0a9b5eeb
Kovalev Roman <KovalevRA@rubezh.ru>
2015-12-26 16:52:37 +0300

    fix

--- Projects/FiresecService/FiresecService.Report/Templates/EmployeeRootReport.Designer.cs
+++ Projects/FiresecService/FiresecService.Report/Templates/EmployeeRootReport.Designer.cs
@@ -47,14 +47,14 @@
 			// Detail
 			// 
 			this.Detail.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
-			this.xrTable2});
+            this.xrTable2});
 			this.Detail.HeightF = 63.5F;
 			// 
 			// GroupHeader1
 			// 
 			this.GroupHeader1.BackColor = System.Drawing.Color.LightGray;
 			this.GroupHeader1.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
-			this.xrTable1});
+            this.xrTable1});
 			this.GroupHeader1.Dpi = 254F;
 			this.GroupHeader1.GroupUnion = DevExpress.XtraReports.UI.GroupUnion.WithFirstDetail;
 			this.GroupHeader1.HeightF = 64F;
@@ -66,16 +66,16 @@
 			// xrTable1
 			// 
 			this.xrTable1.BackColor = System.Drawing.Color.DarkGray;
-			this.xrTable1.Borders = ((DevExpress.XtraPrinting.BorderSide)((((DevExpress.XtraPrinting.BorderSide.Left | DevExpress.XtraPrinting.BorderSide.Top)
-			| DevExpress.XtraPrinting.BorderSide.Right)
-			| DevExpress.XtraPrinting.BorderSide.Bottom)));
+			this.xrTable1.Borders = ((DevExpress.XtraPrinting.BorderSide)((((DevExpress.XtraPrinting.BorderSide.Left | DevExpress.XtraPrinting.BorderSide.Top) 
+            | DevExpress.XtraPrinting.BorderSide.Right) 
+            | DevExpress.XtraPrinting.BorderSide.Bottom)));
 			this.xrTable1.Dpi = 254F;
 			this.xrTable1.KeepTogether = true;
 			this.xrTable1.LocationFloat = new DevExpress.Utils.PointFloat(0F, 0.4999695F);
 			this.xrTable1.Name = "xrTable1";
 			this.xrTable1.Padding = new DevExpress.XtraPrinting.PaddingInfo(3, 3, 3, 3, 254F);
 			this.xrTable1.Rows.AddRange(new DevExpress.XtraReports.UI.XRTableRow[] {
-			this.xrTableRow1});
+            this.xrTableRow1});
 			this.xrTable1.SizeF = new System.Drawing.SizeF(1700F, 63.5F);
 			this.xrTable1.StylePriority.UseBorders = false;
 			this.xrTable1.StylePriority.UsePadding = false;
@@ -85,8 +85,8 @@
 			// xrTableRow1
 			// 
 			this.xrTableRow1.Cells.AddRange(new DevExpress.XtraReports.UI.XRTableCell[] {
-			this.xrTableCell1,
-			this.xrTableCell2});
+            this.xrTableCell1,
+            this.xrTableCell2});
 			this.xrTableRow1.Dpi = 254F;
 			this.xrTableRow1.Name = "xrTableRow1";
 			this.xrTableRow1.Weight = 11.5D;
@@ -96,14 +96,14 @@
 			this.xrTableCell1.Dpi = 254F;
 			this.xrTableCell1.Name = "xrTableCell1";
 			this.xrTableCell1.Text = "Дата/время прохода";
-			this.xrTableCell1.Weight = 0.15384616648438571D;
+			this.xrTableCell1.Weight = 0.12032424464473662D;
 			// 
 			// xrTableCell2
 			// 
 			this.xrTableCell2.Dpi = 254F;
 			this.xrTableCell2.Name = "xrTableCell2";
 			this.xrTableCell2.Text = "Зона";
-			this.xrTableCell2.Weight = 0.15384614120792228D;
+			this.xrTableCell2.Weight = 0.18736806304757139D;
 			// 
 			// employeeRootDataSet1
 			// 
@@ -112,22 +112,22 @@
 			// 
 			// xrTable2
 			// 
-			this.xrTable2.Borders = ((DevExpress.XtraPrinting.BorderSide)(((DevExpress.XtraPrinting.BorderSide.Left | DevExpress.XtraPrinting.BorderSide.Right)
-			| DevExpress.XtraPrinting.BorderSide.Bottom)));
+			this.xrTable2.Borders = ((DevExpress.XtraPrinting.BorderSide)(((DevExpress.XtraPrinting.BorderSide.Left | DevExpress.XtraPrinting.BorderSide.Right) 
+            | DevExpress.XtraPrinting.BorderSide.Bottom)));
 			this.xrTable2.Dpi = 254F;
 			this.xrTable2.LocationFloat = new DevExpress.Utils.PointFloat(0F, 0F);
 			this.xrTable2.Name = "xrTable2";
 			this.xrTable2.OddStyleName = "xrControlStyle1";
 			this.xrTable2.Rows.AddRange(new DevExpress.XtraReports.UI.XRTableRow[] {
-			this.xrTableRow2});
+            this.xrTableRow2});
 			this.xrTable2.SizeF = new System.Drawing.SizeF(1700F, 63.5F);
 			this.xrTable2.StylePriority.UseBorders = false;
 			// 
 			// xrTableRow2
 			// 
 			this.xrTableRow2.Cells.AddRange(new DevExpress.XtraReports.UI.XRTableCell[] {
-			this.xrTableCell3,
-			this.xrTableCell5});
+            this.xrTableCell3,
+            this.xrTableCell5});
 			this.xrTableRow2.Dpi = 254F;
 			this.xrTableRow2.Name = "xrTableRow2";
 			this.xrTableRow2.Weight = 0.567901234567901D;
@@ -135,24 +135,24 @@
 			// xrTableCell3
 			// 
 			this.xrTableCell3.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
-			new DevExpress.XtraReports.UI.XRBinding("Text", null, "Data.DateTime")});
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "Data.DateTime")});
 			this.xrTableCell3.Dpi = 254F;
 			this.xrTableCell3.Name = "xrTableCell3";
 			this.xrTableCell3.StylePriority.UseTextAlignment = false;
 			this.xrTableCell3.Text = "xrTableCell3";
-			this.xrTableCell3.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopCenter;
-			this.xrTableCell3.Weight = 0.270788912579957D;
+			this.xrTableCell3.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+			this.xrTableCell3.Weight = 0.21178606106767542D;
 			// 
 			// xrTableCell5
 			// 
 			this.xrTableCell5.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
-			new DevExpress.XtraReports.UI.XRBinding("Text", null, "Data.Zone")});
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "Data.Zone")});
 			this.xrTableCell5.Dpi = 254F;
 			this.xrTableCell5.Name = "xrTableCell5";
 			this.xrTableCell5.StylePriority.UseTextAlignment = false;
 			this.xrTableCell5.Text = "xrTableCell5";
-			this.xrTableCell5.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopCenter;
-			this.xrTableCell5.Weight = 0.270788912579957D;
+			this.xrTableCell5.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+			this.xrTableCell5.Weight = 0.32979176409223865D;
 			// 
 			// xrControlStyle1
 			// 
@@ -163,15 +163,15 @@
 			// EmployeeRootReport
 			// 
 			this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
-			this.Detail,
-			this.GroupHeader1});
+            this.Detail,
+            this.GroupHeader1});
 			this.DataMember = "Data";
 			this.DataSource = this.employeeRootDataSet1;
 			this.DataSourceSchema = null;
 			this.ExportOptions.PrintPreview.ActionAfterExport = DevExpress.XtraPrinting.ActionAfterExport.Open;
 			this.ExportOptions.PrintPreview.ShowOptionsBeforeExport = false;
 			this.StyleSheet.AddRange(new DevExpress.XtraReports.UI.XRControlStyle[] {
-			this.xrControlStyle1});
+            this.xrControlStyle1});
 			this.Version = "15.1";
 			this.Controls.SetChildIndex(this.GroupHeader1, 0);
 			this.Controls.SetChildIndex(this.Detail, 0);
