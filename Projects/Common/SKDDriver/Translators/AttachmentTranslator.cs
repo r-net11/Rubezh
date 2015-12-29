8bba6afe79795e8126a35a0dffa6f2b47155aed7
Alexey Oleynichenko <oleynichenkoal@rvigroup.ru>
2015-12-29 16:09:26 +0300

    УРВ. Прикрепляемые файлы к оправдательным документам. Недостающие файлы.

---
+++ Projects/Common/SKDDriver/Translators/AttachmentTranslator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FiresecAPI;
+using FiresecAPI.SKD;
+
+namespace SKDDriver
+{
+	public class AttachmentTranslator : TranslatorBase<DataAccess.Attachment, Attachment>
+	{
+		public AttachmentTranslator(SKDDatabaseService databaseService)
+			: base(databaseService)
+		{
+		}
+
+		protected override void TranslateBack(DataAccess.Attachment tableItem, Attachment apiItem)
+		{
+			tableItem.UID = apiItem.UID;
+			tableItem.Name = apiItem.FileName;
+		}
+
+		protected override Attachment Translate(DataAccess.Attachment tableItem)
+		{
+			var result = base.Translate(tableItem);
+			result.FileName = tableItem.Name;
+			return result;
+		}
+	}
+}
