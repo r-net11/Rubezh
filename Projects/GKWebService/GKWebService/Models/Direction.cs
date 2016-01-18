dbdb01786f70d0ee303dc58cb122542149f0c056
Sevastyanov Alexander <sevastyanovaa@rubezh.ru>
2016-01-18 20:09:51 +0300

    WEB. Directions updater

---
+++ Projects/GKWebService/GKWebService/Models/Direction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GKWebService.Models
+{
+	public class Direction
+	{
+		public Guid UID { get; set; }
+		public int No { get; set; }
+		public string Name { get; set; }
+		public string State { get; set; }
+	}
+}
