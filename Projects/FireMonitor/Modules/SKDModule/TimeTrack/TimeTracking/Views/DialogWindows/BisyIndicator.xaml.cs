850fa37ea992646c3acc19f6ad719386fab637d0
KonstantinDavidov <konstantin.davidov1@gmail.com>
2016-01-18 09:04:24 +0300

    Создание и имплементирование BisyIndicator Мелкий рефакторинг

---
+++ Projects/FireMonitor/Modules/SKDModule/TimeTrack/TimeTracking/Views/DialogWindows/BisyIndicator.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SKDModule.Views
+{
+	/// <summary>
+	/// Interaction logic for BisyIndicator.xaml
+	/// </summary>
+	public partial class BisyIndicator : UserControl
+	{
+		public BisyIndicator()
+		{
+			InitializeComponent();
+		}
+	}
+}
