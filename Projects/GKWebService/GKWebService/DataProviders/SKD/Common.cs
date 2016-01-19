ee5495352f5b2a64e2ac9f9da754ca7626347d37
Obycheff Maxim <obyichevma@rubezh.ru>
2016-01-19 14:27:12 +0300

    webService fix

--- Projects/GKWebService/GKWebService/DataProviders/SKD/Common.cs
+++ Projects/GKWebService/GKWebService/DataProviders/SKD/Common.cs
@@ -8,26 +8,7 @@ namespace GKWebService.DataProviders.SKD
 {
     static class Common
     {
-        public static bool ThrowErrorIfExists(OperationResult operationResult)
-        {
-            if (operationResult == null)
-            {
-                throw new InvalidOperationException("Сервис вернул ссылку на объект не указывающую на экземпляр объекта");
-            }
-            if (operationResult.HasWarnings)
-            {
-            }
-            if (operationResult.HasError)
-            {
-                if (operationResult.Error.Contains("String or binary data would be truncated"))
-                    operationResult.Error = "Превышен максимальный размер строки";
-
-                throw new InvalidOperationException(operationResult.Error);
-            }
-            return true;
-        }
-
-        public static T ThrowErrorIfExists<T>(OperationResult<T> operationResult)
+		public static T ThrowErrorIfExists<T>(OperationResult<T> operationResult)
         {
             if (operationResult == null)
             {
