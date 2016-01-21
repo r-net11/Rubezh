946a13390253e5f0718b5dfd5b9c8a934d2f66d0
Kiselev Andrey <kiselevay@rubezh.ru>
2016-01-21 16:51:23 +0300

    fix

--- Projects/GKWebService/GKWebService/DataProviders/MPTHubs/MPTHub.cs
+++ Projects/GKWebService/GKWebService/DataProviders/MPTHubs/MPTHub.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace GKWebService.DataProviders.MPTHubs
 {
+	[HubName("mptsUpdater")]
 	public class MPTHub : Hub
 	{
 	
