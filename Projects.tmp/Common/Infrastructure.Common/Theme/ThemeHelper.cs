fe7108b371c7433898baed026fccf570fcebd2d5
Dmitry Zabello <zabelloda@rubezh.ru>
2016-01-26 13:22:29 +0300

    Monodevelop experiment - build Common/Infrastructure.Common

--- Projects.tmp/Common/Infrastructure.Common/Theme/ThemeHelper.cs
+++ Projects.tmp/Common/Infrastructure.Common/Theme/ThemeHelper.cs
@@ -23,13 +23,12 @@ namespace Infrastructure.Common.Theme
 					CurrentTheme = "BlueTheme";
 				}
 				var themePath = "pack://application:,,,/Controls, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null;component/Themes/" + CurrentTheme + ".xaml";
-				Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri(themePath) });
+
 			}
 			catch (Exception e)
 			{
 				CurrentTheme = "BlueTheme";
 				var themePath = "pack://application:,,,/Controls, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null;component/Themes/" + CurrentTheme + ".xaml";
-				Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri(themePath) });
 				Logger.Error(e, "ThemeHelper.LoadThemeFromRegister");
 			}
 		}
