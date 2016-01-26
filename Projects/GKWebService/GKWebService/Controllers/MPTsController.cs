015e97b3adec067e9620ab7c2ffb59defad7d938
Kiselev Andrey <kiselevay@rubezh.ru>
2016-01-26 18:02:54 +0300

    mpt

---
+++ Projects/GKWebService/GKWebService/Controllers/MPTsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GKWebService.Controllers
+{
+    public class MPTsController : Controller
+    {
+        // GET: MPTs
+		public ActionResult Index()
+		{
+			return View();
+		}
+    }
+}
