b49fc8ec296de53d8084fa62cc2fdda6e2d111ef
KonstantinDavidov <konstantin.davidov1@gmail.com>
2016-02-02 16:16:54 +0300

    Исправления в формате вывода данных

--- Projects/FiresecService/FiresecService.Report/Templates/DisciplineReport.cs
+++ Projects/FiresecService/FiresecService.Report/Templates/DisciplineReport.cs
@@ -39,7 +39,7 @@ namespace FiresecService.Report.Templates
 			var dataSet = new DisciplineDataSet();
 			foreach (var employee in employees)
 			{
-				if (filter.ScheduleSchemas != null && filter.ScheduleSchemas.Count > 0)
+				if (filter.ScheduleSchemas != null && filter.ScheduleSchemas.Any())
 				{
 					if (employee.Item.Schedule == null || !filter.ScheduleSchemas.Contains(employee.Item.Schedule.UID))
 						continue;
@@ -70,14 +70,17 @@ namespace FiresecService.Report.Templates
 						{
 							dataRow.FirstEnter = isFirstEnterCrossNight
 												? "Пред. сутки"
-												: dayTimeTrack.RealTimeTrackParts.Min(x => x.EnterDateTime.TimeOfDay).ToString();
+												: dayTimeTrack.RealTimeTrackParts.Min(x => x.EnterDateTime.TimeOfDay).ToString(@"hh\:mm\:ss");
 							dataRow.LastExit = isLastExitCrossNight
 												? "След. сутки"
 												: dayTimeTrack.RealTimeTrackParts
 													.Where(x => x.ExitDateTime.HasValue && x.IsForURVZone)
 													.Select(x => x.ExitDateTime)
-													.DefaultIfEmpty().Max().GetValueOrDefault().TimeOfDay
-													.ToString();
+													.DefaultIfEmpty()
+													.Max()
+													.GetValueOrDefault()
+													.TimeOfDay
+													.ToString(@"hh\:mm\:ss");
 						}
 
 						var absence = GetTimespanForTimeTrackType(dayTimeTrack.Totals, TimeTrackType.Absence);
@@ -90,18 +93,18 @@ namespace FiresecService.Report.Templates
 							|| (earlyLeave.TotalSeconds > default(int) && filter.ShowEarlуLeave)
 							|| (overtime.TotalSeconds > default(int) && filter.ShowOvertime)) && filter.ShowShiftedViolation)
 						{
-							dataRow.Absence = absence == TimeSpan.Zero ? string.Empty : absence.ToString();
-							dataRow.Late = late == TimeSpan.Zero ? string.Empty : late.ToString();
-							dataRow.EarlyLeave = earlyLeave == TimeSpan.Zero ? string.Empty : earlyLeave.ToString();
-							dataRow.Overtime = overtime == TimeSpan.Zero ? string.Empty : overtime.ToString();
+							dataRow.Absence = absence == TimeSpan.Zero ? string.Empty : absence.ToString(@"hh\:mm\:ss");
+							dataRow.Late = late == TimeSpan.Zero ? string.Empty : late.ToString(@"hh\:mm\:ss");
+							dataRow.EarlyLeave = earlyLeave == TimeSpan.Zero ? string.Empty : earlyLeave.ToString(@"hh\:mm\:ss");
+							dataRow.Overtime = overtime == TimeSpan.Zero ? string.Empty : overtime.ToString(@"hh\:mm\:ss");
 							dataSet.Data.Rows.Add(dataRow);
 						}
 						else if (filter.ShowAllViolation)
 						{
-							dataRow.Absence = absence == TimeSpan.Zero ? string.Empty : absence.ToString();
-							dataRow.Late = late == TimeSpan.Zero ? string.Empty : late.ToString();
-							dataRow.EarlyLeave = earlyLeave == TimeSpan.Zero ? string.Empty : earlyLeave.ToString();
-							dataRow.Overtime = overtime == TimeSpan.Zero ? string.Empty : overtime.ToString();
+							dataRow.Absence = absence == TimeSpan.Zero ? string.Empty : absence.ToString(@"hh\:mm\:ss");
+							dataRow.Late = late == TimeSpan.Zero ? string.Empty : late.ToString(@"hh\:mm\:ss");
+							dataRow.EarlyLeave = earlyLeave == TimeSpan.Zero ? string.Empty : earlyLeave.ToString(@"hh\:mm\:ss");
+							dataRow.Overtime = overtime == TimeSpan.Zero ? string.Empty : overtime.ToString(@"hh\:mm\:ss");
 							dataSet.Data.Rows.Add(dataRow);
 						}
 					}
