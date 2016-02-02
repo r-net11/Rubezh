b7019c71702ccadf532bd8b8ed8a76cac4633c8a
Andrey Ivanov <goapsy2008@yandex.ru>
2016-02-02 20:10:29 +0300

    Индикаторы. Задание для индикаторв цвета, иконки и типа

--- Projects/GKWebService/GKWebService/Controllers/AlarmsController.cs
+++ Projects/GKWebService/GKWebService/Controllers/AlarmsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GKWebService.Models.GK.Alarms;
+using GKWebService.Utils;
 
 namespace GKWebService.Controllers
 {
@@ -17,5 +19,11 @@ namespace GKWebService.Controllers
         {
             return View();
         }
-    }
+
+		[ErrorHandler]
+		public JsonResult GetAlarmGroups()
+		{
+			return Json(new AlarmGroupsViewModel(), JsonRequestBehavior.AllowGet);
+		}
+	}
 }
