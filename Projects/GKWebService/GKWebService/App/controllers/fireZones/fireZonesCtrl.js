ca1d3a83005f94a5203303244e2646d3228d04f5
Alexandervelilyaev <Alexandervelilyaev@gmail.com>
2016-02-02 17:03:26 +0300

    Web fireZones Удалил директиву ngJqGrid, заменил обработку клика событием rowSelectionChanged(избавился от $scope.main),
    добавил кнопку закрытия в окно управления зоной.
    Выровнял ширину и сделал автовысоту в обеих таблицах

--- Projects/GKWebService/GKWebService/App/controllers/fireZones/fireZonesCtrl.js
+++ Projects/GKWebService/GKWebService/App/controllers/fireZones/fireZonesCtrl.js
@@ -5,19 +5,17 @@
     var app = angular.module('gkApp.controllers').controller('fireZonesCtrl',
         ['$scope', '$http', '$timeout', '$uibModal', 'signalrFireZonesService', 'broadcastService',
         function ($scope, $http, $timeout, $uibModal, signalrFireZonesService, broadcastService) {
-
             $scope.gridOptions = {
                 enableRowHeaderSelection: false,
                 enableSorting: false,
                 multiSelect: false,
                 enableColumnMenus: false,
                 enableRowSelection: true,
-                rowTemplate: "<div ng-click=\"grid.appScope.changeZone(row)\" ng-repeat=\"(colRenderIndex, col) in colContainer.renderedColumns track by col.colDef.name\" class=\"ui-grid-cell\" ng-class=\"{ 'ui-grid-row-header-cell': col.isRowHeader }\" ui-grid-cell></div>",
                 columnDefs: [
                     { field: 'No', enableColumnResizing: false, displayName: '№', width: 50, cellTemplate: '<div class="ui-grid-cell-contents"><img style="vertical-align: middle; padding-right: 3px" ng-src="data:image/gif;base64,{{row.entity.ImageSource.Item1}}"/>{{row.entity[col.field]}}</div>' },
                     { field: 'Name', width: 200, displayName: 'Наименование', cellTemplate: '<div class="ui-grid-cell-contents"><a href="#" ng-click="grid.appScope.fireZonesClick(row.entity)"><img style="vertical-align: middle; padding-right: 3px" ng-src="{{row.entity.StateIcon}}"/>{{row.entity[col.field]}}</a></div>' },
-                    { field: 'Fire1Count', displayName: 'Количество датчиков для перевода в Пожар1:', width: Math.round(($(window).width() - 500) / 2) },
-                    { field: 'Fire2Count', displayName: 'Количество датчиков для перевода в Пожар2:', width: Math.round(($(window).width() - 500) / 2) }
+                    { field: 'Fire1Count', displayName: 'Количество датчиков для перевода в Пожар1:', width: Math.round(($(window).width() - 525) / 2) },
+                    { field: 'Fire2Count', displayName: 'Количество датчиков для перевода в Пожар2:', width: Math.round(($(window).width() - 525) / 2) }
                 ]
             };
 
@@ -45,27 +43,25 @@
             };
 
             $scope.changeZone = function (row) {
-                //$scope.$parent.zoneNumber = row.entity.No - 1;
-                //angular.element(document.getElementById('devices')).scope().main();
-
                 broadcastService.send('selectedZoneChanged', row.entity.No - 1);
             }
 
             $http.get('FireZones/GetFireZonesData').success(function (data, status, headers, config) {
                 $scope.gridOptions.data = data;
+                //Выбираем первую строку после загрузки данных
                 $timeout(function () {
                     if ($scope.gridApi.selection.selectRow) {
                         $scope.gridApi.selection.selectRow($scope.gridOptions.data[0]);
                     }
                 });
             });
-
+           
             $scope.gridOptions.onRegisterApi = function (gridApi) {
                 $scope.gridApi = gridApi;
+                //Подписчик на событие изменения выбранной строки
+                gridApi.selection.on.rowSelectionChanged($scope, $scope.changeZone);
             };
 
-            //Номер активной зоны
-            //$scope.$parent.zoneNumber = 0;
         }]
     );
 }());
