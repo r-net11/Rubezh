6d7a3d7f0ce69cab4a29332a1b5d28caef4cdd45
Dmitry Zabello <zabelloda@rubezh.ru>
2016-02-03 08:55:12 +0300

    TestService

--- Projects.tmp/test/Common/TestAPI/BindingHelper.cs
+++ Projects.tmp/test/Common/TestAPI/BindingHelper.cs
@@ -12,15 +12,27 @@ namespace TestAPI
 			binding.SendTimeout = TimeSpan.FromMinutes(10);
 			binding.ReceiveTimeout = TimeSpan.FromMinutes(10);
 			binding.MaxReceivedMessageSize = Int32.MaxValue;
+<<<<<<< HEAD
 			binding.ReaderQuotas = new System.Xml.XmlDictionaryReaderQuotas();
+=======
+			binding.ReaderQuotas = new System.Xml.XmlDictionaryReaderQuotas ();
+>>>>>>> TestService
 			binding.ReaderQuotas.MaxStringContentLength = Int32.MaxValue;
 			binding.ReaderQuotas.MaxArrayLength = Int32.MaxValue;
 			binding.ReaderQuotas.MaxBytesPerRead = Int32.MaxValue;
 			binding.ReaderQuotas.MaxDepth = Int32.MaxValue;
 			binding.ReaderQuotas.MaxNameTableCharCount = Int32.MaxValue;
 			binding.Security.Mode = SecurityMode.None;
+<<<<<<< HEAD
 			binding.TransferMode = TransferMode.Streamed;
+=======
+			//binding.TransferMode = TransferMode.Streamed;
+>>>>>>> TestService
 			return binding;
 		}
 	}
 }
+<<<<<<< HEAD
+=======
+
+>>>>>>> TestService
