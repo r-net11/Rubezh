9ef0f8d15074f4f520634118dc832739dca2d10f
Dmitry Zabello <zabelloda@rubezh.ru>
2016-02-03 13:59:16 +0300

    TestService - mex

--- Projects.tmp/test/Server/Server/Program.cs
+++ Projects.tmp/test/Server/Server/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.ServiceModel;
-using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Description;
 using TestAPI;
 
 namespace Server
@@ -11,18 +11,24 @@ namespace Server
 		{
 			var address = args.Length == 0 ? "localhost:1050" : args[0];
 			Console.Title = address;
-			ServiceHost host = new ServiceHost(typeof(TestService), new Uri("net.tcp://" + address + "/TestService"));
+			ServiceHost host = new ServiceHost(typeof(TestService), new Uri("net.tcp://" + address + "/TestService"), new Uri("http://localhost:9987"));
 			host.AddServiceEndpoint(typeof(ITestService), BindingHelper.CreateBinding(), "");
+
+			var smb = new ServiceMetadataBehavior();
+			host.Description.Behaviors.Add(smb);
+			var mexBinding = MetadataExchangeBindings.CreateMexHttpBinding();
+			host.AddServiceEndpoint(typeof(IMetadataExchange), mexBinding, "mex");
+
 			host.Open();
 
-			var throttle = ((ChannelDispatcher)host.ChannelDispatchers [0]).ServiceThrottle;
+			//var throttle = ((ChannelDispatcher)host.ChannelDispatchers [0]).ServiceThrottle;
 			//throttle.MaxConcurrentCalls = Int32.MaxValue;
 			//throttle.MaxConcurrentInstances = Int32.MaxValue;
 			//throttle.MaxConcurrentSessions = Int32.MaxValue;
 
-			Console.WriteLine("MaxConcurrentCalls = " + throttle.MaxConcurrentCalls);
-			Console.WriteLine("MaxConcurrentInstances = " + throttle.MaxConcurrentInstances);
-			Console.WriteLine("MaxConcurrentSessions = " + throttle.MaxConcurrentSessions);
+			//Console.WriteLine("MaxConcurrentCalls = " + throttle.MaxConcurrentCalls);
+			//Console.WriteLine("MaxConcurrentInstances = " + throttle.MaxConcurrentInstances);
+			//Console.WriteLine("MaxConcurrentSessions = " + throttle.MaxConcurrentSessions);
 
 			Console.WriteLine("Сервер запущен");
 			Console.ReadLine();
