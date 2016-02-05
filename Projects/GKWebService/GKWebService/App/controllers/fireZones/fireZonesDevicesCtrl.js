733a021ebe1914203c962a1e69c00b6e2dd94cf1
Alexandervelilyaev <Alexandervelilyaev@gmail.com>
2016-02-05 11:40:01 +0300

    Web FireZones начал реализовывать окно настройки устройств

--- Projects/GKWebService/GKWebService/App/controllers/fireZones/fireZonesDevicesCtrl.js
+++ Projects/GKWebService/GKWebService/App/controllers/fireZones/fireZonesDevicesCtrl.js
@@ -6,7 +6,7 @@
         ['$scope', '$http', '$timeout', '$uibModal', 'uiGridTreeViewConstants', 'uiGridTreeBaseService', 'signalrFireZonesService',
         function ($scope, $http, $timeout, $uibModal, uiGridTreeViewConstants, uiGridTreeBaseService) {
 
-            var template = "<div class=\"ui-grid-cell-contents\"><div style=\"float:left;\" class=\"ui-grid-tree-base-row-header-buttons\" ng-class=\"{'ui-grid-tree-base-header': row.treeLevel > -1 }\" ng-click=\"grid.appScope.toggleRow(row,evt)\"><i ng-class=\"{'ui-grid-icon-minus-squared': ( ( grid.options.showTreeExpandNoChildren && row.treeLevel > -1 ) || ( row.treeNode.children && row.treeNode.children.length > 0 ) ) && row.treeNode.state === 'expanded', 'ui-grid-icon-plus-squared': ( ( grid.options.showTreeExpandNoChildren && row.treeLevel > -1 ) || ( row.treeNode.children && row.treeNode.children.length > 0 ) ) && row.treeNode.state === 'collapsed', 'ui-grid-icon-blank': ( ( grid.options.showTreeExpandNoChildren && row.treeLevel > -1 ) || ( row.treeNode.children && row.treeNode.children.length == 0 ) ) && row.treeNode.state === 'expanded'}\" ng-style=\"{'padding-left': grid.options.treeIndent * row.treeLevel + 'px'}\"></i> &nbsp;</div>{{ CUSTOM_FILTERS}}<a href=\"#\" ng-click=\"grid.appScope.fireZonesClick(row.entity)\"><img style=\"vertical-align: middle; padding-right: 3px\" ng-src=\"{{row.entity.imageState}}\"/><img style=\"vertical-align: middle\" width=\"16px\" height=\"16px\" ng-src=\"{{row.entity.imageDevice}}\"/> {{row.entity[col.field]}}</a></div>";
+            var template = "<div class=\"ui-grid-cell-contents\"><div style=\"float:left;\" class=\"ui-grid-tree-base-row-header-buttons\" ng-class=\"{'ui-grid-tree-base-header': row.treeLevel > -1 }\" ng-click=\"grid.appScope.toggleRow(row,evt)\"><i ng-class=\"{'ui-grid-icon-minus-squared': ( ( grid.options.showTreeExpandNoChildren && row.treeLevel > -1 ) || ( row.treeNode.children && row.treeNode.children.length > 0 ) ) && row.treeNode.state === 'expanded', 'ui-grid-icon-plus-squared': ( ( grid.options.showTreeExpandNoChildren && row.treeLevel > -1 ) || ( row.treeNode.children && row.treeNode.children.length > 0 ) ) && row.treeNode.state === 'collapsed', 'ui-grid-icon-blank': ( ( grid.options.showTreeExpandNoChildren && row.treeLevel > -1 ) || ( row.treeNode.children && row.treeNode.children.length == 0 ) ) && row.treeNode.state === 'expanded'}\" ng-style=\"{'padding-left': grid.options.treeIndent * row.treeLevel + 'px'}\"></i> &nbsp;</div>{{ CUSTOM_FILTERS}}<a href=\"#\" ng-click=\"grid.appScope.fireZonesDevicesClick(row.entity)\"><img style=\"vertical-align: middle; padding-right: 3px\" ng-src=\"{{row.entity.imageState}}\"/><img style=\"vertical-align: middle\" width=\"16px\" height=\"16px\" ng-src=\"{{row.entity.imageDevice}}\"/> {{row.entity[col.field]}}</a></div>";
             $scope.gridOptions = {
                 enableSorting: false,
                 enableFiltering: false,
@@ -44,25 +44,22 @@
                 }
             });
 
-            $scope.fireZonesDevicesClick = function (fireZoneDevice) {
+            $scope.fireZonesDevicesClick = function (device) {
                 var modalInstance = $uibModal.open({
                     animation: false,
-                    templateUrl: 'FireZones/FireZonesDevicesDetails',
+                    templateUrl: 'Devices/DeviceDetails',
                     controller: 'fireZonesDevicesDetailsCtrl',
                     resolve: {
-                        fireZoneDevice: function () {
-                            return fireZoneDevice;
+                        device: function () {
+                            return device;
                         }
                     }
                 });
             };
 
             $scope.$on('selectedZoneChanged', function (event, args) {
-                $http({
-                    url: 'FireZones/GetDevicesListByZoneNumber/',
-                    method: "GET",
-                    params: { uid: args }
-                }).success(function (data, status, headers, config) {
+                $http.get('FireZones/GetDevicesListByZoneNumber/' + args
+                ).success(function (data, status, headers, config) {
                     $scope.gridOptions.data = [];
                     for (var i in data) {
                         var item = data[i].DeviceList;
