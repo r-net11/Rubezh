90b56e8d78537dcd9a0d01cba87377c6228d0aaf
Alexandervelilyaev <Alexandervelilyaev@gmail.com>
2016-02-05 20:47:04 +0300

    Web FireZones окно для устройств в процессе

--- Projects/GKWebService/GKWebService/App/controllers/fireZones/fireZonesDevicesDetailsCtrl.js
+++ Projects/GKWebService/GKWebService/App/controllers/fireZones/fireZonesDevicesDetailsCtrl.js
@@ -1,7 +1,7 @@
 ﻿(function () {
 
     angular.module('gkApp.controllers').controller('fireZonesDevicesDetailsCtrl',
-        function ($scope, $uibModalInstance, $http, device) {
+        function ($scope, $uibModalInstance, device) {
             $scope.device = device;
 
             $scope.$on('fireZonesDeviceChanged', function (event, args) {
@@ -10,6 +10,21 @@
                     $scope.$apply();
                 };
             });
+
+            $scope.gridOptions = {
+                enableRowHeaderSelection: false,
+                enableSorting: false,
+                multiSelect: false,
+                enableColumnMenus: false,
+                enableRowSelection: true,
+                columnDefs:
+                    [
+                        { field: 'Name', displayName: 'Параметр', width: 275, cellTemplate: '<span style="font-weight: bold; color: black">{{row.entity[col.field]}}</span>' },
+                        { field: 'Value', displayName: 'Значение', width: 275, cellTemplate: '<span style="font-weight: bold; color: black">{{row.entity[col.field]}}</span>' }
+                    ]
+            };
+            $scope.gridOptions.data = device.Properties;
+
 
             $scope.Show = function () {
 
