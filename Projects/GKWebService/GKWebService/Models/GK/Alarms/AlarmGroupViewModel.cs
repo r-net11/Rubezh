be16bba084f410c55c49663987d245e7ff9dbf64
Andrey Ivanov <goapsy2008@yandex.ru>
2016-02-09 23:21:13 +0300

    Индикаторы. всплывающие подсказки

--- Projects/GKWebService/GKWebService/Models/GK/Alarms/AlarmGroupViewModel.cs
+++ Projects/GKWebService/GKWebService/Models/GK/Alarms/AlarmGroupViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Controls.Converters;
 using RubezhAPI;
 using RubezhAPI.GK;
 
@@ -12,9 +13,12 @@ namespace GKWebService.Models.GK.Alarms
 		public GKAlarmType AlarmType { get; set; }
 		public string AlarmTypeName { get; set; }
 		public string GlowColor { get; set; }
+		public string AlarmImageSource { get; set; }
+		public string AlarmName { get; set; }
 		public int Count { get; set; }
 		public bool HasAlarms { get; set; }
-
+		public List<ShortAlarmObject> Alarms { get; set; }
+		
 		public AlarmGroupViewModel()
 		{
 			
@@ -25,12 +29,32 @@ namespace GKWebService.Models.GK.Alarms
 			this.AlarmType = alarmType;
 			AlarmTypeName = AlarmType.ToString();
             GlowColor = new AlarmTypeToColorConverter().Convert(this.AlarmType);
+			AlarmImageSource = ((string)new AlarmTypeToBIconConverter().Convert(alarmType, null, null, null)).Substring(36).Replace(".png", "");
+			AlarmName = alarmType.ToDescription();
 		}
 
 		public void Update(List<AlarmViewModel> Alarms)
 		{
 			Count = Alarms.Count;
 			HasAlarms = (Count > 0);
+			this.Alarms = Alarms.Select(a => new ShortAlarmObject(a)).ToList();
+		}
+	}
+
+	public class ShortAlarmObject
+	{
+		public string ObjectName { get; set; }
+
+		public string ObjectImageSource { get; set; }
+
+		public ShortAlarmObject()
+		{
+
+		}
+		public ShortAlarmObject(AlarmViewModel model)
+		{
+			ObjectName = model.ObjectName;
+			ObjectImageSource = model.ObjectImageSource;
 		}
 	}
 }
