7741c9dde45822d4835bf95bd333a19e681c2c72
Maxim Obycheff <obychevma@mail.com>
2016-02-09 17:31:16 +0300

    RG-820 Реализация вкладки Журнал Событий

--- Projects/GKWebService/GKWebService/App/services/signalrJournalService.js
+++ Projects/GKWebService/GKWebService/App/services/signalrJournalService.js
@@ -6,7 +6,6 @@
 			var delaysHub = new Hub('journalUpdaterHub', {
 				listeners: {
 					'updateJournalItems': function (journalItems) {
-						console.log('broadcastService.send');
 						broadcastService.send('updateJournalItemsJs', journalItems);
 					}
 				}
