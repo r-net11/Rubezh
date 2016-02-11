d8488f5c0db062466d33a60fd75645ce694a5383
Dmitry Zabello <zabelloda@rubezh.ru>
2016-02-11 07:47:11 +0300

    Monodevelop experiment - FiresecService2

---
+++ Projects.tmp/FiresecClient/FiresecServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Common;
+using RubezhAPI;
+using System;
+using System.ServiceModel;
+
+namespace FiresecServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var address = args.Length == 0 ? "localhost:9988" : args[0];
+			Console.Title = address;
+			ServiceHost host = new ServiceHost(typeof(FiresecService2), new Uri("net.tcp://" + address));
+			host.AddServiceEndpoint(typeof(IFiresecService), BindingHelper.CreateNetTcpBinding(), "FiresecService");
+
+			host.Open();
+			Console.WriteLine("Сервер запущен");
+			Console.ReadLine();
+
+			host.Close();
+		}
+	}
+}
