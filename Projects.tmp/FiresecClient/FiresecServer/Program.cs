4821833b0ae5bdd5761c4d9d453d7f90e43f35f7
Dmitry Zabello <zabelloda@rubezh.ru>
2016-02-11 08:47:51 +0300

    Monodevelop experiment

--- Projects.tmp/FiresecClient/FiresecServer/Program.cs
+++ Projects.tmp/FiresecClient/FiresecServer/Program.cs
@@ -9,10 +9,10 @@ namespace FiresecServer
 	{
 		static void Main(string[] args)
 		{
-			var address = args.Length == 0 ? "localhost:9988" : args[0];
+			var address = args.Length == 0 ? "192.168.21.70:9988" : args[0];
 			Console.Title = address;
-			ServiceHost host = new ServiceHost(typeof(FiresecService2), new Uri("net.tcp://" + address));
-			host.AddServiceEndpoint(typeof(IFiresecService), BindingHelper.CreateNetTcpBinding(), "FiresecService");
+			ServiceHost host = new ServiceHost(typeof(FiresecService2));
+			host.AddServiceEndpoint(typeof(IFiresecService), BindingHelper.CreateNetTcpBinding(), new Uri("net.tcp://" + address + "/FiresecService/"));
 
 			host.Open();
 			Console.WriteLine("Сервер запущен");
