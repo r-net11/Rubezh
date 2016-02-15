e4b53a7e921e8f3bd89c0a41796430cf8c89670b
Kiselev Andrey <kiselevay@rubezh.ru>
2016-02-15 14:22:58 +0300

    Add Doors

---
+++ Projects/GKWebService/GKWebService/App/controllers/Doors/doorsCtrl.js
@@ -0,0 +1,55 @@
+﻿(function () {
+    var app = angular.module('gkApp.controllers');
+    app.controller('doorsCtrl', ['$scope', '$http', '$uibModal', 'signalrDoorsService', 'broadcastService',
+        function ($scope, $http, $uibModal, signalrDoorsService, broadcastService) {
+            $scope.uiGrid = {
+                enableRowSelection: true,
+                enableRowHeaderSelection: false,
+                multiSelect: false,
+                modifierKeysToMultiSelect: true,
+                noUnselect: true,
+                enableSorting: false,
+                enableColumnResizing: true,
+                enableColumnMenus: false,
+                onRegisterApi: function (gridApi) {
+                    $scope.gridApi = gridApi;
+                    gridApi.selection.on.rowSelectionChanged($scope, $scope.showSelectedRow);
+                },
+
+                columnDefs: [
+                    { field: 'No', enableColumnResizing: false, displayName: '№', width: 50, cellTemplate: '<div class="ui-grid-cell-contents"><img style="vertical-align: middle; padding-right: 3px" ng-src="/Content/Image/{{row.entity.ImageSource}}" />{{row.entity[col.field]}}</div>' },
+                    { field: 'Name', width: 200, displayName: 'Наименование', cellTemplate: '<div class="ui-grid-cell-contents"><a href="#" ng-click="grid.appScope.doorClick(row.entity)"><img style="vertical-align: middle; padding-right: 3px" ng-src="/Content/Image/Icon/GKStateIcons/{{row.entity.StateIcon}}.png"/>{{row.entity[col.field]}}</a></div>' },
+                    { field: 'DoorType', width: Math.round(($(window).width() - 525) / 2), displayName: 'Тип точки доступа' },
+                    { field: 'Desription', width: Math.round(($(window).width() - 525) / 2), displayName: 'Примечание' }
+                ]
+            };
+
+            $scope.$on('doorChanged', function (event, args) {
+                for (var i in $scope.uiGrid.data) {
+                    if (args.UID === $scope.uiGrid.data[i].UID) {
+                        $scope.uiGrid.data[i] = args;
+                        $scope.$apply();
+                        break;
+                    }
+                }
+            });
+
+            $scope.doorClick = function (door) {
+                var modalInstance = $uibModal.open({
+                    animation: false,
+                    size: 'rbzh',
+                    templateUrl: 'Doors/DoorDetails',
+                    controller: 'doorDetailsCtrl',
+                    resolve: {
+                        door: function () {
+                            return door;
+                        }
+                    }
+                });
+            };
+
+            $http.get('Doors/GetDoors').success(function (data) {
+                $scope.uiGrid.data = data;
+            });
+        }]);
+}());
---
+++ Projects/GKWebService/GKWebService/Models/Door/Door.cs
@@ -0,0 +1,81 @@
+﻿using RubezhAPI.GK;
+using RubezhAPI.Models;
+using RubezhClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Controls.Converters;
+using GKWebService.Converters;
+using RubezhAPI;
+using GKWebService.Models.GK;
+
+namespace GKWebService.Models.Door
+{
+	public class Door
+	{
+		public Door(GKDoor door)
+		{
+			UID = door.UID;
+			No = door.No;
+			GKDescriptorNo = door.GKDescriptorNo;
+			Name = door.Name;
+			DoorType = door.DoorType.ToDescription();
+			FullCanControl = ClientManager.CheckPermission(PermissionType.Oper_Full_Door_Control);
+			CanControl = ClientManager.CheckPermission(PermissionType.Oper_Door_Control);
+			Desription = door.Description;
+			ImageSource = door.ImageSource.Replace("/Controls;component/", "");
+			
+			State = door.State.StateClass.ToDescription();
+			StateIcon = door.State.StateClass.ToString();
+			StateClasses = door.State.StateClasses.Select(x => new StateClass(x)).ToList();
+			StateColor = "'#" + new XStateClassToColorConverter2().Convert(door.State.StateClass, null, null, null).ToString().Substring(3) + "'";
+
+
+			OnDelay = door.State.OnDelay;
+			HasOnDelay = door.State.StateClasses.Contains(XStateClass.TurningOn) && door.State.OnDelay > 0;
+			HasOffDelay = door.State.StateClasses.Contains(XStateClass.TurningOff) && door.State.OnDelay > 0;
+			OffDelay = door.State.OffDelay;
+			HasHoldDelay = door.State.StateClasses.Contains(XStateClass.Attention) && door.State.OffDelay > 0;
+
+			var controlRegime = door.State.StateClasses.Contains(XStateClass.Ignore)
+				? DeviceControlRegime.Ignore
+				: !door.State.StateClasses.Contains(XStateClass.AutoOff) ? DeviceControlRegime.Automatic : DeviceControlRegime.Manual;
+			//ControlRegimeIcon = "data:image/gif;base64," + InternalConverter.GetImageResource(((string)new DeviceControlRegimeToIconConverter().Convert(controlRegime)) ?? string.Empty).Item1;
+			ControlRegimeName = controlRegime.ToDescription();
+			ControlRegimeIcon = (new DeviceControlRegimeToIconConverter()).Convert(controlRegime);
+			CanSetAutomaticState = (controlRegime != DeviceControlRegime.Automatic);
+			CanSetManualState = (controlRegime != DeviceControlRegime.Manual);
+			CanSetIgnoreState = (controlRegime != DeviceControlRegime.Ignore);
+			IsControlRegime = (controlRegime == DeviceControlRegime.Manual);
+
+		}
+		public Guid UID { get; set; }
+		public int No { get; set; }
+		public string Name { get; set; }
+		public String OnClausesGroup { get; set; }
+		public string StateIcon { get; set; }
+		public bool CanSetAutomaticState { get; set; }
+		public bool CanSetManualState { get; set; }
+		public bool CanSetIgnoreState { get; set; }
+		public bool IsControlRegime { get; set; }
+		public string ControlRegimeName { get; set; }
+		public string ControlRegimeIcon { get; set; }
+		public bool HasOnDelay { get; set; }
+		public ushort GKDescriptorNo { get; set; }
+		public List<StateClass> StateClasses { get; set; }
+		public string StateColor { get; set; }
+		public string State { get; set; }
+		public int OnDelay { get; set; }
+		public int OffDelay { get; set; }
+		public bool HasOffDelay { get; set; }
+		public bool HasHoldDelay { get; set; }
+		public bool FullCanControl { get; set; }
+		public bool CanControl { get; set; }
+
+		public string DoorType { get; set; }
+
+		public string Desription { get; set; }
+
+		public string ImageSource { get; set; }
+	}
+}
