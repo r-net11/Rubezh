03b90801b657bed90228ee0f870f9de6ee9f4379
Alexandervelilyaev <Alexandervelilyaev@gmail.com>
2016-02-24 14:33:08 +0300

    Web. Сверстал окно, всплывающее, когда конфигурация на сервере изменилась. Добавил в контент картинок.

---
+++ Projects/GKWebService/GKWebService/App/controllers/restart/restartDetailsCtrl.js
@@ -0,0 +1,34 @@
+﻿(function () {
+
+	'use strict';
+
+	var app = angular.module('gkApp.controllers').controller('restartDetailsCtrl',
+        ['$scope', '$http', '$timeout', '$uibModalInstance',
+        function ($scope, $http, $timeout, $uibModalInstance) {
+
+        	$scope.time = 0;
+        	$scope.max = 60;
+
+        	$scope.ok = function () {
+        		location.reload();
+        		$uibModalInstance.close();
+        	};
+
+        	$scope.cancel = function () {
+        		$uibModalInstance.dismiss('cancel');
+        	};
+
+        	$scope.tick = function () {
+        		if ($scope.time <= $scope.max) {
+        			$scope.time += 1;
+        			$timeout($scope.tick, 1000);
+        		}
+        		else {
+        			$scope.ok();
+        		}
+        	}
+
+        	$timeout($scope.tick, 1000);
+        }]
+    );
+}());
