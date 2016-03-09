436103a534dbf056befe4971744913a4e40f7d8a
Kiselev Andrey <kiselevay@rubezh.ru>
2016-03-09 17:30:41 +0300

    Web. Скорректировать название состояний для охранных зон и точек доступа

---
+++ Projects/GKWebService/GKWebService/Models/Door/DoorStateClassToStringConverter.cs
@@ -0,0 +1,27 @@
+﻿using RubezhAPI.GK;
+using RubezhAPI;
+
+namespace GKWebService.Models.Door
+{
+	public class DoorStateClassToStringConverter
+	{
+		public static string Converter(XStateClass satate)
+		{
+			switch (satate)
+			{
+				case XStateClass.On:
+					return "Открыто";
+
+				case XStateClass.Off:
+					return "Закрыто";
+
+				case XStateClass.TurningOff:
+					return "Закрывается";
+
+				case XStateClass.Fire1:
+					return "Тревога";
+			}
+			return satate.ToDescription();
+		}
+	}
+}
