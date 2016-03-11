21ed5977b229cbb31c59281901ea6e0481a7fa16
Andrey Ivanov <goapsy2008@yandex.ru>
2016-03-11 09:27:01 +0300

    снятие выделения кнопок на планшете

---
+++ Projects/GKWebService/GKWebService/App/directives/mobileHoverFix.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    /*Для сенсорных устройств сброс состояния "курсор внутри"*/
+    angular.module("gkApp")
+        .directive('mobileHoverFix', function () {
+            return function (scope, element) {
+                element.find(".btn-primary")
+                    .on("touchstart", function () {
+                        $(this).addClass("mobileHoverFix");
+                    });
+            };
+        });
+}());
+
+
