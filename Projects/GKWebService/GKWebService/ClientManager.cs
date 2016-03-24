7380dd331920cd45dc4d636207d8f8a4a88b9347
Andrey Ivanov <goapsy2008@yandex.ru>
2016-03-24 16:47:48 +0300

    Веб. Исправление логина к серверу приложений

--- Projects/GKWebService/GKWebService/ClientManager.cs
+++ Projects/GKWebService/GKWebService/ClientManager.cs
@@ -11,74 +11,30 @@ namespace GKWebService
 {
 	public class ClientManager
 	{
-		private static ConcurrentDictionary<string, ISafeFiresecService> users;
-
-		private static object sync = new object();
-
 		public static ISafeFiresecService FiresecService
 		{
 			get
 			{
-				var userName = HttpContext.Current.User.Identity.Name;
-				return users[userName];
+				return RubezhClient.ClientManager.FiresecService;
 			}
-		}
-
-		static ClientManager()
-		{
-			users = new ConcurrentDictionary<string, ISafeFiresecService>();
 		}
 
 		public static string Connect(string userName, string password)
 		{
-			lock (sync)
+			string error = null;
+
+			if (!CheckPass(userName, password ?? string.Empty))
 			{
-				var clientCredentials = new ClientCredentials()
-				{
-					Login = userName,
-					Password = password ?? string.Empty,
-					ClientType = ClientType.WebService
-				};
-
-				string error = null;
-
-				if (users.ContainsKey(clientCredentials.Login))
-				{
-					//Если пользователь уже аудентифицирован и законнектен то надо только аудентифицировать, но не коннектить
-					if (!CheckPass(clientCredentials.Login, clientCredentials.Password))
-					{
-						error = "Неверный логин или пароль";
-					}
-				}
-				else
-				{
-					for (int i = 0; i < 3; i++)
-					{
-						var firesecService = new SafeFiresecService(ConnectionSettingsManager.ServerAddress);
-						var operationResult = firesecService.Connect(clientCredentials);
-						if (!operationResult.HasError)
-						{
-							users.AddOrUpdate(clientCredentials.Login, firesecService, (key, oldValue) => firesecService);
-							error = null;
-							break;
-						}
-						error = operationResult.Error;
-					}
-				}
-
-				return error;
+				error = "Неверный логин или пароль";
 			}
+
+			return error;
 		}
 
 		public static bool CheckPass(string userName, string password)
 		{
 			var user = RubezhClient.ClientManager.SecurityConfiguration.Users.FirstOrDefault(x => x.Login == userName);
 			return HashHelper.CheckPass(password, user.PasswordHash);
-		}
-
-		public static void AddAdminUser(string login, ISafeFiresecService firesecService)
-		{
-			users.AddOrUpdate(login, firesecService, (key, oldValue) => firesecService);
 		}
 
 		public static User CurrentUser
