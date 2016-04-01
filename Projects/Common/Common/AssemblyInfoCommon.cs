de71f61809d906d41b2da6f1831282a4f3548327
KonstantinDavidov <konstantin.davidov1@gmail.com>
2016-04-01 12:18:32 +0300

    Выпуск релиза 1.0.2 http://jira.rnd.local/browse/SKDDEV-1236

--- Projects/Common/Common/AssemblyInfoCommon.cs
+++ Projects/Common/Common/AssemblyInfoCommon.cs
@@ -3,7 +3,7 @@ using System.Windows;
 
 [assembly: AssemblyCompany("Компания СТРАЖ")]
 [assembly: AssemblyCopyright("Copyright © Компания СТРАЖ 2015")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
 [assembly: AssemblyTrademark("http://strazh-skd.ru/")]
 [assembly: AssemblyCulture("")]
 [assembly: ThemeInfo(
