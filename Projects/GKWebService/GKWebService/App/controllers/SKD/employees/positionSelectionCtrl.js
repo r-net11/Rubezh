78ddd74e74cdef7260185e7519ef5f16f0064488
Andrey Ivanov <goapsy2008@yandex.ru>
2016-04-20 16:00:52 +0300

    Веб. Перевод картотеки на angular. Выбор графика доступа, подразделения, сотрудника, руководителя, должности

---
+++ Projects/GKWebService/GKWebService/App/controllers/SKD/employees/positionSelectionCtrl.js
@@ -0,0 +1,58 @@
+﻿(function () {
+
+    'use strict';
+
+    var app = angular.module('gkApp.controllers').controller('positionSelectionCtrl',
+        ['$scope', '$http', '$uibModalInstance', 'organisationUID', 'positionUID', '$timeout',
+         function ($scope, $http, $uibModalInstance, organisationUID, positionUID, $timeout) {
+             $scope.gridOptions = {
+                 onRegisterApi: function (gridApi) {
+                     $scope.gridApi = gridApi;
+                     gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+                         $scope.selectedPosition = row.entity;
+                     });
+                 },
+                 enableRowHeaderSelection: false,
+                 enableSorting: false,
+                 multiSelect: false,
+                 enableColumnMenus: false,
+                 enableRowSelection: true,
+                 noUnselect: true,
+                 columnDefs: [
+                     { field: 'Name', width: 210, displayName: 'Название' },
+                     { field: 'Description', width: 210, displayName: 'Описание' }
+                 ]
+             };
+
+             $scope.gridStyle = function () {
+                 return "height: 300px";
+             }();
+
+             var reload = function () {
+                 $http.get('Employees/GetPositions', {
+                     params: { organisationUID: organisationUID, positionUID: positionUID }
+                 })
+                     .then(function (response) {
+                         $scope.positions = response.data.rows;
+                         $scope.gridOptions.data = $scope.positions;
+                         $scope.selectedPosition = null;
+                     });
+             }
+
+             reload();
+
+             $scope.save = function () {
+                 $uibModalInstance.close($scope.selectedPosition);
+             };
+
+             $scope.clear = function () {
+                 $uibModalInstance.close(null);
+             };
+
+             $scope.cancel = function () {
+                 $uibModalInstance.dismiss('cancel');
+             };
+         }]
+    );
+
+}());
--- Projects/GKWebService/GKWebService/App/controllers/SKD/organisations/organisationDetailsCtrl.js
+++ Projects/GKWebService/GKWebService/App/controllers/SKD/organisations/organisationDetailsCtrl.js
@@ -3,8 +3,8 @@
     'use strict';
 
     var app = angular.module('gkApp.controllers').controller('organisationDetailsCtrl',
-        ['$scope', '$uibModalInstance', 'organisation', 'isNew', 'organisationsService',
-        function ($scope, $uibModalInstance, organisation, isNew, organisationsService) {
+        ['$scope', '$uibModal', '$uibModalInstance', 'organisation', 'isNew', 'organisationsService',
+        function ($scope, $uibModal, $uibModalInstance, organisation, isNew, organisationsService) {
             $scope.isNew = isNew;
 
             $scope.organisation = organisation.Organisation;
@@ -16,6 +16,38 @@
             } else {
                 $scope.title = "Свойства организации: " + $scope.organisation.Name;
             }
+
+            $scope.selectChief = function() {
+                var modalInstance = $uibModal.open({
+                    animation: false,
+                    templateUrl: 'Hr/EmployeeSelectionDialog',
+                    controller: 'employeeSelectionDialogCtrl',
+                    backdrop: 'static',
+                    resolve: {
+                        employees: organisationsService.getOrganisationEmployees($scope.organisation.UID)
+                    }
+                });
+
+                modalInstance.result.then(function (employee) {
+                    $scope.model.SelectedChief = employee;
+                });
+            };
+
+            $scope.selectHRChief = function() {
+                var modalInstance = $uibModal.open({
+                    animation: false,
+                    templateUrl: 'Hr/EmployeeSelectionDialog',
+                    controller: 'employeeSelectionDialogCtrl',
+                    backdrop: 'static',
+                    resolve: {
+                        employees: organisationsService.getOrganisationEmployees($scope.organisation.UID)
+                    }
+                });
+
+                modalInstance.result.then(function (employee) {
+                    $scope.model.SelectedHRChief = employee;
+                });
+            };
 
             $scope.save = function () {
                 organisationsService.saveOrganisation($scope.model, $scope.isNew).then(function () {
