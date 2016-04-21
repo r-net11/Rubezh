67b076e5df49417161c2828afe7212f07c0ec53c
Sergey Durnov <durnov@outlook.com>
2016-04-21 14:19:28 +0300

    Веб. Фиксация.

--- Projects/GkWeb/src/GkWeb/wwwroot/_references.js
+++ Projects/GkWeb/src/GkWeb/wwwroot/_references.js
@@ -1,6 +1,7 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
 /// <reference path="app/app.component.js" />
+/// <reference path="app/hello/hello-world.component.js" />
 /// <reference path="app/main.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
