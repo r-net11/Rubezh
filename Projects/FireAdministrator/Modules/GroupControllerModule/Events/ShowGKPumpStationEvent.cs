416be2559167a5ccccb588ed070576a0ec8797cd
Kovalev Roman <KovalevRA@rubezh.ru>
2016-04-21 16:53:22 +0300

    RG-1307 АДМ. Валидация проекта. При клике на сообщение "Несвязанная задержка(НС)" не открывается соответствующая вкладка

--- Projects/FireAdministrator/Modules/GroupControllerModule/Events/ShowGKPumpStationEvent.cs
+++ Projects/FireAdministrator/Modules/GroupControllerModule/Events/ShowGKPumpStationEvent.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Infrastructure.Common.Navigation;
 using Microsoft.Practices.Prism.Events;
+using System;
 
 namespace GKModule.Events
 {
-	public class ShowGKPumpStationEvent : CompositePresentationEvent<Guid>
+	public class ShowGKPumpStationEvent : CompositePresentationEvent<ShowOnPlanArgs<Guid>>
 	{
 	}
 }
