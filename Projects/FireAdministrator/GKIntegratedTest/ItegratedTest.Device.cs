85b80164af42443fe9d7c5483bb4c001a9448c27
Dmitry Korneychuk <korneychukda@rubezh.ru>
2016-04-25 18:10:32 +0300

    Интеграционные тесты по сценарию RG-1015

--- Projects/FireAdministrator/GKIntegratedTest/ItegratedTest.Device.cs
+++ Projects/FireAdministrator/GKIntegratedTest/ItegratedTest.Device.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using System.Runtime.InteropServices;
+using GKProcessor;
+using NUnit.Framework;
 using RubezhAPI;
 using RubezhAPI.GK;
 using RubezhAPI.Journal;
@@ -17,16 +20,16 @@ namespace GKIntegratedTest
 		[TestCase(GKDriverType.RSR2_ABShS)]
 		[TestCase(GKDriverType.RSR2_ABTK)]
 		[TestCase(GKDriverType.RSR2_IOLIT)]
-		//[TestCase(GKDriverType.RSR2_Button)]
+		//[TestCase(GKDriverType.RSR2_Button)] устройство не поддерживается
 		/* Тестирование жизненного цикла датчиков и пожарной зоны в режиме Сработка1 */
 		public void TestDeviceFire1(GKDriverType driver)
 		{
 			var device1 = AddDevice(kauDevice11, driver);
 			var device2 = AddDevice(kauDevice11, driver);
 			var device3 = AddDevice(kauDevice11, driver);
-			var zone = new GKZone { Name = "Пожарная зона", No = 1 };
+			var zone = new GKZone {Name = "Пожарная зона", No = 1};
 			GKManager.AddZone(zone);
-			GKManager.AddDeviceToZone(device1,zone);
+			GKManager.AddDeviceToZone(device1, zone);
 			GKManager.AddDeviceToZone(device2, zone);
 			GKManager.AddDeviceToZone(device3, zone);
 			SetConfigAndRestartImitator();
@@ -37,18 +40,21 @@ namespace GKIntegratedTest
 			WaitWhileState(device1, XStateClass.Fire1, 3000, "Ожидаем пока device1 перейдёт в Сработка1");
 			Assert.IsTrue(device1.State.StateClass == XStateClass.Fire1, "Проверка того, что device1 находится в Сработка1");
 			Assert.IsTrue(zone.State.StateClass == XStateClass.Attention, "Проверка того, что пожараная зона перешла во Внимание");
-			CheckJournal(2, JournalItem(device1, JournalEventNameType.Сработка_1), JournalItem(zone, JournalEventNameType.Внимание));
+			CheckJournal(2, JournalItem(device1, JournalEventNameType.Сработка_1),
+				JournalItem(zone, JournalEventNameType.Внимание));
 			ConrtolGKBase(device2, GKStateBit.Fire1, "Сработка1 в device2");
 			WaitWhileState(device2, XStateClass.Fire1, 3000, "Ожидаем пока device2 перейдёт в Сработка1");
 			Assert.IsTrue(device2.State.StateClass == XStateClass.Fire1, "Проверка того, что device2 находится в Сработка1");
 			Assert.IsTrue(zone.State.StateClass == XStateClass.Fire1, "Проверка того, что пожараная зона перешла в Пожар1");
-			CheckJournal(2, JournalItem(device2, JournalEventNameType.Сработка_1), JournalItem(zone, JournalEventNameType.Пожар_1));
+			CheckJournal(2, JournalItem(device2, JournalEventNameType.Сработка_1),
+				JournalItem(zone, JournalEventNameType.Пожар_1));
 			ConrtolGKBase(device1, GKStateBit.Reset, "Сброс device1");
 			ConrtolGKBase(zone, GKStateBit.Reset, "Сброс зоны");
 			WaitWhileState(device1, XStateClass.Norm, 3000, "Ожидаем пока device1 перейдёт в Норму");
 			Assert.IsTrue(device1.State.StateClass == XStateClass.Norm, "Проверка того, что device1 перешёл в Норму");
 			Assert.IsTrue(zone.State.StateClasses.Contains(XStateClass.Attention), "Проверка того, что зона перешла во Внимание");
-			CheckJournal(3, JournalItem(device1, JournalEventNameType.Норма), JournalItem(zone, JournalEventNameType.Норма), JournalItem(zone, JournalEventNameType.Внимание));
+			CheckJournal(3, JournalItem(device1, JournalEventNameType.Норма), JournalItem(zone, JournalEventNameType.Норма),
+				JournalItem(zone, JournalEventNameType.Внимание));
 			ConrtolGKBase(device2, GKStateBit.Reset, "Сброс device2");
 			WaitWhileState(device2, XStateClass.Norm, 3000, "Ожидаем пока device2 перейдёт в Норму");
 			Assert.IsTrue(device2.State.StateClass == XStateClass.Norm, "Проверка того, что device2 перешёл в Норму");
@@ -67,7 +73,7 @@ namespace GKIntegratedTest
 			var device1 = AddDevice(kauDevice11, driver);
 			var device2 = AddDevice(kauDevice11, driver);
 			var device3 = AddDevice(kauDevice11, driver);
-			var zone = new GKZone { Name = "Пожарная зона", No = 1 };
+			var zone = new GKZone {Name = "Пожарная зона", No = 1};
 			GKManager.AddZone(zone);
 			GKManager.AddDeviceToZone(device1, zone);
 			GKManager.AddDeviceToZone(device2, zone);
@@ -80,7 +86,8 @@ namespace GKIntegratedTest
 			WaitWhileState(device1, XStateClass.Fire2, 3000, "Ожидаем пока device1 перейдёт в Сработка2");
 			Assert.IsTrue(device1.State.StateClass == XStateClass.Fire2, "Проверка того, что device1 находится в Сработка2");
 			Assert.IsTrue(zone.State.StateClass == XStateClass.Fire2, "Проверка того, что пожараная зона перешла в Пожар2");
-			CheckJournal(2, JournalItem(device1, JournalEventNameType.Сработка_2), JournalItem(zone, JournalEventNameType.Пожар_2));
+			CheckJournal(2, JournalItem(device1, JournalEventNameType.Сработка_2),
+				JournalItem(zone, JournalEventNameType.Пожар_2));
 			ConrtolGKBase(zone, GKStateBit.Reset, "Сброс зоны");
 			Assert.IsTrue(zone.State.StateClass == XStateClass.Fire2, "Проверка того, что зона всё ещё в режиме Сработка2");
 			ConrtolGKBase(device1, GKStateBit.Reset, "Сброс устройства1");
@@ -91,6 +98,92 @@ namespace GKIntegratedTest
 			WaitWhileState(zone, XStateClass.Norm, 3000, "Ожидаем пока зона перейдёт в Норму");
 			Assert.IsTrue(zone.State.StateClass == XStateClass.Norm, "Проверка того, что зона перешла в Норму");
 			CheckJournal(1, JournalItem(zone, JournalEventNameType.Норма));
+		}
+
+		[TestCase(DelayRegime.On, XStateClass.On)]
+		[TestCase(DelayRegime.Off, XStateClass.Off)]
+		/* RG-1015 (Если у направления задан Режим после удержания "Включено", то после окончания отсчета удержания он не должен переходить в режим "Включается")*/
+		public void TestDelayRegimeDirection(DelayRegime regime, XStateClass state, JournalEventNameType eventname)
+		{
+			var direction = new GKDirection { Name = "Направление", No = 1 };
+
+			var zone = new GKZone { Name = "Пожарная зона", No = 1 };
+			var clause = new GKClause
+			{
+				ClauseOperationType = ClauseOperationType.AllZones,
+				StateType = GKStateBit.Fire1,
+				ZoneUIDs = { zone.UID }
+			};
+			var device1 = AddDevice(kauDevice11, GKDriverType.RSR2_HeatDetector);
+			var device2 = AddDevice(kauDevice11, GKDriverType.RSR2_HeatDetector);
+			var device3 = AddDevice(kauDevice11, GKDriverType.RSR2_HeatDetector);
+			direction.Logic.OnClausesGroup.Clauses.Add(clause);
+			direction.Delay = 3;
+			direction.Hold = 3;
+			direction.DelayRegime = regime;
+			GKManager.AddZone(zone);
+			GKManager.AddDirection(direction);
+			GKManager.AddDeviceToZone(device1, zone);
+			GKManager.AddDeviceToZone(device2, zone);
+			GKManager.AddDeviceToZone(device3, zone);
+			SetConfigAndRestartImitator();
+
+			WaitWhileState(zone, XStateClass.Norm, 10000, "Ждем Норму в зоне");
+			Assert.IsTrue(zone.State.StateClass == XStateClass.Norm, "Проверка того, что зона в Норме");
+			ConrtolGKBase(device1, GKStateBit.Fire1, "Сработка1 у датчика1");
+			ConrtolGKBase(device2, GKStateBit.Fire1, "Сработка1 у датчика2");
+			WaitWhileState(direction, XStateClass.On, 5000, "Ждем пока направление не Включится");
+			Assert.IsTrue(direction.State.StateClass == XStateClass.On, "Проверка того, что направление Включено");
+			WaitWhileState(direction, XStateClass.TurningOn, 6000, "Ждем 6 секунд, направление не должено перейти в режим Включается");
+			Assert.IsFalse(direction.State.StateClass == XStateClass.TurningOn, "Проверка того, что направление не перешло в режим Включается");
+			Assert.IsTrue(direction.State.StateClass == state, "Проверка того, что направление Включено/Выключено");
+			/*			if (direction.DelayRegime == DelayRegime.On)
+							CheckJournal(4, JournalItem(direction, JournalEventNameType.Включается), JournalItem(new GKDevice(), JournalEventNameType.Включено), JournalItem(new GKDevice(), JournalEventNameType.Включено), JournalItem(direction, JournalEventNameType.Включено));
+						else
+						CheckJournal(5, JournalItem(direction, JournalEventNameType.Включается), JournalItem(new GKDevice(), JournalEventNameType.Включено), JournalItem(new GKDevice(), JournalEventNameType.Включено), JournalItem(direction, JournalEventNameType.Включено), JournalItem(direction, JournalEventNameType.Выключено));
+			Раскомментировать по закрытию RG-1339RG */
+		}
+
+		[TestCase(DelayRegime.On, XStateClass.On)]
+		[TestCase(DelayRegime.Off, XStateClass.Off)]
+		/* RG-1015 (Если у направления задан Режим после удержания "Включено", то после окончания отсчета удержания он не должен переходить в режим "Включается")*/
+		public void TestDelayRegimeDelay(DelayRegime regime, XStateClass state)
+		{
+			var delay = new GKDelay { Name = "Задержка", No = 1 };
+			var zone = new GKZone { Name = "Пожарная зона", No = 1 };
+			var clause = new GKClause
+			{
+				ClauseOperationType = ClauseOperationType.AllZones,
+				StateType = GKStateBit.Fire1,
+				ZoneUIDs = { zone.UID }
+			};
+			var device1 = AddDevice(kauDevice11, GKDriverType.RSR2_HeatDetector);
+			var device2 = AddDevice(kauDevice11, GKDriverType.RSR2_HeatDetector);
+			var device3 = AddDevice(kauDevice11, GKDriverType.RSR2_HeatDetector);
+			delay.Logic.OnClausesGroup.Clauses.Add(clause);
+			delay.DelayTime = 3;
+			delay.Hold = 3;
+			delay.DelayRegime = regime;
+			GKManager.AddZone(zone);
+			GKManager.AddDelay(delay);
+			GKManager.AddDeviceToZone(device1, zone);
+			GKManager.AddDeviceToZone(device2, zone);
+			GKManager.AddDeviceToZone(device3, zone);
+			SetConfigAndRestartImitator();
+
+			WaitWhileState(zone, XStateClass.Norm, 10000, "Ждем Норму в зоне");
+			Assert.IsTrue(zone.State.StateClass == XStateClass.Norm, "Проверка того, что зона в Норме");
+			ConrtolGKBase(device1, GKStateBit.Fire1, "Сработка1 у датчика1");
+			ConrtolGKBase(device2, GKStateBit.Fire1, "Сработка1 у датчика2");
+			WaitWhileState(delay, XStateClass.On, 5000, "Ждем пока задрежка не Включится");
+			Assert.IsTrue(delay.State.StateClass == XStateClass.On, "Проверка того, что задержка Включена");
+			WaitWhileState(delay, XStateClass.TurningOn, 6000, "Ждем 6 секунд, задрежка не должена перейти в режим Включается");
+			Assert.IsFalse(delay.State.StateClass == XStateClass.TurningOn, "Проверка того, что задрежка не перешла в режим Включается");
+			Assert.IsTrue(delay.State.StateClass == state, "Проверка того, что задрежка Включена/Выключена");
+			if (delay.DelayRegime == DelayRegime.On)
+				CheckJournal(3, JournalItem(delay, JournalEventNameType.Включается), JournalItem(gkDevice1.AllChildren[2], JournalEventNameType.Включено), JournalItem(delay, JournalEventNameType.Включено));
+			else
+				CheckJournal(4, JournalItem(delay, JournalEventNameType.Включается), JournalItem(gkDevice1.AllChildren[2], JournalEventNameType.Включено), JournalItem(delay, JournalEventNameType.Включено), JournalItem(delay, JournalEventNameType.Выключено));
 		}
 	}
 }
