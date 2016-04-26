777926ccda4903b700bd95cfadad12b1a92360c4
Mikhail Zemskov <dreamwalkerzmm@gmail.com>
2016-04-27 00:41:29 +0300

    RG-1154 Позиционирование главного окна при изменении положения таскбара.
    Интегрирована библиотека Microsoft.Windows.Shell

---
+++ Projects/Common/Controls/Converters/BoolToResizeModeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Controls.Converters
+{
+	class BoolToResizeModeConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (bool)value ? ResizeMode.CanResize : ResizeMode.NoResize;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
