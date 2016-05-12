fe827db99d70b997819b6a7c107719cfe7a09276
Kovalev Roman <KovalevRA@rubezh.ru>
2016-05-12 18:50:27 +0300

    Перемотка к выделенному элементу реализована для окон свойств, процедур, задержек, направлений, ТД, МПТ, НС

--- Projects/FireAdministrator/Modules/AutomationModule/Plans/Views/ProcedurePropertiesView.xaml.cs
+++ Projects/FireAdministrator/Modules/AutomationModule/Plans/Views/ProcedurePropertiesView.xaml.cs
@@ -8,5 +8,13 @@ namespace AutomationModule.Plans.Views
 		{
 			InitializeComponent();
 		}
+		private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			DataGrid dataGrid = sender as DataGrid;
+			if (dataGrid != null && dataGrid.SelectedItem != null && !dataGrid.IsMouseOver)
+			{
+				dataGrid.ScrollIntoView(dataGrid.SelectedItem);
+			}
+		}
 	}
 }
