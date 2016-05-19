aa918f76ad74e74b8fcb5a66fe8846494c42a5a0
Semen <sgordeev@live.com>
2016-05-19 13:40:30 +0300

    Веб. Звуки.

---
+++ Projects/GKWebService/GKWebService/DataProviders/Sounds/SoundsUpdaterHub.cs
@@ -0,0 +1,86 @@
+﻿using GKWebService.Models.Sound;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RubezhAPI;
+using RubezhAPI.GK;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace GKWebService.DataProviders.Sounds
+{
+	[HubName("soundsUpdater")]
+	public class SoundsUpdaterHub : Hub
+	{
+		public static SoundsUpdaterHub Instance { get; private set; }
+
+		List<RubezhAPI.Models.Sound> Sounds
+		{
+			get { return RubezhClient.ClientManager.SystemConfiguration.Sounds; }
+		}
+
+		public SoundsUpdaterHub()
+		{
+			Instance = this;
+		}
+
+		public void BroadcastSounds()
+		{
+			if (Sounds.IsNotNullOrEmpty() == false)
+			{
+				//IsSoundOn = false;
+				return;
+			}
+
+			var minSoundStateClass = XStateClass.Norm;
+			RubezhAPI.Models.Sound minSound = null;
+
+			foreach (var sound in Sounds)
+			{
+				if (!string.IsNullOrEmpty(sound.SoundName))
+				{
+					var hasStateClass = false;
+					foreach (var device in GKManager.Devices.Where(x => x.IsRealDevice))
+					{
+						if (sound.StateClass != XStateClass.Attention && sound.StateClass != XStateClass.Fire1 && sound.StateClass != XStateClass.Fire2)
+						{
+							if (device.State.StateClass == sound.StateClass)
+							{
+								hasStateClass = true;
+								break;
+							}
+						}
+					}
+					if (GKManager.Zones.Any(x => x.State != null && x.State.StateClass == sound.StateClass))
+					{
+						hasStateClass = true;
+					}
+					if (GKManager.Directions.Any(x => x.State != null && x.State.StateClass == sound.StateClass))
+					{
+						hasStateClass = true;
+					}
+
+					if (hasStateClass)
+					{
+						if (sound.StateClass < minSoundStateClass)
+						{
+							minSoundStateClass = sound.StateClass;
+							minSound = sound;
+						}
+					}
+				}
+			}
+			//if (minSound != null)
+			//{
+			//	AlarmPlayerHelper.Play(RubezhClient.FileHelper.GetSoundFilePath(minSound.SoundName), minSound.BeeperType, minSound.IsContinious);
+			//}
+			//else
+			//{
+			//	AlarmPlayerHelper.Stop();
+			//}
+
+			Clients.All.updateSounds(new { sound = minSound });
+		}
+	}
+}
---
+++ Projects/GKWebService/GKWebService/Models/Sound/Sound.cs
@@ -0,0 +1,20 @@
+﻿using RubezhAPI.GK;
+using RubezhAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GKWebService.Models.Sound
+{
+	public class Sound
+	{
+		public XStateClass StateClass { get; set; }
+
+		public string SoundName { get; set; }
+
+		public BeeperType BeeperType { get; set; }
+
+		public bool IsContinious { get; set; }
+	}
+}
