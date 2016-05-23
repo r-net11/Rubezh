a540ad809c9c5cf6722070e39fc3b2475e34f6d4
Dmitry Zabello <zabelloda@rubezh.ru>
2016-05-23 15:22:58 +0300

    fix

---
+++ Projects/RubezhService/RubezhService.Web/BoolExtentions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RubezhService
+{
+	public static class BoolExtentions
+	{
+		public static string ToYesNo(this Boolean value)
+		{
+			return value ? "Да" : "Нет";
+		}
+	}
+}
