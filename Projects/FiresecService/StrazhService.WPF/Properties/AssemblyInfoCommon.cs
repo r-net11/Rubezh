90c1d9ad820137f8a21cfda4a3d4051fb5b6fdce
Alexey Oleynichenko <oleynichenkoal@rvigroup.ru>
2016-05-26 17:31:49 +0300

    Удален проект "StrazhService.WPF"

--- Projects/FiresecService/StrazhService.WPF/Properties/AssemblyInfoCommon.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompany("Компания СТРАЖ")]
-[assembly: AssemblyCopyright("Copyright © Компания СТРАЖ 2015")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyTrademark("http://strazh-skd.ru/")]
-[assembly: AssemblyCulture("")]
