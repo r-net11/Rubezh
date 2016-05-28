e2f052a75e89e8162e95accb38bc04f6bd05eda3
Alexey Oleynichenko <oleynichenkoal@rvigroup.ru>
2016-05-28 17:22:27 +0300

    Монитор сервера. Бэкап базы

---
+++ Projects/FiresecService/StrazhService.Monitor/Database/IDatabaseService.cs
@@ -0,0 +1,8 @@
+﻿namespace StrazhService.Monitor.Database
+{
+	public interface IDatabaseService
+	{
+		bool CheckConnection(string ipAddress, int ipPort, string instanceName, bool useIntegratedSecurity, string userID, string userPwd, out string errors);
+		bool CreateBackup(string ipAddress, int ipPort, string instanceName, bool useIntegratedSecurity, string userID, string userPwd, string databaseName, string backupFileName, out string errors);
+	}
+}
