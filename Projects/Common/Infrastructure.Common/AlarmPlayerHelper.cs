77cfe8fde6c60d968a1e82f805d1fd14567e9069
Alexey Oleynichenko <oleynichenkoal@rvigroup.ru>
2016-06-09 15:26:35 +0300

    SKDDEV-1316 Реализовать библиотеку звуков ОЗ. Клавиши управления "Вкл/Выкл звук" и "Остановить проигрывание звука"

--- Projects/Common/Infrastructure.Common/AlarmPlayerHelper.cs
+++ Projects/Common/Infrastructure.Common/AlarmPlayerHelper.cs
@@ -1,67 +1,16 @@
-﻿using StrazhAPI.Models;
-using System;
-using System.IO;
+﻿using System.IO;
 using System.Media;
-using System.Threading;
 
 namespace Infrastructure.Common
 {
 	public static class AlarmPlayerHelper
 	{
 		private static SoundPlayer _soundPlayer;
-		private static bool _isContinious;
-		private static Thread _beepThread;
-		private static int _beepFrequency;
-		private static bool _isBeepThreadStopping;
+		private static bool _isMuted;
 
 		static AlarmPlayerHelper()
 		{
 			_soundPlayer = new SoundPlayer();
-		}
-
-		private static void PlayBeep()
-		{
-			try
-			{
-				do
-				{
-					if (_isBeepThreadStopping)
-						break;
-					Console.Beep(_beepFrequency, 600);
-					if (_isBeepThreadStopping)
-						break;
-					Thread.Sleep(1000);
-				}
-				while (_isContinious);
-				_isBeepThreadStopping = false;
-				_beepThread = null;
-			}
-			catch (Exception e)
-			{
-				return;
-			}
-		}
-
-		private static void StopPlayPCSpeaker()
-		{
-			_isBeepThreadStopping = true;
-		}
-
-		private static void PlayPCSpeaker(BeeperType beeperType, bool isContinious)
-		{
-			if (beeperType != BeeperType.None)
-			{
-				_beepFrequency = (int)beeperType;
-				_isContinious = isContinious;
-
-				_isBeepThreadStopping = false;
-				if (_beepThread == null)
-				{
-					_beepThread = new Thread(PlayBeep);
-					_beepThread.Name = "PC Speaker";
-					_beepThread.Start();
-				}
-			}
 		}
 
 		private static void PlaySound(string filePath, bool isContinious)
@@ -97,13 +46,24 @@ namespace Infrastructure.Common
 
 		public static void Play(string filePath, bool isContinious)
 		{
-			PlaySound(filePath, isContinious);
+			if (!IsMuted)
+				PlaySound(filePath, isContinious);
 		}
 
 		public static void Stop()
 		{
 			StopPlaySound();
-			StopPlayPCSpeaker();
+		}
+
+		public static bool IsMuted
+		{
+			get { return _isMuted; }
+			set
+			{
+				_isMuted = value;
+				if (_isMuted)
+					StopPlaySound();
+			}
 		}
 
 		public static void Dispose()
--- Projects/FireMonitor/FireMonitor/ViewModels/SoundViewModel.cs
+++ Projects/FireMonitor/FireMonitor/ViewModels/SoundViewModel.cs
@@ -1,14 +1,6 @@
-﻿using System.Collections.Generic;
-using StrazhAPI;
-using StrazhAPI.Events;
-using StrazhAPI.GK;
-using StrazhAPI.Models;
-using FiresecClient;
-using Infrastructure;
-using Infrastructure.Common;
+﻿using Infrastructure.Common;
 using Infrastructure.Common.Windows.ViewModels;
-using Infrastructure.Events;
-using StrazhAPI.SKD;
+using System.Windows.Input;
 
 namespace FireMonitor.ViewModels
 {
@@ -16,14 +8,12 @@ namespace FireMonitor.ViewModels
 	{
 		public SoundViewModel()
 		{
-			ServiceFactory.Events.GetEvent<SKDObjectsStateChangedEvent>().Unsubscribe(OnStateChanged);
-			ServiceFactory.Events.GetEvent<SKDObjectsStateChangedEvent>().Subscribe(OnStateChanged);
-
-			PlaySoundCommand = new RelayCommand(OnPlaySound);
-			OnStateChanged(null);
+			SoundOnOffCommand = new RelayCommand(OnSoundOnOff);
+			StopPlayingCommand = new RelayCommand(OnStopPlaying);
+			IsSoundOn = true;
 		}
 
-		bool _isSoundOn;
+		private bool _isSoundOn;
 		public bool IsSoundOn
 		{
 			get { return _isSoundOn; }
@@ -34,72 +24,27 @@ namespace FireMonitor.ViewModels
 			}
 		}
 
-		List<Sound> Sounds
+		public ICommand SoundOnOffCommand { get; private set; }
+		private void OnSoundOnOff()
 		{
-			get { return FiresecClient.FiresecManager.SystemConfiguration.Sounds; }
-		}
-
-		public void OnStateChanged(object obj)
-		{
-			IsSoundOn = true;
-			PlayAlarm();
-		}
-
-		void PlayAlarm()
-		{
-			if (Sounds.IsNotNullOrEmpty() == false)
-			{
-				IsSoundOn = false;
-				return;
-			}
-
-			var minSoundStateClass = XStateClass.Norm;
-			Sound minSound = null;
-
-			foreach (var sound in Sounds)
-			{
-				if (!string.IsNullOrEmpty(sound.SoundName))
-				{
-					var hasStateClass = false;
-
-					foreach (var device in SKDManager.Devices)
-					{
-						if (device.State != null && device.State.StateClass == sound.StateClass)
-						{
-							hasStateClass = true;
-							break;
-						}
-					}
-
-					if (hasStateClass)
-					{
-						if (sound.StateClass < minSoundStateClass)
-						{
-							minSoundStateClass = sound.StateClass;
-							minSound = sound;
-						}
-					}
-				}
-			}
-			if (minSound != null)
-			{
-				AlarmPlayerHelper.Play(FiresecClient.FileHelper.GetSoundFilePath(minSound.SoundName), minSound.IsContinious);
-			}
-		}
-
-		public RelayCommand PlaySoundCommand { get; private set; }
-		void OnPlaySound()
-		{
+			// Выключить звук
 			if (IsSoundOn)
 			{
-				AlarmPlayerHelper.Stop();
+				AlarmPlayerHelper.IsMuted = true;
 				IsSoundOn = false;
 			}
+			// Включить звук
 			else
 			{
-				PlayAlarm();
+				AlarmPlayerHelper.IsMuted = false;
 				IsSoundOn = true;
 			}
+		}
+
+		public ICommand StopPlayingCommand { get; private set; }
+		private void OnStopPlaying()
+		{
+			AlarmPlayerHelper.Stop();
 		}
 	}
 }
