231ad74228abce731920a5f4dddef89a2e9fdee3
Alexey Oleynichenko <oleynichenkoal@rvigroup.ru>
2016-08-01 09:46:11 +0300

    SKDDEV-709 В списке устройств отображается активным 2 считыватель у однодверного контроллера с односторонней ТД В списке выбора устройств при указании считывателя для ТД участвуют только считыватели, у которых IsEnabled = true

--- Projects/FireAdministrator/Modules/StrazhModule/Doors/ViewModels/DeviceSelectationViewModel.cs
+++ Projects/FireAdministrator/Modules/StrazhModule/Doors/ViewModels/DeviceSelectationViewModel.cs
@@ -12,28 +12,25 @@ namespace StrazhModule.ViewModels
 		{
 			Title = "Выбор устройства";
 			Devices = new ObservableCollection<SKDDevice>();
-			foreach (var skdDevice in SKDManager.Devices)
+			foreach (var skdDevice in SKDManager.Devices.Where(x => x.DriverType == SKDDriverType.Reader && x.IsEnabled))
 			{
-				if (skdDevice.DriverType == SKDDriverType.Reader)
+				if (skdDevice.Parent != null && skdDevice.Parent.DoorType == doorType)
 				{
-					if (skdDevice.Parent != null && skdDevice.Parent.DoorType == doorType)
+					if (doorType == DoorType.TwoWay)
 					{
-						if (doorType == DoorType.TwoWay)
-						{
-							if (skdDevice.IntAddress % 2 == 1)
-								continue;
-							var outReader = SKDManager.Devices.FirstOrDefault(x => x.Parent != null && skdDevice.Parent != null && x.Parent.UID == skdDevice.Parent.UID && x.IntAddress == skdDevice.IntAddress + 1);
-							if (outReader == null)
-								continue;
-							if (outReader.Door != null)
-								continue;
-						}
-
-						if (skdDevice.Door != null)
+						if (skdDevice.IntAddress % 2 == 1)
 							continue;
-
-						Devices.Add(skdDevice);
+						var outReader = SKDManager.Devices.FirstOrDefault(x => x.Parent != null && skdDevice.Parent != null && x.Parent.UID == skdDevice.Parent.UID && x.IntAddress == skdDevice.IntAddress + 1);
+						if (outReader == null)
+							continue;
+						if (outReader.Door != null)
+							continue;
 					}
+
+					if (skdDevice.Door != null)
+						continue;
+
+					Devices.Add(skdDevice);
 				}
 			}
 			SelectedDevice = Devices.FirstOrDefault(x => x.UID == deviceUID);
@@ -41,7 +38,7 @@ namespace StrazhModule.ViewModels
 
 		public ObservableCollection<SKDDevice> Devices { get; private set; }
 
-		SKDDevice _selectedDevice;
+		private SKDDevice _selectedDevice;
 		public SKDDevice SelectedDevice
 		{
 			get { return _selectedDevice; }
