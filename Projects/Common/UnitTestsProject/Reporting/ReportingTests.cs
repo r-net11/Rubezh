3a02e39afa195af11488728c8a883af22ab85538
paxan222 <paxan222@yandex.ru>
2016-09-05 08:18:32 +0300

    1. Добавление файлов локализации в сервер приложений 2. Рефакторинг FiresecServiceUnitTests/ReportingTests

--- Projects/Common/UnitTestsProject/Reporting/ReportingTests.cs
+++ Projects/Common/UnitTestsProject/Reporting/ReportingTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using FiresecService.Report.Helpers;
 using FiresecService.Report.Templates;
+using Localization.FiresecService.Report.Common;
 using NUnit.Framework;
 using System;
 using System.Linq;
@@ -17,25 +18,26 @@ namespace UnitTestsProject.Reporting
 			//Arrange
 			var testCollection = new List<string>
 			{
-				"Сведения о пропусках",
-				"Список подразделений организации",
-				"Дисциплинарный отчет",
-				"Отчет по оправдательным документам",
-				"Список точек доступа",
-				"Доступ в зоны сотрудников",
-				"Права доступа сотрудников",
-				"Справка о сотруднике",
-				"Маршрут сотрудника",
-				"Местонахождение сотрудников",
-				"Отчет по событиям системы контроля доступа",
-				"Список должностей организации",
-				"Отчет по графикам работы",
-				"Справка по отработанному времени"
+				CommonResources.PasscardsInfo,//"Сведения о пропусках",
+				CommonResources.DepartList,//"Список подразделений организации",
+				CommonResources.DisciplinaryReport,//"Дисциплинарный отчет",
+				CommonResources.DocumentsReport,//"Отчет по оправдательным документам",
+				CommonResources.DoorsList,//"Список точек доступа",
+				CommonResources.AccessZone+ CommonResources.Employees,//"Доступ в зоны сотрудников",
+				CommonResources.AccessPermissions + CommonResources.Employees,//"Права доступа сотрудников",
+				CommonResources.InfoAboutEmployee,//"Справка о сотруднике",
+				CommonResources.Route + CommonResources.EmployeeNo,//"Маршрут сотрудника",
+				CommonResources.Location + CommonResources.EmployeeNo,//"Местонахождение сотрудников",
+				CommonResources.SystemEventsReport,//"Отчет по событиям системы контроля доступа",
+				CommonResources.PositionList,//"Список должностей организации",
+				CommonResources.WorkScheduleReport,//"Отчет по графикам работы",
+				CommonResources.InfoAboutWorkTime//"Справка по отработанному времени"
 			}.OrderBy(x => x)
 			.ToList();
 
 			//Act
 			var result = ReportingHelpers.GetReportNames().OrderBy(x => x).ToList();
+
 
 			//Assert
 			Assert.AreEqual(result.Count, 14);
