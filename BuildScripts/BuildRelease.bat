ea2b3e6e63c5c038e91c5b2eba921c2e57e7f9ab
Kovalev Roman <KovalevRA@rubezh.ru>
2017-05-07 17:24:59 +0400

    Поднял версию до 1.0.3.5

--- BuildScripts/BuildRelease.bat
+++ BuildScripts/BuildRelease.bat
@@ -4,5 +4,5 @@ set NAntPath=%cd%\..\3rdParty\NAnt\bin\NAnt.exe
 del %cd%\bin\GK /Q
 %NAntPath% -logger:NAnt.Core.XmlLogger -logfile:buildlog_Copy3dPartyDlls.xml -buildfile:nant/Copy3dPartyDlls.build
 %NAntPath% -logger:NAnt.Core.XmlLogger -logfile:buildlog_CopySDK.xml -buildfile:nant/CopySDK.build
-%NAntPath% -logger:NAnt.Core.XmlLogger -logfile:buildlog_BuildRelease.xml -buildfile:nant/BuildRelease.build -D:InstallerAssemblyNamePrefix= -D:InstallerAssemblyName=A.C.Tech -D:OurProductVersion=1.0.3 -D:OurProductBuildNumber=4 -D:Culture=ru
+%NAntPath% -logger:NAnt.Core.XmlLogger -logfile:buildlog_BuildRelease.xml -buildfile:nant/BuildRelease.build -D:InstallerAssemblyNamePrefix= -D:InstallerAssemblyName=A.C.Tech -D:OurProductVersion=1.0.3 -D:OurProductBuildNumber=5 -D:Culture=ru
 echo END
